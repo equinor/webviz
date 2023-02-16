7e8fe8cf4704661070eedcaa3948997694f1892e
Sigurd Pettersen <sigurd.pettersen@ceetronsolutions.com>
2023-02-16 08:11:03 +0100

    Regenerated service api in frontend

---
+++ frontend/src/api/models/StatisticValueObject.ts
@@ -0,0 +1,11 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+import type { StatisticFunction } from './StatisticFunction';
+
+export type StatisticValueObject = {
+    statistic_function: StatisticFunction;
+    values: Array<number>;
+};
+
