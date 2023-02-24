b05844bc9516473a019b612573bf3176a1db9a78
Sigurd Pettersen <sigurd.pettersen@ceetronsolutions.com>
2023-02-24 17:55:13 +0100

    Hardcode most variables in config.py instead of using environment

--- backend/src/fastapi_app/config.py
+++ backend/src/fastapi_app/config.py
@@ -1,25 +1,36 @@
 import os
 from dotenv import load_dotenv
 
+# Import these from sumo wrapper package in order to grab the resource scopes
+from sumo.wrapper.config import APP_REGISTRATION as sumo_app_reg
+
 # Load environment variables from .env file
 # Note that values set in the system environment will override those in the .env file
 load_dotenv()
 
-TENANT_ID = os.environ["WEBVIZ_TENANT_ID"]
-CLIENT_ID = os.environ["WEBVIZ_CLIENT_ID"]
+
+TENANT_ID = "3aa4a235-b6e2-48d5-9195-7fcf05b459b0"
+CLIENT_ID = "900ed417-a860-4970-bd37-73b059ca6f0d"
+
 CLIENT_SECRET = os.environ["WEBVIZ_CLIENT_SECRET"]
-SUMO_RESOURCE_SCOPE = os.environ["WEBVIZ_SUMO_RESOURCE_SCOPE"]
-SMDA_RESOURCE_SCOPE = os.environ["WEBVIZ_SMDA_RESOURCE_SCOPE"]
+
+
+GRAPH_SCOPES = ["User.Read"]
+
+RESOURCE_SCOPES_DICT = {
+    "sumo": [f"api://{sumo_app_reg['prod']['RESOURCE_ID']}/access_as_user"],
+}
+
+# Since we are not using SMDA yet, leave it as optional
+SMDA_RESOURCE_SCOPE = os.environ.get("WEBVIZ_SMDA_RESOURCE_SCOPE")
+if SMDA_RESOURCE_SCOPE is not None:
+    RESOURCE_SCOPES_DICT["smda"] = [SMDA_RESOURCE_SCOPE]
+
+print(f"{RESOURCE_SCOPES_DICT=}")
+
 
 # Allow None her for now, since we don't always run with redis and a password
 # REDIS_PASSWORD: str = os.environ.get("WEBVIZ_REDIS_PASSWORD")
-
-GRAPH_SCOPES = ["User.Read"]
-RESOURCE_SCOPES_DICT = {
-    "sumo": [SUMO_RESOURCE_SCOPE],
-    "smda": [SMDA_RESOURCE_SCOPE],
-}
-
 
 # Format: redis://[[username]:[password]]@localhost:6379/0
 # REDIS_URL = "redis://localhost"
