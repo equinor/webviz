633f855b8eab08fa3e44f0d0f691ebeee25e8ad8
Sigurd Pettersen <sigurd.pettersen@ceetronsolutions.com>
2023-02-27 15:37:32 +0100

    Started work on accessing surfaces in sumo

--- backend/src/services/sumo_access/dev_sumo_access_test_driver.py
+++ backend/src/services/sumo_access/dev_sumo_access_test_driver.py
@@ -1,46 +1,18 @@
 import sys
 from typing import List
+import logging
 
 import pyarrow as pa
 
 from fmu.sumo.explorer.explorer import SumoClient
 
 from .summary_access import SummaryAccess, RealizationVector, Frequency
+from .surface_access import SurfaceAccess, DynamicSurfaceDirectory
 from .sumo_explore import SumoExplore
 from ..summary_vector_statistics import compute_vector_statistics_table, compute_vector_statistics
 
 
-def main() -> None:
-    print("## Running dev_sumo_access_test_driver")
-    print("## =================================================")
-
-    #dummy_sumo_client = SumoClient("prod")
-    dummy_sumo_client = SumoClient("dev")
-    access_token = dummy_sumo_client._retrieve_token()
-
-    explore = SumoExplore(access_token=access_token)
-    cases = explore.get_cases(field_identifier="DROGON")
-    print(cases)
-
-    #sumo_case_id = "0db5f2dd-aa62-407f-9ac4-0dbbe30371a2"
-    sumo_case_id = "10f41041-2c17-4374-a735-bb0de62e29dc"
-    sumo_case_name = None
-    for case_info in cases:
-        if case_info.uuid == sumo_case_id:
-            sumo_case_name = case_info.name
-
-    if not sumo_case_name:
-        print("The sumo case id was not found")
-        sys.exit(1)
-
-    iterations = explore.get_iterations(sumo_case_id)
-    print("\n\n")
-    print(iterations)
-
-    iteration_name = iterations[0].name
-    summary_access = SummaryAccess(access_token=access_token, case_uuid=sumo_case_id, iteration_name=iteration_name)
-
-
+def test_summary_access(summary_access: SummaryAccess) -> None:
     vector_names = summary_access.get_vector_names()
     print("\n\n")
     print(vector_names)
@@ -84,6 +56,61 @@ def main() -> None:
     print("\n\nSTATS")
     vec_stats = compute_vector_statistics(vector_table, "FOPT", None)
     print(vec_stats)
+
+
+def test_surface_access(surf_access: SurfaceAccess) -> None:
+    dynamic_surf_dir = surf_access.get_dynamic_surf_dir()
+    print(f"{dynamic_surf_dir=}")
+
+    static_surf_dir = surf_access.get_static_surf_dir()
+    print(f"{static_surf_dir=}")
+
+    dyn_surf = surf_access.get_dynamic_surf(real_num=0, name=dynamic_surf_dir.names[0], attribute=dynamic_surf_dir.attributes[0], time_or_interval_str=dynamic_surf_dir.date_strings[0])
+    print(f"{type(dyn_surf)=}")
+
+
+def main() -> None:
+    print("## Running dev_sumo_access_test_driver")
+    print("## =================================================")
+
+    logging.basicConfig(
+        level=logging.WARNING,
+        format="%(asctime)s %(levelname)-3s [%(name)s]: %(message)s",
+    )
+    #logging.getLogger("").setLevel(level=logging.DEBUG)
+    logging.getLogger("src.services.sumo_access").setLevel(level=logging.DEBUG)
+
+    #dummy_sumo_client = SumoClient("prod")
+    dummy_sumo_client = SumoClient("dev")
+    access_token = dummy_sumo_client._retrieve_token()
+
+    explore = SumoExplore(access_token=access_token)
+    cases = explore.get_cases(field_identifier="DROGON")
+    print(cases)
+
+    #sumo_case_id = "0db5f2dd-aa62-407f-9ac4-0dbbe30371a2"
+    sumo_case_id = "10f41041-2c17-4374-a735-bb0de62e29dc"
+    sumo_case_name = None
+    for case_info in cases:
+        if case_info.uuid == sumo_case_id:
+            sumo_case_name = case_info.name
+
+    if not sumo_case_name:
+        print("The sumo case id was not found")
+        sys.exit(1)
+
+    iterations = explore.get_iterations(sumo_case_id)
+    print("\n\n")
+    print(iterations)
+
+    iteration_name = iterations[0].name
+
+    # summary_access = SummaryAccess(access_token=access_token, case_uuid=sumo_case_id, iteration_name=iteration_name)
+    # test_summary_access(summary_access)
+
+    surface_access = SurfaceAccess(access_token=access_token, case_uuid=sumo_case_id, iteration_name=iteration_name)
+    test_surface_access(surface_access)
+
 
 
 # Running:
---
+++ backend/src/services/sumo_access/surface_access.py
@@ -0,0 +1,161 @@
+import logging
+from io import BytesIO
+from typing import List, Optional, Tuple
+
+import xtgeo
+from fmu.sumo.explorer import TimeFilter, TimeType
+from fmu.sumo.explorer.objects import (Case, CaseCollection, Surface,
+                                       SurfaceCollection)
+from pydantic import BaseModel
+from sumo.wrapper import SumoClient
+
+from ..utils.perf_timer import PerfTimer
+from ._helpers import create_sumo_client_instance
+
+LOGGER = logging.getLogger(__name__)
+
+
+class DynamicSurfaceDirectory(BaseModel):
+    names: List[str]
+    attributes: List[str]
+    date_strings: List[str]
+
+    @classmethod
+    def create_empty(cls):
+        return cls(attributes=[], names=[], date_strings=[])
+
+
+class StaticSurfaceDirectory(BaseModel):
+    names: List[str]
+    attributes: List[str]
+
+    @classmethod
+    def create_empty(cls):
+        return cls(attributes=[], names=[])
+
+
+class SurfaceAccess:
+    def __init__(self, access_token: str, case_uuid: str, iteration_name: str):
+        self._sumo_client: SumoClient = create_sumo_client_instance(access_token)
+        self._case_uuid = case_uuid
+        self._iteration_name = iteration_name
+
+    def get_dynamic_surf_dir(self) -> DynamicSurfaceDirectory:
+        """
+        Get a directory of surface names, attributes and date strings for simulated dynamic surfaces.
+        These are the non-observed surfaces that DO have time stamps or time intervals
+        """
+        timer = PerfTimer()
+
+        case_collection = CaseCollection(self._sumo_client).filter(uuid=self._case_uuid)
+        if len(case_collection) != 1:
+            raise ValueError(f"None or multiple sumo cases found {self._case_uuid=}")
+
+        case = case_collection[0]
+
+        timestamp_or_interval_filter = TimeFilter(TimeType.ALL)
+        surface_collection: SurfaceCollection = case.surfaces.filter(
+            iteration=self._iteration_name, aggregation=False, time=timestamp_or_interval_filter
+        )
+
+        names = surface_collection.names
+        attributes = surface_collection.tagnames
+        timestamps: List[str] = surface_collection.timestamps
+        intervals: List[Tuple[str, str]] = surface_collection.intervals
+
+        # ISO 8601 recommends '/' as separator, alternatively '--'
+        # https://en.wikipedia.org/wiki/ISO_8601#Time_intervals
+        intervals_as_strings: List[str] = [f"{interval[0]}--{interval[1]}" for interval in intervals]
+
+        date_strings: List[str] = []
+        date_strings.extend(timestamps)
+        date_strings.extend(intervals_as_strings)
+
+        surf_dir = DynamicSurfaceDirectory(names=names, attributes=attributes, date_strings=date_strings)
+
+        LOGGER.debug(f"Built dynamic surface directory in: {timer.elapsed_ms():}ms")
+
+        return surf_dir
+
+    def get_static_surf_dir(self) -> StaticSurfaceDirectory:
+        """
+        Get a directory of surface names and attributes for static surfaces.
+        These are the non-observed surfaces that do NOT have time stamps
+        """
+        timer = PerfTimer()
+
+        case_collection = CaseCollection(self._sumo_client).filter(uuid=self._case_uuid)
+        if len(case_collection) != 1:
+            raise ValueError(f"None or multiple sumo cases found {self._case_uuid=}")
+
+        case = case_collection[0]
+
+        no_time_data_filter = TimeFilter(TimeType.NONE)
+        surface_collection: SurfaceCollection = case.surfaces.filter(
+            iteration=self._iteration_name, aggregation=False, time=no_time_data_filter
+        )
+
+        names = surface_collection.names
+        attributes = surface_collection.tagnames
+
+        surf_dir = StaticSurfaceDirectory(names=names, attributes=attributes)
+
+        LOGGER.debug(f"Built static surface directory in: {timer.elapsed_ms():}ms")
+
+        return surf_dir
+
+    def get_dynamic_surf(
+        self, real_num: int, name: str, attribute: str, time_or_interval_str: str
+    ) -> Optional[xtgeo.RegularSurface]:
+        """Get actual surface data for a simulated dynamic surface"""
+
+        # All dynamic sim surfaces should have a string containing a time stamp or time interval
+        if not time_or_interval_str or len(time_or_interval_str) < 1:
+            raise ValueError("time_or_interval_str must contain a non-empty string")
+
+        timestamp_arr = time_or_interval_str.split("--", 1)
+        if len(timestamp_arr) == 0 or len(timestamp_arr) > 2:
+            raise ValueError("time_or_interval_str must contain a single timestamp or interval")
+
+        timer = PerfTimer()
+
+        addr_str = self._make_addr_str(real_num, name, attribute, time_or_interval_str)
+
+        case_collection = CaseCollection(self._sumo_client).filter(uuid=self._case_uuid)
+        if len(case_collection) != 1:
+            raise ValueError(f"None or multiple sumo cases found {self._case_uuid=}")
+
+        case = case_collection[0]
+
+        if len(timestamp_arr) == 1:
+            time_filter = TimeFilter(TimeType.TIMESTAMP, start=timestamp_arr[0], end=timestamp_arr[0], exact=True)
+        else:
+            time_filter = TimeFilter(TimeType.INTERVAL, start=timestamp_arr[0], end=timestamp_arr[1], exact=True)
+
+        surface_collection = case.surfaces.filter(
+            iteration=self._iteration_name,
+            aggregation=False,
+            realization=real_num,
+            name=name,
+            tagname=attribute,
+            time=time_filter,
+        )
+
+        surf_count = len(surface_collection)
+        if surf_count == 0:
+            LOGGER.warning(f"No dynamic surface found in Sumo for {addr_str}")
+            return None
+        if surf_count > 1:
+            LOGGER.warning(f"Multiple ({surf_count}) surfaces found in Sumo for: {addr_str}. returning first surface.")
+
+        sumo_surf = surface_collection[0]
+        byte_stream: BytesIO = sumo_surf.blob
+        xtgeo_surf = xtgeo.surface_from_file(byte_stream)
+
+        LOGGER.debug(f"Got dynamic surface from Sumo in: {timer.elapsed_ms()}ms ({addr_str})")
+
+        return xtgeo_surf
+
+    def _make_addr_str(self, real_num: int, name: str, attribute: str, date_str: Optional[str]) -> str:
+        addr_str = f"R:{real_num}__N:{name}__A:{attribute}__D:{date_str}__I:{self._iteration_name}__C:{self._case_uuid}"
+        return addr_str
