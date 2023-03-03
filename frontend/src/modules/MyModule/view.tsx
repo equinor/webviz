427cdd0870ab4410fd9524b7f688ad8db76bf5cb
Sigurd Pettersen <sigurd.pettersen@ceetronsolutions.com>
2023-03-03 14:47:24 +0100

    Switch from v3 of react-query to v4 (#44)

--- frontend/src/modules/MyModule/view.tsx
+++ frontend/src/modules/MyModule/view.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { useQuery } from "@tanstack/react-query";
 
 import { apiService } from "@framework/ApiService";
 import { ModuleFCProps } from "@framework/Module";
