0c059bded17e489fd96fd0e4db67945dd68c9588
Hans Kallekleiv <16436291+HansKallekleiv@users.noreply.github.com>
2023-03-16 17:03:02 +0100

    sensitivties

--- backend/src/fastapi_app/routers/parameters/router.py
+++ backend/src/fastapi_app/routers/parameters/router.py
@@ -1,10 +1,11 @@
+import datetime
 import logging
 from typing import List, Optional, Sequence, Union
 
 from pydantic import BaseModel
 from fastapi import APIRouter, Depends, HTTPException, Query
 
-from ....services.sumo_access.parameter_access import ParameterAccess, EnsembleParameter
+from ....services.sumo_access.parameter_access import ParameterAccess, EnsembleParameter, EnsembleSensitivity
 from ....services.utils.authenticated_user import AuthenticatedUser
 from ....services.utils.perf_timer import PerfTimer
 from ...auth.auth_helper import AuthHelper
@@ -30,3 +31,34 @@ async def get_parameters(
     if exclude_all_values_constant:
         parameters = [p for p in parameters if not len(set(p.values)) == 1]
     return parameters
+
+
+@router.get("/is_sensitivity_run/")
+async def is_sensitivity_run(
+    # fmt:off
+    authenticated_user: AuthenticatedUser = Depends(AuthHelper.get_authenticated_user),
+    case_uuid: str = Query(description="Sumo case uuid"),
+    ensemble_name: str = Query(description="Ensemble name"),
+    # fmt:on
+) -> bool:
+    """Get all parameters in a given Sumo ensemble"""
+
+    access = ParameterAccess(authenticated_user.get_sumo_access_token(), case_uuid, ensemble_name)
+    return access.is_sensitivity_run()
+
+
+@router.get("/sensitivities/")
+async def get_sensitivities(
+    # fmt:off
+    authenticated_user: AuthenticatedUser = Depends(AuthHelper.get_authenticated_user),
+    case_uuid: str = Query(description="Sumo case uuid"),
+    ensemble_name: str = Query(description="Ensemble name"),
+    # fmt:on
+) -> List[EnsembleSensitivity]:
+    """Get sensitivities in a given Sumo ensemble"""
+
+    access = ParameterAccess(authenticated_user.get_sumo_access_token(), case_uuid, ensemble_name)
+    if not access.is_sensitivity_run():
+        return []
+    sensitivities = access.get_sensitivities()
+    return sensitivities
--- backend/src/services/sumo_access/parameter_access.py
+++ backend/src/services/sumo_access/parameter_access.py
@@ -1,8 +1,15 @@
-from typing import List, Optional, Union, Dict
+from enum import Enum
+from io import BytesIO
+from typing import List, Optional, Sequence, Union, Dict
 import logging
 
+import numpy as np
 from pydantic import BaseModel
+import pandas as pd
 from fmu.sumo.explorer.explorer import CaseCollection, SumoClient
+from fmu.sumo.explorer.objects import TableCollection
+
+# from fmu.sumo.explorer.objects.table import AggregatedTable
 
 from ..utils.perf_timer import PerfTimer
 from ._helpers import create_sumo_client_instance
@@ -12,10 +19,32 @@ LOGGER = logging.getLogger(__name__)
 
 class EnsembleParameter(BaseModel):
     name: str
+    values: List[Union[str, int, float]]
+    realizations: List[int]
+
+
+class EnsembleParameter(BaseModel):
+    name: str
     group_name: Optional[str]
     descriptive_name: Optional[str]
     realizations: List[int]
     values: List[Union[str, int, float]]
+
+
+class EnsembleSensitivityCase(BaseModel):
+    name: str
+    realizations: List[int]
+
+
+class EnsembleSensitivity(BaseModel):
+    name: str
+    type: str
+    cases: List[EnsembleSensitivityCase]
+
+
+class SENSTYPE(str, Enum):
+    MONTECARLO = "montecarlo"
+    SCALAR = "scalar"
 
 
 class ParameterAccess:
@@ -75,3 +104,36 @@ class ParameterAccess:
                 ensemble_parameters.append(ensemble_parameter)
 
         return ensemble_parameters
+
+    def is_sensitivity_run(self) -> List[EnsembleSensitivity]:
+        """Check if the current ensemble is a sensitivity run"""
+        parameters = self.get_parameters()
+        return any([parameter.name == "SENSNAME" for parameter in parameters])
+
+    def get_sensitivities(self) -> List[EnsembleSensitivity]:
+        parameters = self.get_parameters()
+        sensname_parameter = next(parameter for parameter in parameters if parameter.name == "SENSNAME")
+        senscase_parameter = next(parameter for parameter in parameters if parameter.name == "SENSCASE")
+        dframe_sensitivity = pd.DataFrame(
+            columns=["Sensitivity", "Realization"], data=zip(sensname_parameter.values, sensname_parameter.realizations)
+        )
+        dframe_case = pd.DataFrame(
+            columns=["Case", "Realization"], data=zip(senscase_parameter.values, senscase_parameter.realizations)
+        )
+        dframe = pd.merge(dframe_sensitivity, dframe_case, on="Realization")
+        dframe["type"] = dframe.apply(
+            lambda row: SENSTYPE.MONTECARLO if row["Case"] == "p10_p90" else SENSTYPE.SCALAR, axis=1
+        )
+        ensemble_sensitivities = []
+        for sensitivity, sensitivity_df in dframe.groupby("Sensitivity"):
+            ensemble_sensitivities.append(
+                EnsembleSensitivity(
+                    name=sensitivity,
+                    type=sensitivity_df["type"].iloc[0],
+                    cases=[
+                        EnsembleSensitivityCase(name=case, realizations=case_df["Realization"].tolist())
+                        for case, case_df in sensitivity_df.groupby("Case")
+                    ],
+                )
+            )
+        return ensemble_sensitivities
