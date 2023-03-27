cbb25b63224358c8d4512bc5e94c69d966d39887
Sigurd Pettersen <sigurd.pettersen@ceetronsolutions.com>
2023-03-27 16:05:25 +0200

    Rename and export SelectOption and DropdownOption

--- frontend/src/lib/components/Dropdown/index.ts
+++ frontend/src/lib/components/Dropdown/index.ts
@@ -1,1 +1,2 @@
 export { Dropdown } from "./dropdown";
+export type { DropdownOption } from "./dropdown";
--- frontend/src/lib/components/Select/index.ts
+++ frontend/src/lib/components/Select/index.ts
@@ -1,1 +1,2 @@
 export { Select } from "./select";
+export type { SelectOption } from "./select";
