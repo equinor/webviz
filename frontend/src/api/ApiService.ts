ec91356d84fd04495d4ae32d85e7463ffd145b6c
Hans Kallekleiv <16436291+HansKallekleiv@users.noreply.github.com>
2023-04-24 13:40:04 +0200

    Regenerate api

--- frontend/src/api/ApiService.ts
+++ frontend/src/api/ApiService.ts
@@ -8,6 +8,7 @@ import { AxiosHttpRequest } from './core/AxiosHttpRequest';
 import { DefaultService } from './services/DefaultService';
 import { ExploreService } from './services/ExploreService';
 import { InplaceVolumetricsService } from './services/InplaceVolumetricsService';
+import { ParametersService } from './services/ParametersService';
 import { SurfaceService } from './services/SurfaceService';
 import { TimeseriesService } from './services/TimeseriesService';
 
@@ -18,6 +19,7 @@ export class ApiService {
     public readonly default: DefaultService;
     public readonly explore: ExploreService;
     public readonly inplaceVolumetrics: InplaceVolumetricsService;
+    public readonly parameters: ParametersService;
     public readonly surface: SurfaceService;
     public readonly timeseries: TimeseriesService;
 
@@ -39,6 +41,7 @@ export class ApiService {
         this.default = new DefaultService(this.request);
         this.explore = new ExploreService(this.request);
         this.inplaceVolumetrics = new InplaceVolumetricsService(this.request);
+        this.parameters = new ParametersService(this.request);
         this.surface = new SurfaceService(this.request);
         this.timeseries = new TimeseriesService(this.request);
     }
