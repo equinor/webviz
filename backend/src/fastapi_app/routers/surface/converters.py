94c1779143e9f91518776821f50a24184eca7b08
Hans Kallekleiv <16436291+HansKallekleiv@users.noreply.github.com>
2023-04-26 18:02:47 +0200

    black

--- backend/src/fastapi_app/routers/surface/converters.py
+++ backend/src/fastapi_app/routers/surface/converters.py
@@ -4,6 +4,7 @@ import xtgeo
 from src.services.utils.surface_to_float32 import surface_to_float32_array
 from . import schemas
 
+
 def to_api_surface_data(xtgeo_surf: xtgeo.RegularSurface) -> schemas.SurfaceData:
     """
     Create API SurfaceData from xtgeo regular surface
@@ -20,5 +21,5 @@ def to_api_surface_data(xtgeo_surf: xtgeo.RegularSurface) -> schemas.SurfaceData
         val_min=xtgeo_surf.values.min(),
         val_max=xtgeo_surf.values.max(),
         rot_deg=xtgeo_surf.rotation,
-        mesh_data=orjson.dumps(float32values)
+        mesh_data=orjson.dumps(float32values),
     )
