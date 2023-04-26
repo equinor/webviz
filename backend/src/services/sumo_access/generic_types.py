5ab80c24afe0d88642b8a9b2a07f84dc2b646b46
Hans Kallekleiv <16436291+HansKallekleiv@users.noreply.github.com>
2023-04-26 11:38:25 +0200

    black

--- backend/src/services/sumo_access/generic_types.py
+++ backend/src/services/sumo_access/generic_types.py
@@ -5,9 +5,11 @@ from pydantic import BaseModel
 
 class EnsembleScalarResponse(BaseModel):
     """ """
+
     realizations: List[int]
     values: List[float]
 
+
 class EnsembleCorrelations(BaseModel):
     names: List[str]
-    values:List[float]
+    values: List[float]
