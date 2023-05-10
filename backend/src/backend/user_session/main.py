53f28641992776d152fee6d43ed3da1a1a8d1812
Anders Fredrik Kiær <31612826+anders-kiaer@users.noreply.github.com>
2023-05-10 10:44:17 +0200

    Use `orjson` as default encoder (#123)

--- backend/src/backend/user_session/main.py
+++ backend/src/backend/user_session/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
 
 from src.backend.shared_middleware import add_shared_middlewares
 from .inactivity_shutdown import InactivityShutdown
 from .routers.general import router as general_router
 
-app = FastAPI()
+app = FastAPI(default_response_class=ORJSONResponse)
 
 app.include_router(general_router)
 
