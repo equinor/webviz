eab99ceab258acc08dabed061bc9d132fb363abb
Anders Fredrik Kiær <31612826+anders-kiaer@users.noreply.github.com>
2023-05-16 14:55:17 +0200

    Enable React plugin in `eslint` - disallow destructuring of props (#138)

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,12 +1,14 @@
 module.exports = {
     env: {
-        node: true,
+        browser: true,
+        es2021: true
     },
     extends: ["eslint:recommended", "plugin:react/recommended", "plugin:@typescript-eslint/recommended"],
     rules: {
         "@typescript-eslint/no-explicit-any": "off",
         "react/jsx-uses-react": "off", // Import of React is not required anymore in React 17
         "react/react-in-jsx-scope": "off", // Import of React is not required anymore in React 17
+        "react/destructuring-assignment": ["error", "never"],
     },
     parser: "@typescript-eslint/parser",
     parserOptions: {
