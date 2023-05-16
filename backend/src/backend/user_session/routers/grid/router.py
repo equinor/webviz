d5391befb6ef019f8e443b8a367936b7bb93b110
Hans Kallekleiv <16436291+HansKallekleiv@users.noreply.github.com>
2023-05-16 11:07:55 +0200

    lint

--- backend/src/backend/user_session/routers/grid/router.py
+++ backend/src/backend/user_session/routers/grid/router.py
@@ -1,3 +1,4 @@
+# type: ignore
 # for now
 from functools import cache
 from typing import List, Tuple
@@ -63,7 +64,11 @@ async def grid_surface(
     grid_surface = get_grid_surface(grid_geometry=xtgeo_grid)
 
     # Extract points and polygons from surface
-    points_np = vtk_to_numpy(grid_surface.polydata.GetPoints().GetData()).ravel().astype(np.float32)
+    points_np = (
+        vtk_to_numpy(grid_surface.polydata.GetPoints().GetData())
+        .ravel()
+        .astype(np.float32)
+    )
     polys_np = vtk_to_numpy(grid_surface.polydata.GetPolys().GetData()).astype(np.int64)
 
     # Reduce precision of points to 2 decimals
@@ -114,7 +119,9 @@ async def grid_parameter(
     )
 
     # Get scalar values from parameter
-    scalar_values = get_scalar_values(xtgeo_parameter, cell_ids=grid_polydata.original_cell_ids)
+    scalar_values = get_scalar_values(
+        xtgeo_parameter, cell_ids=grid_polydata.original_cell_ids
+    )
 
     # Handle xtgeo undefined values and truncate
     scalar_values[scalar_values == -999.0] = np.nan
@@ -183,7 +190,9 @@ async def grid_parameter(
     )
 
     # Generate intersection data
-    coords, triangles, original_cell_indices_np, polyline = generate_grid_intersection(xtgeo_grid, xyz_arr)
+    coords, triangles, original_cell_indices_np, polyline = generate_grid_intersection(
+        xtgeo_grid, xyz_arr
+    )
     print(
         f"CALCULATED INTERSECTION: realization: {realization}: {round(timer.lap_s(),2)}s",
         flush=True,
@@ -204,7 +213,9 @@ async def grid_parameter(
     # scalar_values[scalar_values == -999.0] = 0
 
     # Get polyline coordinates
-    polyline_coords = np.array([polyline.GetPoint(i)[:3] for i in range(polyline.GetNumberOfPoints())])
+    polyline_coords = np.array(
+        [polyline.GetPoint(i)[:3] for i in range(polyline.GetNumberOfPoints())]
+    )
 
     # Calculate the cumulative distance along the polyline
     polyline_distances = np.zeros(polyline_coords.shape[0])
@@ -217,7 +228,9 @@ async def grid_parameter(
     polyline_y = polyline_coords[:, 2]
 
     # Visualize the intersection using matplotlib as a base64 encoded image
-    image_data = visualize_triangles_with_scalars(coords, triangles, values, polyline, "55/33-A-4")
+    image_data = visualize_triangles_with_scalars(
+        coords, triangles, values, polyline, "55/33-A-4"
+    )
     print(
         f"MATPLOTLIB IMAGE: realization: {realization}: {round(timer.lap_s(),2)}s",
         flush=True,
@@ -265,7 +278,9 @@ async def grid_parameter(
     # convert json string of realizations into list
     realizations = orjson.loads(request.query_params.get("realizations"))
 
-    grid_access = GridAccess(authenticated_user.get_sumo_access_token(), case_uuid, ensemble_name)
+    grid_access = GridAccess(
+        authenticated_user.get_sumo_access_token(), case_uuid, ensemble_name
+    )
 
     # Check that all grids have equal nx, ny, nz
     # Should raise a http exception instead of a value error
@@ -328,7 +343,9 @@ async def grid_parameter(
     print("GENERATING GRID INTERSECTION", flush=True)
 
     # Generate intersection data
-    coords, triangles, original_cell_indices_np, polyline = generate_grid_intersection(xtgeo_grid, xyz_arr)
+    coords, triangles, original_cell_indices_np, polyline = generate_grid_intersection(
+        xtgeo_grid, xyz_arr
+    )
     print(
         f"CALCULATED INTERSECTION: realization: {0}: {round(timer.lap_s(),2)}s",
         flush=True,
@@ -337,7 +354,8 @@ async def grid_parameter(
 
     # Get scalar values for each realization
     all_scalar_values = [
-        get_scalar_values(xtgeo_parameter, cell_ids=original_cell_indices_np) for xtgeo_parameter in xtgeo_parameters
+        get_scalar_values(xtgeo_parameter, cell_ids=original_cell_indices_np)
+        for xtgeo_parameter in xtgeo_parameters
     ]
 
     # Calculate the mean scalar value for each cell
@@ -353,7 +371,9 @@ async def grid_parameter(
     )
 
     # Get polyline coordinates
-    polyline_coords = np.array([polyline.GetPoint(i)[:3] for i in range(polyline.GetNumberOfPoints())])
+    polyline_coords = np.array(
+        [polyline.GetPoint(i)[:3] for i in range(polyline.GetNumberOfPoints())]
+    )
 
     # Calculate the cumulative distance along the polyline
     polyline_distances = np.zeros(polyline_coords.shape[0])
@@ -369,7 +389,9 @@ async def grid_parameter(
     print("GENERATE MATPLOTLIB IMAGE", flush=True)
 
     # Visualize the intersection using matplotlib as a base64 encoded image
-    image_data = visualize_triangles_with_scalars(coords, triangles, values, polyline, "55/33-A-4")
+    image_data = visualize_triangles_with_scalars(
+        coords, triangles, values, polyline, "55/33-A-4"
+    )
     print(
         f"GENERATED MATPLOTLIB IMAGE: {parameter_name}, realization: {0}: {round(timer.lap_s(),2)}s",
         flush=True,
@@ -417,7 +439,9 @@ async def statistical_grid_parameter(
     # convert json string of realizations into list
     realizations = orjson.loads(request.query_params.get("realizations"))
 
-    grid_access = GridAccess(authenticated_user.get_sumo_access_token(), case_uuid, ensemble_name)
+    grid_access = GridAccess(
+        authenticated_user.get_sumo_access_token(), case_uuid, ensemble_name
+    )
 
     # Check that all grids have equal nx, ny, nz
     # Should riase a http exception instead of a value error
@@ -455,12 +479,18 @@ async def statistical_grid_parameter(
     ]
 
     # Calculate the mean scalar values for each cell
-    mean_scalar_values = np.nanmean([scalar_values for scalar_values in all_scalar_values], axis=0)
+    mean_scalar_values = np.nanmean(
+        [scalar_values for scalar_values in all_scalar_values], axis=0
+    )
 
     # Handle xtgeo undefined values and truncate
     mean_scalar_values[mean_scalar_values == -999.0] = np.nan
-    mean_scalar_values[mean_scalar_values < np.nanmin(mean_scalar_values)] = np.nanmin(mean_scalar_values)
-    mean_scalar_values[mean_scalar_values > np.nanmax(mean_scalar_values)] = np.nanmax(mean_scalar_values)
+    mean_scalar_values[mean_scalar_values < np.nanmin(mean_scalar_values)] = np.nanmin(
+        mean_scalar_values
+    )
+    mean_scalar_values[mean_scalar_values > np.nanmax(mean_scalar_values)] = np.nanmax(
+        mean_scalar_values
+    )
 
     return ORJSONResponse(mean_scalar_values.tolist())
 
@@ -500,7 +530,9 @@ def get_grid_parameter(
     token = authenticated_user.get_sumo_access_token()
     grid_access = GridAccess(token, case_uuid, ensemble_name)
 
-    grid_parameter = grid_access.get_grid_parameter(grid_name, parameter_name, int(realization))
+    grid_parameter = grid_access.get_grid_parameter(
+        grid_name, parameter_name, int(realization)
+    )
 
     return grid_parameter
 
@@ -515,14 +547,22 @@ def generate_grid_intersection(grid_geometry: xtgeo.Grid, xyz_arr: Tuple[List[fl
     esgrid = xtgeo_grid_to_vtk_explicit_structured_grid(grid_geometry)
     sliced_grid = cut_along_polyline(esgrid, poly_xy)
     original_cell_indices_np = [
-        int(c) for c in vtk_to_numpy(sliced_grid.GetCellData().GetAbstractArray("vtkOriginalCellIds"))
+        int(c)
+        for c in vtk_to_numpy(
+            sliced_grid.GetCellData().GetAbstractArray("vtkOriginalCellIds")
+        )
     ]
 
-    flattened_grid = flatten_sliced_grid(sliced_grid, polyline, original_cell_ids=original_cell_indices_np)
+    flattened_grid = flatten_sliced_grid(
+        sliced_grid, polyline, original_cell_ids=original_cell_indices_np
+    )
     coords = grid_to_numpy(flattened_grid)
     triangles = get_triangles(flattened_grid)
     original_cell_indices_np = [
-        int(c) for c in vtk_to_numpy(flattened_grid.GetCellData().GetAbstractArray("vtkOriginalCellIds"))
+        int(c)
+        for c in vtk_to_numpy(
+            flattened_grid.GetCellData().GetAbstractArray("vtkOriginalCellIds")
+        )
     ]
 
     return (coords, triangles, original_cell_indices_np, polyline)
