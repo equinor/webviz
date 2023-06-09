829400cf686b7beb4d9ed320e7e2dd87651a41ac
Hans Kallekleiv <16436291+HansKallekleiv@users.noreply.github.com>
2023-06-09 14:47:38 +0200

    revert

--- frontend/src/modules/Sensitivity/sensitivityChart.tsx
+++ frontend/src/modules/Sensitivity/sensitivityChart.tsx
@@ -25,13 +25,6 @@ type SelectedBar = {
     index: number
 }
 
-// Modify the Plotly layout interface to allow use of domain in axis reference
-interface ModifiedPlotlyLayout extends Omit<Partial<Layout>, 'shapes'> {
-    shapes: Partial<ModifiedPlotlyShape>[];
-}
-interface ModifiedPlotlyShape extends Omit<Partial<Shape>, "yref"> {
-    yref: 'paper' | YAxisName | 'y domain';
-}
 
 const lowTrace = (sensitivityResponses: SensitivityResponse[], showLabels: boolean, selectedBar: SelectedBar | null, barColor = "e53935"): Partial<PlotData> => {
     const label = showLabels ? sensitivityResponses.map(s =>
@@ -124,7 +117,7 @@ const sensitivityChart: React.FC<sensitivityChartProps> = (props) => {
             props.onSelectedSensitivity(selectedSensitivity);
         }
     };
-    const layout: Partial<ModifiedPlotlyLayout> = {
+    const layout: Partial<Layout> = {
         width,
         height,
         margin: { t: 100, r: 0, b: 100, l: 100 },
@@ -179,7 +172,8 @@ const sensitivityChart: React.FC<sensitivityChartProps> = (props) => {
                 "x1": 0, //if it is not true base else use the reference average, TODO
                 "y0": 0,
                 "y1": 1,
-                "xref": "x",   
+                "xref": "x",
+                //@ts-ignore
                 "yref": "y domain",
             }
         ],
