f85fb2de13a3e8000be3e8d3cbc096065bea9b99
Sigurd Pettersen <sigurd.pettersen@ceetronsolutions.com>
2023-06-18 23:23:52 +0200

    Trying to use SelectProps

--- frontend/src/framework/components/MultiEnsembleSelect/multiEnsembleSelect.tsx
+++ frontend/src/framework/components/MultiEnsembleSelect/multiEnsembleSelect.tsx
@@ -1,4 +1,4 @@
-import { Select, SelectOption } from "@lib/components/Select";
+import { Select, SelectProps, SelectOption } from "@lib/components/Select";
 
 import { EnsembleIdent } from "../../EnsembleIdent";
 import { EnsembleSet } from "../../EnsembleSet";
@@ -7,31 +7,32 @@ type MultiEnsembleSelectProps = {
     ensembleSet: EnsembleSet;
     value: EnsembleIdent[];
     onChange: (ensembleIdentArr: EnsembleIdent[]) => void;
-    size?: number;
-};
+} & Omit<SelectProps, "options" | "value" | "onChange" | "multiple">;
 
 export function MultiEnsembleSelect(props: MultiEnsembleSelectProps): JSX.Element {
+    const {ensembleSet, value, onChange, ...rest} = props;
+
     function handleSelectionChanged(selectedEnsembleIdentStrArr: string[]) {
         const identArr: EnsembleIdent[] = [];
         for (const identStr of selectedEnsembleIdentStrArr) {
-            const foundEnsemble = props.ensembleSet.findEnsembleByIdentString(identStr);
+            const foundEnsemble = ensembleSet.findEnsembleByIdentString(identStr);
             if (foundEnsemble) {
                 identArr.push(foundEnsemble.getIdent());
             }
         }
 
-        props.onChange(identArr);
+        onChange(identArr);
     }
 
     const optionsArr: SelectOption[] = [];
-    for (const ens of props.ensembleSet.getEnsembleArr()) {
+    for (const ens of ensembleSet.getEnsembleArr()) {
         optionsArr.push({ value: ens.getIdent().toString(), label: ens.getDisplayName() });
     }
 
     const selectedArr: string[] = [];
-    for (const ident of props.value) {
+    for (const ident of value) {
         selectedArr.push(ident.toString());
     }
 
-    return <Select options={optionsArr} value={selectedArr} onChange={handleSelectionChanged} size={props.size} />;
+    return <Select options={optionsArr} value={selectedArr} onChange={handleSelectionChanged}  {...rest} />;
 }
