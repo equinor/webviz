ef1cf89a0522dde8015fbabfcfb739a0581098ed
Sigurd Pettersen <sigurd.pettersen@ceetronsolutions.com>
2023-06-18 23:24:15 +0200

    Trying to use DropdownProps

--- frontend/src/framework/components/SingleEnsembleSelect/singleEnsembleSelect.tsx
+++ frontend/src/framework/components/SingleEnsembleSelect/singleEnsembleSelect.tsx
@@ -1,4 +1,4 @@
-import { Dropdown, DropdownOption } from "@lib/components/Dropdown";
+import { Dropdown, DropdownOption, DropdownProps } from "@lib/components/Dropdown";
 
 import { EnsembleIdent } from "../../EnsembleIdent";
 import { EnsembleSet } from "../../EnsembleSet";
@@ -7,18 +7,20 @@ type SingleEnsembleSelectProps = {
     ensembleSet: EnsembleSet;
     value: EnsembleIdent | null;
     onChange: (ensembleIdent: EnsembleIdent | null) => void;
-};
+}& Omit<DropdownProps, "options" | "value" | "onChange">;
 
 export function SingleEnsembleSelect(props: SingleEnsembleSelectProps): JSX.Element {
+    const { ensembleSet, value, onChange, ...rest } = props;
+
     function handleSelectionChanged(selectedEnsembleIdentStr: string) {
-        const foundEnsemble = props.ensembleSet.findEnsembleByIdentString(selectedEnsembleIdentStr);
-        props.onChange(foundEnsemble ? foundEnsemble.getIdent() : null);
+        const foundEnsemble = ensembleSet.findEnsembleByIdentString(selectedEnsembleIdentStr);
+        onChange(foundEnsemble ? foundEnsemble.getIdent() : null);
     }
 
     const optionsArr: DropdownOption[] = [];
-    for (const ens of props.ensembleSet.getEnsembleArr()) {
+    for (const ens of ensembleSet.getEnsembleArr()) {
         optionsArr.push({ value: ens.getIdent().toString(), label: ens.getDisplayName() });
     }
 
-    return <Dropdown options={optionsArr} value={props.value?.toString()} onChange={handleSelectionChanged} />;
+    return <Dropdown options={optionsArr} value={value?.toString()} onChange={handleSelectionChanged} {...rest}/>;
 }
