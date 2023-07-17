4527fa886b71463e73e4768cc6ebe359879dbe5f
Ruben Thoms <ruben.thoms@ceetronsolutions.com>
2023-07-17 15:35:44 +0200

    Further improvements

--- frontend/src/lib/components/Plot/plot.tsx
+++ frontend/src/lib/components/Plot/plot.tsx
@@ -1,127 +1,106 @@
 import React from "react";
 import PlotlyPlot, { Figure, PlotParams } from "react-plotly.js";
 
-import { cloneDeep, differenceWith, isEqual, toPairs } from "lodash";
+import { cloneDeep, isEqual } from "lodash";
 import Plotly from "plotly.js";
 
-function applyChanges(old: any, changes: any) {
-    if (typeof old === "object") {
-        if (Array.isArray(old)) {
-            applyChangesToArray(old, changes);
-        } else {
-            applyChangesToObject(old, changes);
-        }
-    } else {
-        old = changes;
-    }
-}
-
-function applyChangesToArray(array: Array<any>, changes: Array<any>) {
-    for (const change of changes) {
-        const index = array.findIndex((item) => isEqual(item, change));
-        if (index === -1) {
-            array.push(change);
-        } else {
-            const value = change;
-            applyChanges(array[index], value);
-        }
-    }
-}
-
-function applyChangesToObject(obj: Record<string, any>, changes: Record<string, any>) {
-    for (const key in changes) {
-        if (!(key in obj)) {
-            obj[key] = changes[key];
-        } else {
-            const value = changes[key];
-            applyChanges(obj[key], value);
-        }
-    }
-}
-
 export const Plot: React.FC<PlotParams> = (props) => {
-    const { data: propsData, layout: propsLayout, frames: propsFrames, ...rest } = props;
+    const { data: propsData, layout: propsLayout, frames: propsFrames, onHover: propsOnHover, ...rest } = props;
+
+    const hoverTimeout = React.useRef<ReturnType<typeof setTimeout> | null>(null);
 
     const [data, setData] = React.useState<Plotly.Data[]>(propsData);
     const [layout, setLayout] = React.useState<Partial<Plotly.Layout>>(propsLayout);
     const [frames, setFrames] = React.useState<Plotly.Frame[] | null>(propsFrames || null);
-
-    const [ranges, setRanges] = React.useState<{
-        xMin?: Plotly.Datum;
-        xMax?: Plotly.Datum;
-        yMin?: Plotly.Datum;
-        yMax?: Plotly.Datum;
-    }>({
-        xMin: 0,
-        xMax: 0,
-        yMin: 0,
-        yMax: 0,
-    });
-
     const [prevData, setPrevData] = React.useState<Plotly.Data[]>(propsData);
     const [prevLayout, setPrevLayout] = React.useState<Partial<Plotly.Layout>>(propsLayout);
     const [prevFrames, setPrevFrames] = React.useState<Plotly.Frame[] | null>(propsFrames || null);
 
     React.useEffect(() => {
-        if (isEqual(prevData, propsData)) return;
-        console.info("Plot: data changed");
-        console.info(differenceWith(propsData, prevData, isEqual));
+        return () => {
+            if (hoverTimeout.current) {
+                clearTimeout(hoverTimeout.current);
+            }
+        };
+    }, []);
+
+    if (!isEqual(propsData, prevData)) {
         setData(cloneDeep(propsData));
         setPrevData(cloneDeep(propsData));
-    }, [propsData, prevData]);
+    }
 
-    React.useEffect(() => {
-        if (isEqual(prevLayout, propsLayout)) return;
-        setLayout(cloneDeep(propsLayout));
+    if (!isEqual(prevLayout, propsLayout)) {
+        setLayout({ ...layout, ...cloneDeep(propsLayout) });
         setPrevLayout(cloneDeep(propsLayout));
-    }, [propsLayout, prevLayout]);
+    }
 
-    React.useEffect(() => {
-        if (isEqual(prevFrames, propsFrames)) return;
-        console.info("Plot: frames changed");
+    if (!isEqual(prevFrames, propsFrames || null)) {
         setFrames(cloneDeep(propsFrames || null));
         setPrevFrames(cloneDeep(propsFrames || null));
-    }, [propsFrames, prevFrames]);
+    }
 
-    const handleInitialized = (figure: Figure) => {
+    function handleInitialized(figure: Figure) {
         setLayout(figure.layout);
         setData(figure.data);
         setFrames(figure.frames || null);
-    };
+    }
 
-    const handleUpdate = (figure: Figure) => {
+    function handleUpdate(figure: Figure) {
         if (!isEqual(layout, figure.layout)) {
             setLayout(figure.layout);
-            console.info("Plot: layout updated");
         }
         if (!isEqual(data, figure.data)) {
             setData(figure.data);
-            console.info("Plot: data updated");
         }
         if (!isEqual(frames, figure.frames)) {
             setFrames(figure.frames || null);
-            console.info("Plot: frames updated");
         }
-    };
+    }
 
-    const handleRelayout = (e: Plotly.PlotRelayoutEvent) => {
-        setRanges({
-            xMin: e["xaxis.range[0]"],
-            xMax: e["xaxis.range[1]"],
-            yMin: e["yaxis.range[0]"],
-            yMax: e["yaxis.range[1]"],
+    function handleRelayout(e: Plotly.PlotRelayoutEvent) {
+        setLayout({
+            ...layout,
+            xaxis: {
+                ...layout.xaxis,
+                range: [e["xaxis.range[0]"], e["xaxis.range[1]"]],
+                autorange: e["xaxis.autorange"],
+            },
+            yaxis: {
+                ...layout.yaxis,
+                range: [e["yaxis.range[0]"], e["yaxis.range[1]"]],
+                autorange: e["yaxis.autorange"],
+            },
         });
-    };
+    }
+
+    function handleHover(event: Readonly<Plotly.PlotHoverEvent>) {
+        if (propsOnHover) {
+            if (hoverTimeout.current) {
+                clearTimeout(hoverTimeout.current);
+            }
+            hoverTimeout.current = setTimeout(() => {
+                if (propsOnHover) {
+                    propsOnHover(event);
+                }
+            }, 180);
+        }
+    }
 
     return (
         <PlotlyPlot
             {...rest}
+            config={{
+                modeBarButtons: [["pan2d", "autoScale2d", "zoomIn2d", "zoomOut2d", "toImage"]],
+                displaylogo: false,
+                ...props.config,
+            }}
             data={data}
-            layout={{ ...layout, ...ranges }}
+            layout={layout}
             frames={frames || undefined}
             onInitialized={handleInitialized}
             onUpdate={handleUpdate}
             onRelayout={handleRelayout}
+            onHover={handleHover}
         />
     );
 };
