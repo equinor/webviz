90391825ead0cd62a81470747f8dd3d4d1fb2aa8
Ruben Thoms <ruben.thoms@ceetronsolutions.com>
2023-07-17 15:53:55 +0200

    Disabling dialog buttons when no changes

--- frontend/src/framework/internal/components/SelectEnsemblesDialog/selectEnsemblesDialog.tsx
+++ frontend/src/framework/internal/components/SelectEnsemblesDialog/selectEnsemblesDialog.tsx
@@ -135,6 +135,10 @@ export const SelectEnsemblesDialog: React.FC<SelectEnsemblesDialogProps> = (prop
         props.onClose(newlySelectedEnsembles);
     }
 
+    function checkIfAnyChanges(): boolean {
+        return !isEqual(props.selectedEnsembles, newlySelectedEnsembles);
+    }
+
     const fieldOpts = fieldsQuery.data?.map((f) => ({ value: f.field_identifier, label: f.field_identifier })) ?? [];
     const caseOpts = casesQuery.data?.map((c) => ({ value: c.uuid, label: c.name })) ?? [];
     const ensembleOpts =
@@ -152,10 +156,12 @@ export const SelectEnsemblesDialog: React.FC<SelectEnsemblesDialogProps> = (prop
                 width={"75%"}
                 actions={
                     <div className="flex gap-4">
-                        <Button onClick={handleClose} color="danger">
+                        <Button onClick={handleClose} color="danger" disabled={!checkIfAnyChanges()}>
                             Discard changes
                         </Button>
-                        <Button onClick={handleApplyEnsembleSelection}>Apply changes</Button>
+                        <Button onClick={handleApplyEnsembleSelection} disabled={!checkIfAnyChanges()}>
+                            Apply changes
+                        </Button>
                     </div>
                 }
             >
