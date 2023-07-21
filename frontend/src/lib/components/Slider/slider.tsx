6536a477c9572d2bd6213aecaf0f3ec5eb07bc11
Ruben Thoms <ruben.thoms@ceetronsolutions.com>
2023-07-21 11:33:54 +0200

    Improved slider value label

--- frontend/src/lib/components/Slider/slider.tsx
+++ frontend/src/lib/components/Slider/slider.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
+import { Point } from "@lib/utils/geometry";
 import { Slider as SliderUnstyled, SliderProps as SliderUnstyledProps } from "@mui/base";
 
 import { Input } from "../Input";
@@ -16,26 +18,96 @@ export const Slider = React.forwardRef((props: SliderProps, ref: React.Forwarded
     const [value, setValue] = React.useState<number | number[]>(propsValue ?? 0);
     const [prevValue, setPrevValue] = React.useState<number | number[]>(propsValue ?? 0);
     const [valueLabelVisible, setValueLabelVisible] = React.useState<boolean>(false);
+    const [valueLabelPosition, setValueLabelPosition] = React.useState<Point>({ x: 0, y: 0 });
+
+    const divRef = React.useRef<HTMLDivElement>(null);
+    const valueLabelRef = React.useRef<HTMLDivElement>(null);
 
     if (propsValue !== undefined && propsValue !== prevValue) {
         setValue(propsValue);
         setPrevValue(propsValue);
     }
 
-    const handleValueChanged = (event: Event, value: number | number[], activeThumb: number) => {
+    React.useEffect(() => {
+        let pointerPressed = false;
+        let hovered = false;
+        if (divRef.current) {
+            const elements = divRef.current.getElementsByClassName("MuiSlider-thumb");
+            if (elements.length === 1) {
+                const thumb = elements[0];
+
+                function handlePointerOver() {
+                    setValueLabelVisible(true);
+                    hovered = true;
+                    if (divRef.current) {
+                        const elements = divRef.current.getElementsByClassName("MuiSlider-thumb");
+                        if (elements.length === 1) {
+                            const thumb = elements[0];
+                            setValueLabelPosition({
+                                x: thumb.getBoundingClientRect().left + thumb.getBoundingClientRect().width / 2 + 1,
+                                y: thumb.getBoundingClientRect().top + thumb.getBoundingClientRect().height / 2 + 1,
+                            });
+                        }
+                    }
+                }
+
+                function handlePointerOut() {
+                    hovered = false;
+                    if (pointerPressed) {
+                        return;
+                    }
+                    setValueLabelVisible(false);
+                }
+
+                function handlePointerDown() {
+                    pointerPressed = true;
+                }
+
+                function handlePointerUp() {
+                    pointerPressed = false;
+                    if (hovered) {
+                        return;
+                    }
+                    setValueLabelVisible(false);
+                }
+
+                thumb.addEventListener("pointerover", handlePointerOver);
+                thumb.addEventListener("pointerout", handlePointerOut);
+                thumb.addEventListener("pointerdown", handlePointerDown);
+                document.addEventListener("pointerup", handlePointerUp);
+                return () => {
+                    thumb.removeEventListener("pointerover", handlePointerOver);
+                    thumb.removeEventListener("pointerout", handlePointerOut);
+                    thumb.removeEventListener("pointerdown", handlePointerDown);
+                    document.removeEventListener("pointerup", handlePointerUp);
+                };
+            }
+        }
+    }, []);
+
+    function handleValueChanged(event: Event, value: number | number[], activeThumb: number) {
         setValue(value);
         props.onChange?.(event, value, activeThumb);
-    };
+
+        if (divRef.current) {
+            const elements = divRef.current.getElementsByClassName("MuiSlider-thumb");
+            if (elements.length === 1) {
+                const thumb = elements[0];
+                setValueLabelPosition({
+                    x: thumb.getBoundingClientRect().left + thumb.getBoundingClientRect().width / 2 + 1,
+                    y: thumb.getBoundingClientRect().top + thumb.getBoundingClientRect().height / 2 + 1,
+                });
+            }
+        }
+    }
 
     return (
         <BaseComponent disabled={props.disabled}>
-            <div className="relative">
+            <div ref={divRef} className="mt-2 mb-2">
                 <SliderUnstyled
                     {...rest}
                     max={max}
                     onChange={handleValueChanged}
-                    onPointerEnter={() => setValueLabelVisible(true)}
-                    onPointerLeave={() => setValueLabelVisible(false)}
                     value={value}
                     ref={ref}
                     slotProps={{
@@ -98,33 +170,51 @@ export const Slider = React.forwardRef((props: SliderProps, ref: React.Forwarded
                         },
                     }}
                 />
-                {displayValue && (
-                    <div
-                        className={resolveClassNames(
-                            "absolute",
-                            "rounded",
-                            "bg-blue-600",
-                            "text-white",
-                            "p-2",
-                            "text-xs",
-                            "font-bold",
-                            "leading-none",
-                            "transform",
-                            "-translate-x-1/2",
-                            "-translate-y-full",
-                            "transition-opacity",
-                            "pointer-events-none",
-                            "top-0",
-                            {
-                                hidden: !valueLabelVisible,
-                            }
-                        )}
-                        style={{ left: `${((value as number) / (max ?? 100)) * 100}%` }}
-                    >
-                        {value}
-                    </div>
-                )}
             </div>
+            {displayValue &&
+                ReactDOM.createPortal(
+                    <div
+                        className="absolute flex justify-center w-40 -ml-20 h-4 -mt-4 pointer-events-none z-40"
+                        ref={valueLabelRef}
+                        style={{ left: valueLabelPosition.x, top: valueLabelPosition.y }}
+                    >
+                        <div
+                            className={resolveClassNames(
+                                "rounded",
+                                "bg-blue-600",
+                                "text-white",
+                                "p-2",
+                                "text-xs",
+                                "font-bold",
+                                "leading-none",
+                                "transform",
+                                "-ml-0.5",
+                                "-translate-y-full",
+                                "transition-opacity",
+                                "pointer-events-none",
+                                "-top-2",
+                                "h-6",
+                                "before:absolute",
+                                "before:-bottom-5",
+                                "before:left-1/2",
+                                "before:transform",
+                                "before:-translate-x-1/2",
+                                "before:-translate-y-full",
+                                "before:w-4",
+                                "before:h-4",
+                                "before:bg-blue-600",
+                                "before:rotate-45",
+                                "before:-z-10",
+                                {
+                                    hidden: !valueLabelVisible,
+                                }
+                            )}
+                        >
+                            {value}
+                        </div>
+                    </div>,
+                    document.body
+                )}
         </BaseComponent>
     );
 });
