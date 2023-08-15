8c8c155efc77ff25a6f235546c3be65256785a02
Ruben <ruben.thoms@ceetronsolutions.com>
2023-08-15 14:30:37 +0200

    Disabling `Sign in` button while signing in and showing progress circle

--- frontend/src/framework/internal/components/LoginDialog/loginDialog.tsx
+++ frontend/src/framework/internal/components/LoginDialog/loginDialog.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import { AuthState, useAuthProvider } from "@framework/internal/providers/AuthProvider";
 import { Button } from "@lib/components/Button";
+import { CircularProgress } from "@lib/components/CircularProgress";
 import { Dialog } from "@lib/components/Dialog";
 
 export const LoginDialog: React.FC = () => {
@@ -11,9 +12,18 @@ export const LoginDialog: React.FC = () => {
         window.location.href = `/api/login?redirect_url_after_login=${btoa("/")}`;
     }
 
-    if (auth.authState === AuthState.NotLoggedIn) {
+    if (auth.authState !== AuthState.LoggedIn) {
         return (
-            <Dialog title="Sign in" open modal actions={<Button onClick={signIn}>Sign in</Button>}>
+            <Dialog
+                title="Sign in"
+                open
+                modal
+                actions={
+                    <Button onClick={signIn} disabled={auth.authState !== AuthState.NotLoggedIn}>
+                        {auth.authState === AuthState.NotLoggedIn ? "Sign in" : <CircularProgress size="medium" />}
+                    </Button>
+                }
+            >
                 You have to sign in in order to use this application.
             </Dialog>
         );
