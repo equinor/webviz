f4d8d13fa1990eebcd8eb61b2b14547c31698032
Ruben <ruben.thoms@ceetronsolutions.com>
2023-08-15 14:38:03 +0200

    Adjusted after merge changes

--- frontend/src/modules/registerAllModules.ts
+++ frontend/src/modules/registerAllModules.ts
@@ -1,6 +1,5 @@
 import { isDevMode } from "@lib/utils/devMode";
 
-import "./DbgWorkbenchSpy/registerModule";
 import "./DistributionPlot/registerModule";
 import "./Grid3D/registerModule";
 import "./Grid3D/registerModule";
@@ -8,8 +7,6 @@ import "./Grid3DIntersection/registerModule";
 import "./Grid3DIntersection/registerModule";
 import "./InplaceVolumetrics/registerModule";
 import "./Map/registerModule";
-import "./MyModule2/registerModule";
-import "./MyModule/registerModule";
 import "./Pvt/registerModule";
 import "./Sensitivity/registerModule";
 import "./SimulationTimeSeries/registerModule";
