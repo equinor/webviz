6e79e7d759cd08c6c0cde5aaceeae1eb7c20c3d3
Ruben <ruben.thoms@ceetronsolutions.com>
2023-08-22 12:51:54 +0200

    Improved hook

--- frontend/src/lib/hooks/useValidState.ts
+++ frontend/src/lib/hooks/useValidState.ts
@@ -1,24 +1,41 @@
 import React from "react";
 
-export function useValidState<T>(
-    initialState: T,
-    validStates: T[],
+export function useValidState<T, K = any>(
+    initialState: T | (() => T),
+    validStates: readonly T[] | [readonly K[], (element: K) => T],
     keepStateWhenInvalid = true
-): [T, (value: T) => void] {
+): [T, (newState: T | ((prevState: T) => T), acceptInvalidState?: boolean) => void] {
     const [state, setState] = React.useState<T>(initialState);
 
-    let adjustedState = state;
+    let validState = state;
+    const computedInitialState = typeof initialState === "function" ? (initialState as () => T)() : initialState;
 
-    if (!validStates.includes(state)) {
+    let adjustedValidStates: T[] = [];
+    if (validStates.length === 2 && Array.isArray(validStates[0]) && typeof validStates[1] === "function") {
+        adjustedValidStates = validStates[0].map(validStates[1] as (element: K) => T);
+    } else {
+        adjustedValidStates = validStates as T[];
+    }
+
+    if (!adjustedValidStates.includes(state)) {
         if (validStates.length > 0) {
-            adjustedState = validStates[0];
+            validState = adjustedValidStates[0];
         } else {
-            adjustedState = initialState;
+            validState = computedInitialState;
         }
         if (!keepStateWhenInvalid) {
-            setState(adjustedState);
+            setState(validState);
         }
     }
 
-    return [adjustedState, setState];
+    function setValidState(newState: T | ((prevState: T) => T), acceptInvalidState = true) {
+        const computedNewState = typeof newState === "function" ? (newState as (prevState: T) => T)(state) : newState;
+        if (!acceptInvalidState && !adjustedValidStates.includes(computedNewState)) {
+            return;
+        }
+
+        setState(newState);
+    }
+
+    return [validState, setValidState];
 }
