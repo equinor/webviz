14d797817137bb265dfb4ae22119efcc9d56e449
Hans Kallekleiv <16436291+HansKallekleiv@users.noreply.github.com>
2023-08-25 12:19:28 +0200

    Add env variable for Sumo environment

--- backend/src/backend/config.py
+++ backend/src/backend/config.py
@@ -15,13 +15,13 @@ CLIENT_ID = "900ed417-a860-4970-bd37-73b059ca6f0d"
 CLIENT_SECRET = os.environ["WEBVIZ_CLIENT_SECRET"]
 SMDA_SUBSCRIPTION_KEY = os.environ["WEBVIZ_SMDA_SUBSCRIPTION_KEY"]
 SMDA_RESOURCE_SCOPE = os.environ["WEBVIZ_SMDA_RESOURCE_SCOPE"]
-
+SUMO_ENV = os.getenv("WEBVIZ_SUMO_ENV", "dev")
 GRAPH_SCOPES = ["User.Read"]
 
 RESOURCE_SCOPES_DICT = {
     # "sumo": [f"api://{sumo_app_reg['prod']['RESOURCE_ID']}/access_as_user"],
     # Note that when switching back to prod, SUMO env in create_sumo_client_instance() must also be changed
-    "sumo": [f"api://{sumo_app_reg['prod']['RESOURCE_ID']}/access_as_user"],
+    "sumo": [f"api://{sumo_app_reg[SUMO_ENV]['RESOURCE_ID']}/access_as_user"],
     "smda": [SMDA_RESOURCE_SCOPE],
 }
 
