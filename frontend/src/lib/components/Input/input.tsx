ee92b65aff26ebc5ada718294bd40ff631215176
Ruben Thoms <ruben.thoms@ceetronsolutions.com>
2023-08-28 16:12:47 +0200

    Missing check for input type when treating numbers

--- frontend/src/lib/components/Input/input.tsx
+++ frontend/src/lib/components/Input/input.tsx
@@ -39,22 +39,23 @@ export const Input = React.forwardRef((props: InputProps, ref: React.ForwardedRe
 
     const handleInputChange = React.useCallback(
         (event: React.ChangeEvent<HTMLInputElement>) => {
-            let value = parseFloat(event.target.value);
-            if (props.min !== undefined) {
-                value = Math.max(props.min, value);
+            if (props.type === "number") {
+                let newValue = parseFloat(event.target.value);
+                if (props.min !== undefined) {
+                    newValue = Math.max(props.min, newValue);
+                }
+
+                if (props.max !== undefined) {
+                    newValue = Math.min(props.max, newValue);
+                }
+
+                if (newValue !== prevValue) {
+                    setValue(value);
+                    setPrevValue(value);
+                }
+
+                event.target.value = newValue.toString();
             }
-
-            if (props.max !== undefined) {
-                value = Math.min(props.max, value);
-            }
-
-            if (value !== prevValue) {
-                setValue(value);
-                setPrevValue(value);
-            }
-
-            event.target.value = value.toString();
-
             if (onChange) {
                 onChange(event);
             }
