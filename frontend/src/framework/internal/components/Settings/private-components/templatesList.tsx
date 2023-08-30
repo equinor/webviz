eab5f8285e51ee5afdbc650af4dbbe1747d42605
Ruben Thoms <69145689+rubenthoms@users.noreply.github.com>
2023-08-30 22:19:48 +0200

    Improved UX for settings drawer and activating modules (#280)

--- frontend/src/framework/internal/components/Settings/private-components/templatesList.tsx
+++ frontend/src/framework/internal/components/Settings/private-components/templatesList.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { ModuleRegistry } from "@framework/ModuleRegistry";
-import { useStoreValue } from "@framework/StateStore";
+import { useStoreState } from "@framework/StateStore";
 import { Template, TemplateRegistry } from "@framework/TemplateRegistry";
 import { DrawerContent, Workbench } from "@framework/Workbench";
 import { Drawer } from "@framework/internal/components/Drawer";
@@ -93,7 +93,7 @@ type TemplatesListProps = {
     I will skip it for now and come back to it when it becomes a problem.
 */
 export const TemplatesList: React.FC<TemplatesListProps> = (props) => {
-    const drawerContent = useStoreValue(props.workbench.getGuiStateStore(), "drawerContent");
+    const [drawerContent, setDrawerContent] = useStoreState(props.workbench.getGuiStateStore(), "drawerContent");
     const [searchQuery, setSearchQuery] = React.useState("");
 
     const handleSearchQueryChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -106,6 +106,7 @@ export const TemplatesList: React.FC<TemplatesListProps> = (props) => {
             return;
         }
         props.workbench.applyTemplate(template);
+        setDrawerContent(DrawerContent.ModuleSettings);
     };
 
     return (
