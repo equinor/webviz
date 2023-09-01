7fc816198b639f39a16161fde648633e7634f80f
Ruben Thoms <69145689+rubenthoms@users.noreply.github.com>
2023-09-01 15:33:59 +0200

    Persistent ensemble set selection (#290)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
+import { AuthProvider } from "@framework/internal/providers/AuthProvider";
+import { CustomQueryClientProvider } from "@framework/internal/providers/QueryClientProvider";
+
 import App from "./App";
 
 const container = document.getElementById("root");
@@ -13,6 +16,10 @@ const root = createRoot(container);
 
 root.render(
     <React.StrictMode>
-        <App />
+        <AuthProvider>
+            <CustomQueryClientProvider>
+                <App />
+            </CustomQueryClientProvider>
+        </AuthProvider>
     </React.StrictMode>
 );
