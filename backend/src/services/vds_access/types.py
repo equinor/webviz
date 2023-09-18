a61dd5233fc1316542927072e3df6165b0ad64ba
Hans Kallekleiv <16436291+HansKallekleiv@users.noreply.github.com>
2023-09-18 14:49:44 +0200

    Vds Access

---
+++ backend/src/services/vds_access/types.py
@@ -0,0 +1,41 @@
+from typing import List
+
+from pydantic import BaseModel
+
+
+class VdsHandle(BaseModel):
+    sas_token: str
+    vds_url: str
+
+
+class VdsAxis(BaseModel):
+    annotation: str
+    max: float
+    min: float
+    samples: int
+    unit: str
+
+
+class VdsBoundingBox(BaseModel):
+    cdp: List[List[float]]
+    ij: List[List[float]]
+    ilxl: List[List[float]]
+
+
+class VdsMetaData(BaseModel):
+    axis: List[VdsAxis]
+    boundingBox: VdsBoundingBox
+    crs: str
+
+
+class VdsSliceResponse(BaseModel):
+    values: List[List[float]]
+    geospatial: List[List[float]]
+    shape: List[int]
+    x: VdsAxis
+    y: VdsAxis
+
+
+class VdsFenceResponse(BaseModel):
+    values: List[List[float]]
+    shape: List[int]
---
+++ backend/src/services/vds_access/vds_access.py
@@ -0,0 +1,117 @@
+import logging
+import os
+from typing import List
+
+import numpy as np
+import orjson as json
+import requests
+from requests_toolbelt.multipart.decoder import MultipartDecoder
+
+from .types import VdsFenceResponse, VdsHandle, VdsMetaData, VdsSliceResponse
+
+VDS_HOST_ADDRESS = os.getenv("VDS_HOST_ADDRESS")
+
+LOGGER = logging.getLogger(__name__)
+
+
+class VdsAccess:
+    """Access to the service hosting vds-slice.
+    https://github.com/equinor/vds-slice"""
+
+    def __init__(self, sumo_seismic_vds_handle: VdsHandle) -> None:
+        self.sas: str = sumo_seismic_vds_handle.sas_token
+        self.vds_url: str = sumo_seismic_vds_handle.vds_url
+
+    def _query(self, endpoint: str, params: dict) -> requests.Response:
+        """Query the VDS service"""
+        params.update({"vds": self.vds_url, "sas": self.sas})
+        response = requests.post(
+            f"{VDS_HOST_ADDRESS}/{endpoint}",
+            headers={"Content-Type": "application/json"},
+            data=json.dumps(params),  # pylint: disable=maybe-no-member
+            timeout=60,
+        )
+
+        if not response.ok:
+            raise RuntimeError(f"({str(response.status_code)})-{response.reason}-{response.text} ")
+        return response
+
+    def get_slice(self, direction: str, lineno: int) -> VdsSliceResponse:
+        """Gets a slice in i,j,k direction from the VDS service"""
+
+        endpoint = "slice"
+        params = {
+            "direction": direction,
+            "lineno": lineno,
+            "vds": self.vds_url,
+            "sas": self.sas,
+        }
+        response = self._query(endpoint, params)
+
+        parts = MultipartDecoder.from_response(response).parts
+        meta = json.loads(parts[0].content)  # pylint: disable=maybe-no-member
+        shape = (meta["y"]["samples"], meta["x"]["samples"])
+        values = parts[1].content
+        values = np.ndarray(shape, "f4", values)
+        values = values.tolist()
+        return VdsSliceResponse(values=values, **meta)
+
+    def get_fence(self, coordinates: List[List[float]], coordinate_system: str) -> VdsFenceResponse:
+        endpoint = "fence"
+
+        params = {
+            "coordinateSystem": coordinate_system,
+            "coordinates": coordinates,
+            "vds": self.vds_url,
+            "sas": self.sas,
+            "interpolation": "linear",
+            "fillValue": 10,
+        }
+        response = self._query(endpoint, params)
+
+        parts = MultipartDecoder.from_response(response).parts
+        metadata = json.loads(parts[0].content)  # pylint: disable=maybe-no-member
+        values = parts[1].content
+        values = np.ndarray(metadata["shape"], metadata["format"], values)
+        values = values.tolist()
+        return VdsFenceResponse(values=values, shape=metadata["shape"])
+
+    def get_metadata(self) -> VdsMetaData:
+        endpoint = "metadata"
+
+        params = {
+            "vds": self.vds_url,
+            "sas": self.sas,
+        }
+        response = self._query(endpoint, params)
+        if not response.ok:
+            raise RuntimeError(response.text)
+        metadata = response.json()
+        return VdsMetaData(**metadata)
+
+    # def get_surface_values(
+    #     self,
+    #     xtgeo_surf: RegularSurface,
+    #     above: float,
+    #     below: float,
+    #     attribute: str,
+    #     fill_value: float = -999.25,
+    # ) -> np.ndarray:
+    #     surface_values = xtgeo_surf.values.filled(fill_value)
+    #     array_shape = surface_values.shape
+    #     endpoint = "attributes/surface/along"
+
+    #     timer = PerfTimer()
+
+    #     params = {
+    #         "surface": surface_values.tolist(),
+    #         "above": above,
+    #         "below": below,
+    #         "attributes": [attribute],
+    #     }
+    #     response = self._query(endpoint, params)
+    #     print(f"Got attribute surface from VDS in: {timer.elapsed_ms()}ms ({attribute})")
+    #     parts = MultipartDecoder.from_response(response).parts
+    #     seismic_values = np.ndarray(array_shape, "f4", parts[1].content)
+    #     seismic_values = np.ma.masked_equal(seismic_values, fill_value)
+    #     return seismic_values
