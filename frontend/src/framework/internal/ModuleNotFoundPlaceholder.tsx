0ba8ceb0134745e0a503eb42c145006aac3bed65
Ruben Thoms <ruben.thoms@ceetronsolutions.com>
2023-09-25 11:52:43 +0200

    Adjusted icons

--- frontend/src/framework/internal/ModuleNotFoundPlaceholder.tsx
+++ frontend/src/framework/internal/ModuleNotFoundPlaceholder.tsx
@@ -1,8 +1,8 @@
 import { ImportState, Module } from "@framework/Module";
 import { ModuleInstance } from "@framework/ModuleInstance";
-import { BugAntIcon, ChatBubbleLeftRightIcon, NoSymbolIcon } from "@heroicons/react/20/solid";
 import { Button } from "@lib/components/Button";
 import { Tag } from "@lib/components/Tag";
+import { BugReport, Forum, WebAssetOff } from "@mui/icons-material";
 
 export class ModuleNotFoundPlaceholder extends Module<Record<string, never>> {
     constructor(moduleName: string) {
@@ -30,7 +30,7 @@ export class ModuleNotFoundPlaceholder extends Module<Record<string, never>> {
 
         return (
             <div className="w-full h-full flex flex-col items-center justify-center gap-6">
-                <NoSymbolIcon className="w-16 h-16 text-gray-400" />
+                <WebAssetOff fontSize="large" className="text-gray-400" />
                 <span className="text-lg text-red-500">
                     Module <Tag label={this.getName()} /> not found.
                 </span>
@@ -40,10 +40,10 @@ export class ModuleNotFoundPlaceholder extends Module<Record<string, never>> {
                     module has been removed, please get in touch with us on GitHub.
                 </span>
                 <div className="flex gap-4">
-                    <Button startIcon={<BugAntIcon className="w-4 h-4" />} onClick={reportIssue}>
+                    <Button startIcon={<BugReport fontSize="small" />} onClick={reportIssue}>
                         Report issue
                     </Button>
-                    <Button startIcon={<ChatBubbleLeftRightIcon className="w-4 h-4" />} onClick={startDiscussion}>
+                    <Button startIcon={<Forum fontSize="small" />} onClick={startDiscussion}>
                         Start discussion
                     </Button>
                 </div>
