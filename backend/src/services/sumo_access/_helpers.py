4abf0288286d2361b2ca717118ee92eddea3eb3b
Sigurd Pettersen <sigurd.pettersen@ceetronsolutions.com>
2023-10-03 23:15:36 +0200

    Reverted changes related to specification of SUMO environment. Implement this in separate PR instead

--- backend/src/services/sumo_access/_helpers.py
+++ backend/src/services/sumo_access/_helpers.py
@@ -1,8 +1,11 @@
+import os
+
 from sumo.wrapper import SumoClient
 
-from src.backend import config
+
+SUMO_ENV = os.getenv("WEBVIZ_SUMO_ENV", "dev")
 
 
 def create_sumo_client_instance(access_token: str) -> SumoClient:
-    sumo_client = SumoClient(env=config.SUMO_ENV, token=access_token, interactive=False)
+    sumo_client = SumoClient(env=SUMO_ENV, token=access_token, interactive=False)
     return sumo_client
