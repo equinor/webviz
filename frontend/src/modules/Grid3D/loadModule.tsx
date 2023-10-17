380b79e563a833ec0aaad1730fbf28ba82eddae3
Hans Kallekleiv <16436291+HansKallekleiv@users.noreply.github.com>
2023-10-17 12:12:27 +0100

    Fixes to 3dGrid module (#430)

--- frontend/src/modules/Grid3D/loadModule.tsx
+++ frontend/src/modules/Grid3D/loadModule.tsx
@@ -7,14 +7,12 @@ import { view } from "./view";
 const defaultState: state = {
     gridName: "Simgrid",
     parameterName: "PORO",
+    selectedWellUuids: [],
     realizations: ["1"],
     useStatistics: false,
-
 };
 
-const module = ModuleRegistry.initModule<state>("Grid3D", defaultState, {
-
-});
+const module = ModuleRegistry.initModule<state>("Grid3D", defaultState, {});
 
 module.viewFC = view;
 module.settingsFC = settings;
