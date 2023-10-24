38ddee3031186a2fe68043fc1a563c1f5725a192
Hans Kallekleiv <16436291+HansKallekleiv@users.noreply.github.com>
2023-10-24 10:49:47 +0200

    Add observations service/endpoints (#432)

---
+++ frontend/src/api/models/SummaryVectorDateObservation.ts
@@ -0,0 +1,15 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+/**
+ * A single observation of a summary vector at a specific date.
+ */
+export type SummaryVectorDateObservation = {
+    date: string;
+    comment: (string | null);
+    value: number;
+    error: number;
+    label: string;
+};
+
