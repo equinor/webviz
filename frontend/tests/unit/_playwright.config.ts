fd700770a8f9f4122dbad14c72315112850fb43b
Ruben Thoms <ruben.thoms@ceetronsolutions.com>
2023-10-24 10:34:55 +0200

    Removed unnecessary config file

--- frontend/tests/unit/_playwright.config.ts
+++
@@ -1,7 +0,0 @@
-import { PlaywrightTestConfig } from "@playwright/test";
-
-const config: PlaywrightTestConfig = {
-    snapshotPathTemplate: "{testDir}/{testFilePath}-snapshots/{arg}{ext}",
-};
-
-export default config;
