68c677905ca5ef84f5a96dd59bc20936fa339849
Hans Kallekleiv <16436291+HansKallekleiv@users.noreply.github.com>
2023-11-12 13:13:55 +0100

    Update frontend

--- frontend/src/modules/SubsurfaceMap/queryHooks.tsx
+++ frontend/src/modules/SubsurfaceMap/queryHooks.tsx
@@ -1,32 +1,11 @@
-import {
-    PolygonData_api,
-    SurfaceData_api,
-    SurfacePolygonDirectory_api,
-    WellBoreHeader_api,
-    WellBoreTrajectory_api,
-} from "@api";
+import { SurfaceData_api, WellBoreHeader_api, WellBoreTrajectory_api } from "@api";
 import { apiService } from "@framework/ApiService";
 import { SurfaceAddress } from "@modules_shared/Surface";
 import { SurfaceData_trans, transformSurfaceData } from "@modules_shared/Surface/queryDataTransforms";
 import { QueryFunction, QueryKey, UseQueryResult, useQuery } from "@tanstack/react-query";
 
-import { SurfacePolygonsAddress } from "./SurfacePolygonsAddress";
-
 const STALE_TIME = 60 * 1000;
 const CACHE_TIME = 60 * 1000;
-
-export function usePolygonDirectoryQuery(
-    caseUuid: string | undefined,
-    ensembleName: string | undefined
-): UseQueryResult<SurfacePolygonDirectory_api> {
-    return useQuery({
-        queryKey: ["getSurfacePolygonsDirectory", caseUuid, ensembleName],
-        queryFn: () => apiService.surfacePolygons.getSurfacePolygonsDirectory(caseUuid ?? "", ensembleName ?? ""),
-        staleTime: STALE_TIME,
-        gcTime: STALE_TIME,
-        enabled: caseUuid && ensembleName ? true : false,
-    });
-}
 
 export function useGetWellHeaders(caseUuid: string | undefined): UseQueryResult<WellBoreHeader_api[]> {
     return useQuery({
@@ -141,43 +120,5 @@ export function usePropertySurfaceDataByQueryAddress(
         staleTime: STALE_TIME,
         gcTime: CACHE_TIME,
         enabled: enabled,
-    });
-}
-export function usePolygonsDataQueryByAddress(
-    polygonAddr: SurfacePolygonsAddress | null
-): UseQueryResult<PolygonData_api[]> {
-    function dummyApiCall(): Promise<PolygonData_api[]> {
-        return new Promise((_resolve, reject) => {
-            reject(null);
-        });
-    }
-
-    if (!polygonAddr) {
-        return useQuery({
-            queryKey: ["getSurfacePolygonData_DUMMY_ALWAYS_DISABLED"],
-            queryFn: () => dummyApiCall,
-            enabled: false,
-        });
-    }
-
-    return useQuery({
-        queryKey: [
-            "getSurfacePolygonsData",
-            polygonAddr.caseUuid,
-            polygonAddr.ensemble,
-            polygonAddr.realizationNum,
-            polygonAddr.name,
-            polygonAddr.attribute,
-        ],
-        queryFn: () =>
-            apiService.surfacePolygons.getSurfacePolygonsData(
-                polygonAddr.caseUuid,
-                polygonAddr.ensemble,
-                polygonAddr.realizationNum,
-                polygonAddr.name,
-                polygonAddr.attribute
-            ),
-        staleTime: STALE_TIME,
-        gcTime: CACHE_TIME,
     });
 }
