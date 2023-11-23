0010293ccd1fb1623bbf6992c65058ad978980aa
Hans Kallekleiv <16436291+HansKallekleiv@users.noreply.github.com>
2023-11-23 19:21:05 +0100

    Fix es queries

--- backend/src/services/sumo_access/queries/case.py
+++ backend/src/services/sumo_access/queries/case.py
@@ -3,28 +3,32 @@ from typing import List
 from sumo.wrapper import SumoClient
 
 
-async def get_stratigraphic_column_identifier(sumo_client: SumoClient, case_id: str) -> str:
+async def get_stratigraphic_column_identifier(
+    sumo_client: SumoClient, case_id: str
+) -> str:
     """Get stratigraphic column identifier for a case (assuming unique for all objects)"""
-    response = await sumo_client.get_async(
-        "/search",
-        query=f"_sumo.parent_object:{case_id}",
-        size=1,
-        select="masterdata.smda.stratigraphic_column.identifier",
-    )
-
-    hits = response["hits"]["hits"]
-    return hits[0]["_source"]["masterdata"]["smda"]["stratigraphic_column"]["identifier"]
+    params = {
+        "query": f"_sumo.parent_object:{case_id}",
+        "size": 1,
+        "select": "masterdata.smda.stratigraphic_column.identifier",
+    }
+    response = await sumo_client.get_async("/search", params)
+    result = response.json()
+    hits = result["hits"]["hits"]
+    return hits[0]["_source"]["masterdata"]["smda"]["stratigraphic_column"][
+        "identifier"
+    ]
 
 
 async def get_field_identifiers(sumo_client: SumoClient, case_id: str) -> List[str]:
     """Get field identifiers for a case (assuming unique for all objects)"""
-    response = await sumo_client.get_async(
-        "/search",
-        query=f"_sumo.parent_object:{case_id}",
-        size=1,
-        select="masterdata.smda.field.identifier",
-    )
-
-    hits = response["hits"]["hits"]
+    params = {
+        "query": f"_sumo.parent_object:{case_id}",
+        "size": 1,
+        "select": "masterdata.smda.field.identifier",
+    }
+    response = await sumo_client.get_async("/search", params)
+    result = response.json()
+    hits = result["hits"]["hits"]
     fields = hits[0]["_source"]["masterdata"]["smda"]["field"]
     return [field["identifier"] for field in fields]
