a2e86a0b66cb925212b893c2a02af21e119977a8
Øyvind Lind-Johansen <47847084+lindjoha@users.noreply.github.com>
2023-12-15 14:20:37 +0000

    implemented Statical option

--- frontend/src/api/services/GroupTreeService.ts
+++ frontend/src/api/services/GroupTreeService.ts
@@ -4,6 +4,7 @@
 import type { CancelablePromise } from '../core/CancelablePromise';
 import type { BaseHttpRequest } from '../core/BaseHttpRequest';
 import type { Frequency } from '../models/Frequency';
+import type { StatisticFunction } from '../models/StatisticFunction';
 
 export class GroupTreeService {
 
@@ -17,21 +18,44 @@ export class GroupTreeService {
      * @returns any Successful Response
      * @throws ApiError
      */
-    public getGroupTreeData(
+    public getRealizationGroupTreeData(
         caseUuid: string,
         ensembleName: string,
-        realization?: (number | null),
+        realization: number | undefined,
         resamplingFrequency?: (Frequency | null),
     ): CancelablePromise<Array<any>> {
         console.log(realization)
         console.log(resamplingFrequency)
         return this.httpRequest.request({
             method: 'GET',
-            url: '/group_tree/group_tree_data/',
+            url: '/group_tree/realization_group_tree_data/',
             query: {
                 'case_uuid': caseUuid,
                 'ensemble_name': ensembleName,
                 'realization': realization,
+                'resampling_frequency': resamplingFrequency
+            },
+            errors: {
+                422: `Validation Error`,
+            },
+        });
+    }
+
+    public getStatisticalGroupTreeData(
+        caseUuid: string,
+        ensembleName: string,
+        statOption: StatisticFunction,
+        resamplingFrequency?: (Frequency | null),
+    ): CancelablePromise<Array<any>> {
+        console.log(statOption)
+        console.log(resamplingFrequency)
+        return this.httpRequest.request({
+            method: 'GET',
+            url: '/group_tree/statistical_group_tree_data/',
+            query: {
+                'case_uuid': caseUuid,
+                'ensemble_name': ensembleName,
+                'stat_option': statOption,
                 'resampling_frequency': resamplingFrequency
             },
             errors: {
--- frontend/src/modules/GroupTree/queryHooks.ts
+++ frontend/src/modules/GroupTree/queryHooks.ts
@@ -1,12 +1,12 @@
 import { apiService } from "@framework/ApiService";
 import { UseQueryResult, useQuery } from "@tanstack/react-query";
 import { Data } from "@webviz/group-tree/dist/redux/types"
-import { Frequency_api } from "@api";
+import { Frequency_api, StatisticFunction_api } from "@api";
 
 const STALE_TIME = 60 * 1000;
 const CACHE_TIME = 60 * 1000;
 
-export function useGroupTreeQuery(
+export function useRealizationGroupTreeQuery(
     caseUuid: string | undefined,
     ensembleName: string | undefined,
     realizationNumber: number | undefined,
@@ -15,7 +15,23 @@ export function useGroupTreeQuery(
     return useQuery({
         queryKey: ["getGroupTreeData", caseUuid, ensembleName, realizationNumber, resamplingFrequency],
         queryFn: () =>
-            apiService.groupTree.getGroupTreeData(caseUuid ?? "", ensembleName ?? "", realizationNumber, resamplingFrequency),
+            apiService.groupTree.getRealizationGroupTreeData(caseUuid ?? "", ensembleName ?? "", realizationNumber, resamplingFrequency),
+        staleTime: STALE_TIME,
+        cacheTime: CACHE_TIME,
+        enabled: caseUuid && ensembleName ? true : false,
+    });
+}
+
+export function useStatisticsGroupTreeQuery(
+    caseUuid: string | undefined,
+    ensembleName: string | undefined,
+    statOption: StatisticFunction_api,
+    resamplingFrequency: Frequency_api | null
+): UseQueryResult<any[]> {
+    return useQuery({
+        queryKey: ["getGroupTreeData", caseUuid, ensembleName, statOption, resamplingFrequency],
+        queryFn: () =>
+            apiService.groupTree.getStatisticalGroupTreeData(caseUuid ?? "", ensembleName ?? "", statOption, resamplingFrequency),
         staleTime: STALE_TIME,
         cacheTime: CACHE_TIME,
         enabled: caseUuid && ensembleName ? true : false,
