0ce0dcffaa2392237f51f8b3c4c7f29e62fa1738
Ruben Thoms <ruben.thoms@ceetronsolutions.com>
2024-01-09 15:05:05 +0100

    Improved element size hook in order to trigger fewer rerenders

--- frontend/src/lib/hooks/useElementSize.ts
+++ frontend/src/lib/hooks/useElementSize.ts
@@ -6,17 +6,28 @@ export function useElementSize(ref: React.RefObject<HTMLElement>): Size {
     const [size, setSize] = React.useState<Size>({ width: 0, height: 0 });
 
     React.useEffect(() => {
+        let isHidden = false;
+        let currentSize = { width: 0, height: 0 };
         const handleResize = (): void => {
             let newSize: Size = { width: 0, height: 0 };
             if (ref.current) {
-                // Element is not visible - do not change size as it might be expensive to render
+                // If element is not visible do not change size as it might be expensive to render
                 if (!(ref.current.offsetWidth || ref.current.offsetHeight || ref.current.getClientRects().length)) {
+                    isHidden = true;
                     return;
                 }
+
                 newSize = {
                     width: ref.current.offsetWidth,
                     height: ref.current.offsetHeight,
                 };
+
+                if (isHidden && currentSize.width === newSize.width && currentSize.height === newSize.height) {
+                    isHidden = false;
+                    return;
+                }
+
+                currentSize = newSize;
             }
             setSize(newSize);
         };
