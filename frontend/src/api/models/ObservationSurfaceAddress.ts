19ed95402d1c9ca1ddea8981844cbd60190e7db5
Hans Kallekleiv <16436291+HansKallekleiv@users.noreply.github.com>
2024-02-14 12:46:41 +0100

    surface-imprv

---
+++ frontend/src/api/models/ObservationSurfaceAddress.ts
@@ -0,0 +1,14 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { SurfaceAddressType } from './SurfaceAddressType';
+export type ObservationSurfaceAddress = {
+    address_type: SurfaceAddressType;
+    case_uuid: string;
+    ensemble_name: string;
+    name: string;
+    attribute: string;
+    iso_date_or_interval?: (string | null);
+};
+
