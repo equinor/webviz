77a03f6d5d657ab5b6790a2e9d64332bf48bcaee
Hans Kallekleiv <16436291+HansKallekleiv@users.noreply.github.com>
2024-03-11 15:29:30 +0100

    Sumo Grid 3D access

--- backend_py/primary/primary/routers/grid3d/schemas.py
+++ backend_py/primary/primary/routers/grid3d/schemas.py
@@ -1,3 +1,5 @@
+from typing import List, Optional
+
 from pydantic import BaseModel
 from webviz_pkg.core_utils.b64 import B64FloatArray, B64UintArray
 
@@ -11,3 +13,47 @@ class GridSurface(BaseModel):
     ymax: float
     zmin: float
     zmax: float
+
+
+class Grid3dBoundingBox(BaseModel):
+    """Bounding box for a 3D grid geometry"""
+
+    xmin: float
+    ymin: float
+    zmin: float
+    xmax: float
+    ymax: float
+    zmax: float
+
+
+class Grid3dZone(BaseModel):
+    """Named subset of 3D grid layers (Zone)"""
+
+    name: str
+    start_layer: int
+    end_layer: int
+
+
+class Grid3dDimensions(BaseModel):
+    """Specification of a 3D grid geometry"""
+
+    i_count: int
+    j_count: int
+    k_count: int
+    subgrids: List[Grid3dZone]
+
+
+class Grid3dPropertyInfo(BaseModel):
+    """Metadata for a 3D grid property"""
+
+    property_name: str
+    iso_date_or_interval: Optional[str] = None
+
+
+class Grid3dInfo(BaseModel):
+    """Metadata for a 3D grid model, including its properties and geometry"""
+
+    grid_name: str
+    bbox: Grid3dBoundingBox
+    dimensions: Grid3dDimensions
+    property_info_arr: List[Grid3dPropertyInfo]
