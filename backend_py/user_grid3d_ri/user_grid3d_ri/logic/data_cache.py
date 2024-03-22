ca58e53cb2a490a37193403bbd2bdc37e9a4d04a
Sigurd Pettersen <sigurd.pettersen@ceetronsolutions.com>
2024-03-22 17:07:51 +0100

    Try out caching of source cell indices

--- backend_py/user_grid3d_ri/user_grid3d_ri/logic/data_cache.py
+++ backend_py/user_grid3d_ri/user_grid3d_ri/logic/data_cache.py
@@ -16,14 +16,22 @@ _CACHE_ROOOT_DIR = "/home/appuser/data_cache"
 
 class DataCache:
     def __init__(self) -> None:
-        self._cache = diskcache.Cache(_CACHE_ROOOT_DIR)
+        # Default eviction policy is "least-recently-stored" which avoids writes when accessing the cache
+        # Try out "least-recently-used", keeping in mind that it is slower since does  writes when accessing the cache
+        self._cache = diskcache.Cache(directory=_CACHE_ROOOT_DIR, eviction_policy="least-recently-used")
 
     def set_uint32_numpy_arr(self, key: str, numpy_arr: NDArray[np.unsignedinteger]) -> None:
         byte_stream = io.BytesIO()
         np.save(byte_stream, numpy_arr)
 
         use_key = "nparr_uint32_" + key
-        self._cache.set(use_key, byte_stream.getvalue(), expire=20)
+
+        # Expiry is in seconds!
+        # What expiry should we set here?
+        # Maybe just skip it and relay on the cache eviction policy?
+        # For now, experiment with 5 minutes
+        self._cache.set(use_key, byte_stream.getvalue(), expire=5*60)
+        #self._cache.set(use_key, byte_stream.getvalue())
 
     def get_uint32_numpy_arr(self, key: str) -> NDArray[np.unsignedinteger] | None:
         use_key = "nparr_uint32_" + key
@@ -36,6 +44,7 @@ class DataCache:
 
         return np_arr
 
+    """
     def set_message_GetGridSurfaceResponse(
         self, key: str, message: GridGeometryExtraction_pb2.GetGridSurfaceResponse
     ) -> None:
@@ -51,3 +60,4 @@ class DataCache:
         message.ParseFromString(data)
 
         return message
+    """
--- backend_py/user_grid3d_ri/user_grid3d_ri/routers/grid_router.py
+++ backend_py/user_grid3d_ri/user_grid3d_ri/routers/grid_router.py
@@ -20,6 +20,7 @@ from user_grid3d_ri.logic.resinsight_manager import RESINSIGHT_MANAGER
 
 LOGGER = logging.getLogger(__name__)
 
+DATA_CACHE = DataCache()
 
 router = APIRouter()
 
@@ -53,60 +54,41 @@ async def post_get_grid_geometry(
     grpc_channel: grpc.aio.Channel = await RESINSIGHT_MANAGER.get_channel_for_running_ri_instance_async()
     perf_metrics.record_lap("get-ri")
 
-    # !!!!!!!!!!!!!!!!!!!!!!!
-    # ri_instance = rips.Instance(port=await RESINSIGHT_MANAGER.get_port_of_running_ri_instance_async(), launched=True)
-    # LOGGER.debug(f"{ri_instance.client_version_string()=}")
-    # LOGGER.debug(f"{ri_instance.version_string()=}")
-    # LOGGER.debug(f"{len(ri_instance.project.views())=}")
-    # LOGGER.debug(f"{len(ri_instance.project.cases())=}")
-
-    # data_cache = DataCache()
-    # response = data_cache.get_message_GetGridSurfaceResponse(req_body.grid_blob_object_uuid)
-    # et_read_cache_s = timer.lap_s()
-    grpc_response = None
-
-    if grpc_response is None:
-
-        grpc_ijk_index_filter = None
-        if req_body.ijk_index_filter:
-            grpc_ijk_index_filter = GridGeometryExtraction_pb2.IJKIndexFilter(
-                iMin=req_body.ijk_index_filter.min_i,
-                iMax=req_body.ijk_index_filter.max_i,
-                jMin=req_body.ijk_index_filter.min_j,
-                jMax=req_body.ijk_index_filter.max_j,
-                kMin=req_body.ijk_index_filter.min_k,
-                kMax=req_body.ijk_index_filter.max_k,
-            )
-        LOGGER.debug(f"{myfunc} - grpc_ijk_index_filter: {_proto_msg_as_oneliner(grpc_ijk_index_filter)}")
-
-        perf_metrics.reset_lap_timer()
-
-        request = GridGeometryExtraction_pb2.GetGridSurfaceRequest(
-            gridFilename=grid_path_name,
-            ijkIndexFilter=grpc_ijk_index_filter,
-            cellIndexFilter=None,
-            propertyFilter=None,
+    grpc_ijk_index_filter = None
+    if req_body.ijk_index_filter:
+        grpc_ijk_index_filter = GridGeometryExtraction_pb2.IJKIndexFilter(
+            iMin=req_body.ijk_index_filter.min_i,
+            iMax=req_body.ijk_index_filter.max_i,
+            jMin=req_body.ijk_index_filter.min_j,
+            jMax=req_body.ijk_index_filter.max_j,
+            kMin=req_body.ijk_index_filter.min_k,
+            kMax=req_body.ijk_index_filter.max_k,
         )
+    LOGGER.debug(f"{myfunc} - grpc_ijk_index_filter: {_proto_msg_as_oneliner(grpc_ijk_index_filter)}")
 
-        geo_extraction_stub = GridGeometryExtraction_pb2_grpc.GridGeometryExtractionStub(grpc_channel)
-        grpc_response = await geo_extraction_stub.GetGridSurface(request)
+    perf_metrics.reset_lap_timer()
+
+    request = GridGeometryExtraction_pb2.GetGridSurfaceRequest(
+        gridFilename=grid_path_name,
+        ijkIndexFilter=grpc_ijk_index_filter,
+        cellIndexFilter=None,
+        propertyFilter=None,
+    )
+
+    geo_extraction_stub = GridGeometryExtraction_pb2_grpc.GridGeometryExtractionStub(grpc_channel)
+    grpc_response = await geo_extraction_stub.GetGridSurface(request)
 
     perf_metrics.record_lap("ri-grid-geo")
-
-    # data_cache.set_message_GetGridSurfaceResponse(req_body.grid_blob_object_uuid, response)
-    # et_write_cache_s = timer.lap_s()
 
     grid_dims = grpc_response.gridDimensions
     cell_count = grid_dims.i * grid_dims.j * grid_dims.k
     LOGGER.debug(f"{myfunc} - grid_dims: {_proto_msg_as_oneliner(grid_dims)}")
     LOGGER.debug(f"{myfunc} - {cell_count=}")
-
     LOGGER.debug(f"{myfunc} - {len(grpc_response.quadIndicesArr)=}")
     LOGGER.debug(f"{myfunc} - {len(grpc_response.sourceCellIndicesArr)=}")
 
     vertices_np = np.asarray(grpc_response.vertexArray, dtype=np.float32)
     vertices_np = vertices_np.reshape(-1, 3)
-    # LOGGER.debug(f"{vertices_np[:5]=}")
 
     min_coord = np.min(vertices_np, axis=0)
     max_coord = np.max(vertices_np, axis=0)
@@ -118,10 +100,13 @@ async def post_get_grid_geometry(
     poly_indices_np = np.asarray(grpc_response.quadIndicesArr, dtype=np.int32)
     poly_indices_np = poly_indices_np.reshape(-1, 4)
     poly_indices_np = np.insert(poly_indices_np, 0, 4, axis=1).reshape(-1)
-    # LOGGER.debug(f"{poly_indices_np[:20]=}")
     perf_metrics.record_lap("proc-indices")
 
     source_cell_indices_np = np.asarray(grpc_response.sourceCellIndicesArr, dtype=np.uint32)
+
+    data_cache_key = _make_grid_geo_key(req_body.grid_blob_object_uuid, req_body.ijk_index_filter)
+    DATA_CACHE.set_uint32_numpy_arr(data_cache_key, source_cell_indices_np)
+    perf_metrics.record_lap("write-cache")
 
     ret_obj = api_schemas.GridGeometryResponse(
         vertices_b64arr=b64_encode_float_array_as_float32(vertices_np),
@@ -190,10 +175,15 @@ async def post_get_mapped_grid_properties(
     LOGGER.debug(f"{myfunc} - {property_path_name=}")
     perf_metrics.record_lap("get-blobs")
 
-    # data_cache = DataCache()
-    # source_cell_indices_np = data_cache.get_uint32_numpy_arr(grid_blob_object_uuid)
-    # et_read_cache_s = timer.lap_s()
     source_cell_indices_np = None
+    data_cache_key = _make_grid_geo_key(req_body.grid_blob_object_uuid, req_body.ijk_index_filter)
+    LOGGER.debug(f"{myfunc} - {data_cache_key=}")
+    source_cell_indices_np = DATA_CACHE.get_uint32_numpy_arr(data_cache_key)
+    LOGGER.debug(f"{myfunc} - cache: {len(source_cell_indices_np) if source_cell_indices_np is not None else 'NO'}")
+    perf_metrics.record_lap("read-cache")
+
+    ri_total_time: int | None = None
+    ri_perf_metrics: dict[str, int] | None = None
 
     if source_cell_indices_np is None:
         grpc_channel: grpc.aio.Channel = await RESINSIGHT_MANAGER.get_channel_for_running_ri_instance_async()
@@ -221,11 +211,13 @@ async def post_get_mapped_grid_properties(
         geo_extraction_stub = GridGeometryExtraction_pb2_grpc.GridGeometryExtractionStub(grpc_channel)
         grpc_response = await geo_extraction_stub.GetGridSurface(request)
 
+        ri_total_time=grpc_response.timeElapsedInfo.totalTimeElapsedMs
+        ri_perf_metrics=dict(grpc_response.timeElapsedInfo.namedEventsAndTimeElapsedMs)
         perf_metrics.record_lap("ri-grid-geo")
 
         source_cell_indices_np = np.asarray(grpc_response.sourceCellIndicesArr, dtype=np.uint32)
-        # data_cache.set_uint32_numpy_arr(grid_blob_object_uuid, source_cell_indices_np)
-        # et_write_cache_s = timer.lap_s()
+        DATA_CACHE.set_uint32_numpy_arr(data_cache_key, source_cell_indices_np)
+        perf_metrics.record_lap("write-cache")
 
     LOGGER.debug(f"{type(source_cell_indices_np)=}")
     LOGGER.debug(f"{source_cell_indices_np.dtype=}")
@@ -254,12 +246,11 @@ async def post_get_mapped_grid_properties(
     )
     perf_metrics.record_lap("make-response")
 
-    grpc_timeElapsedInfo = grpc_response.timeElapsedInfo
     ret_obj.stats = api_schemas.Stats(
         total_time=perf_metrics.get_elapsed_ms(),
         perf_metrics=perf_metrics.to_dict(),
-        ri_total_time=grpc_timeElapsedInfo.totalTimeElapsedMs,
-        ri_perf_metrics=dict(grpc_timeElapsedInfo.namedEventsAndTimeElapsedMs),
+        ri_total_time=ri_total_time,
+        ri_perf_metrics=ri_perf_metrics,
         vertex_count=-1,
         poly_count=int(len(source_cell_indices_np)),
     )
@@ -267,3 +258,13 @@ async def post_get_mapped_grid_properties(
     LOGGER.debug(f"{myfunc} - Got mapped grid properties in: {perf_metrics.to_string_s()}")
 
     return ret_obj
+
+
+def _make_grid_geo_key(grid_blob_object_uuid: str, filter: api_schemas.IJKIndexFilter | None) -> str:
+    filter_str = "NoFilter"
+    if filter:
+        filter_str = (
+            f"I[{filter.min_i},{filter.max_i}]-J[{filter.min_j},{filter.max_j}]-K[{filter.min_k},{filter.max_k}]"
+        )
+
+    return f"{grid_blob_object_uuid}--{filter_str}"
