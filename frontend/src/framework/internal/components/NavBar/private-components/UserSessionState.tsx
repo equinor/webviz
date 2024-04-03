34e1d34c6644e04d53ea7990d7b776bb38d800ca
Ruben Thoms <ruben.thoms@ceetronsolutions.com>
2024-04-03 10:55:54 +0200

    Fixed linting issue

--- frontend/src/framework/internal/components/NavBar/private-components/UserSessionState.tsx
+++ frontend/src/framework/internal/components/NavBar/private-components/UserSessionState.tsx
@@ -1,15 +1,20 @@
-import { apiService } from "@framework/ApiService";
 import { Memory } from "@mui/icons-material";
 
-export const UserSessionState = ({expanded}: {expanded: boolean}) => {
+export const UserSessionState = ({ expanded }: { expanded: boolean }) => {
     // const sessionState = useUserSessionState();
     // const memoryPercent = Math.round(sessionState.data?.memorySystem?.percent) || "-"
     // const cpuPercent = Math.round(sessionState.data?.cpuPercent) || "-"
-    const memoryPercent = "-"
-    const cpuPercent = "-"
+    const memoryPercent = "-";
+    const cpuPercent = "-";
 
-    return <div className="text-xs whitespace-nowrap text-gray-400">
-        <div><Memory fontSize="small" /> {expanded ? "Memory:" : "M"} {memoryPercent} %</div>
-        <div><Memory fontSize="small" /> {expanded ? "CPU:" : "C"} {cpuPercent} %</div>
-    </div>
-}
+    return (
+        <div className="text-xs whitespace-nowrap text-gray-400">
+            <div>
+                <Memory fontSize="small" /> {expanded ? "Memory:" : "M"} {memoryPercent} %
+            </div>
+            <div>
+                <Memory fontSize="small" /> {expanded ? "CPU:" : "C"} {cpuPercent} %
+            </div>
+        </div>
+    );
+};
