2276c200fc2949a37f79b2c0a947e2b18bc0e2b3
Jørgen Herje <82032112+jorgenherje@users.noreply.github.com>
2024-06-13 13:58:00 +0200

    Group tree module - initial version w/ single realization trees (#615)
    Co-authored-by: Øyvind Lind-Johansen <47847084+lindjoha@users.noreply.github.com>

---
+++ frontend/src/api/models/GroupTreeData.ts
@@ -0,0 +1,12 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { DatedTree } from './DatedTree';
+import type { GroupTreeMetadata } from './GroupTreeMetadata';
+export type GroupTreeData = {
+    edge_metadata_list: Array<GroupTreeMetadata>;
+    node_metadata_list: Array<GroupTreeMetadata>;
+    dated_trees: Array<DatedTree>;
+};
+
