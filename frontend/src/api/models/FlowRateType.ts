3d8bdddf73e6499d538425a22d50ac75f97653b8
Øyvind Lind-Johansen <47847084+lindjoha@users.noreply.github.com>
2024-07-04 07:14:57 +0000

    Added data types

---
+++ frontend/src/api/models/FlowRateType.ts
@@ -0,0 +1,11 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export enum FlowRateType {
+    OIL = 'OIL',
+    LIQ = 'LIQ',
+    GAS = 'GAS',
+    WG = 'WG',
+    TM = 'TM',
+}
---
+++ frontend/src/api/models/WFR.ts
@@ -0,0 +1,11 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export enum WFR {
+    WOR = 'WOR',
+    WCT = 'WCT',
+    WGR = 'WGR',
+    WWR = 'WWR',
+    WTF = 'WTF',
+}
