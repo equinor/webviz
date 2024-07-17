5ac4e9dada5d77605c2b3d5f19d2ec39d6f21e40
Sigurd Pettersen <sigurd.pettersen@ceetronsolutions.com>
2024-07-17 10:56:31 +0200

    Use Record as type instead of object

--- frontend/src/lib/utils/queryStringUtils.ts
+++ frontend/src/lib/utils/queryStringUtils.ts
@@ -12,7 +12,7 @@
 // Example:
 // encodedKeyValString = "key1~123.5~~key2~'someString'~~key3~false"
 //
-export function encodePropertiesAsKeyValStr(objOrMap: object | Map<string, string | number | boolean>): string {
+export function encodePropertiesAsKeyValStr(objOrMap: Record<string, string | number | boolean | null> | Map<string, string | number | boolean | null>): string {
     const KEYVAL_ASSIGN_SEP = "~";
     const KEYVAL_ELEMENT_SEP = "~~";
 
