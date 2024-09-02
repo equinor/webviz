a56cbd5a472edf77619bf143808f0d3121e84bb1
Hans Kallekleiv <16436291+HansKallekleiv@users.noreply.github.com>
2024-09-02 16:07:15 +0200

    wip

--- backend_py/primary/tests/integration/routers/timeseries/test_get_vector_list.py
+++ backend_py/primary/tests/integration/routers/timeseries/test_get_vector_list.py
@@ -3,6 +3,7 @@ from primary.routers.timeseries import schemas
 from sumo.wrapper import SumoClient
 
 from primary.services.utils.authenticated_user import AuthenticatedUser, AccessTokens
+from primary.services.sumo_access.summary_access import SummaryAccess
 from fmu.sumo.explorer import Explorer
 
 async def test_get_vector_list(test_user, sumo_test_ensemble_ahm):
@@ -54,6 +55,19 @@ async def test_get_vector_list(test_user, sumo_test_ensemble_ahm):
 
     response = await client.post_async("/search", json=search_payload)
     assert response.status_code == 200
+
+    access = SummaryAccess.from_case_uuid(user.get_sumo_access_token(), "485041ce-ad72-48a3-ac8c-484c0ed95cf8", "iter-0")
+    
+
+    vector_info_arr = await access.get_available_vectors_async()
+    
+
+    ret_arr: list[schemas.VectorDescription] = [
+        schemas.VectorDescription(name=vi.name, descriptive_name=vi.name, has_historical=vi.has_historical)
+        for vi in vector_info_arr
+    ]
+    assert len(ret_arr) == 786
+    assert isinstance(ret_arr[0], schemas.VectorDescription)
     # vector_list = await router.get_vector_list(
     #     None, user, sumo_test_ensemble_ahm.case_uuid, sumo_test_ensemble_ahm.ensemble_name
     # )
