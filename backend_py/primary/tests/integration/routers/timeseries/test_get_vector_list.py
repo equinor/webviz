d4b3e31956bf3d0b8f1633973124f5926fa8dabf
Hans Kallekleiv <16436291+HansKallekleiv@users.noreply.github.com>
2024-09-02 15:42:47 +0200

    wip

--- backend_py/primary/tests/integration/routers/timeseries/test_get_vector_list.py
+++ backend_py/primary/tests/integration/routers/timeseries/test_get_vector_list.py
@@ -7,5 +7,7 @@ async def test_get_vector_list(test_user, sumo_test_ensemble_ahm):
     vector_list = await router.get_vector_list(
         None, test_user, sumo_test_ensemble_ahm.case_uuid, sumo_test_ensemble_ahm.ensemble_name
     )
+    assert test_user.has_sumo_access_token() == True
+    assert len(test_user.get_sumo_access_token()) == 537
     assert len(vector_list) == 786
     assert isinstance(vector_list[0], schemas.VectorDescription)
