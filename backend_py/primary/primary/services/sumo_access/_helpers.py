db4c519d195e0b4f30dee9331f5def1a10b0adc7
Hans Kallekleiv <16436291+HansKallekleiv@users.noreply.github.com>
2024-09-02 16:17:21 +0200

    wip

--- backend_py/primary/primary/services/sumo_access/_helpers.py
+++ backend_py/primary/primary/services/sumo_access/_helpers.py
@@ -11,7 +11,7 @@ LOGGER = logging.getLogger(__name__)
 
 
 def create_sumo_client(access_token: str) -> SumoClient:
-    sumo_client = SumoClient(env=config.SUMO_ENV, token=access_token, interactive=False)
+    sumo_client = SumoClient(env="prod")
     return sumo_client
 
 
