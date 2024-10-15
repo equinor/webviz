41c3527be2cc1de1aca852620a617e10a633bed7
jorgenherje <jorgen.herje@ceetronsolutions.com>
2024-10-15 10:59:17 +0200

    Final compact/non-compact realization visualization
    Use circle divs row by row, as discussed in standup

--- frontend/src/framework/components/EnsembleRealizationFilter/private-components/realizationNumberDisplay.tsx
+++ frontend/src/framework/components/EnsembleRealizationFilter/private-components/realizationNumberDisplay.tsx
@@ -12,10 +12,15 @@ export type RealizationNumberDisplayProps = {
     disableInteraction: boolean;
     onRealizationNumberSelectionsChange: (realizationNumberSelections: readonly number[]) => void;
 };
-
 export const RealizationNumberDisplay: React.FC<RealizationNumberDisplayProps> = (props) => {
     const divRef = React.useRef<HTMLDivElement>(null);
     const divSize = useElementSize(divRef);
+
+    // Compact and non-compact element width and gap
+    const compactWidthAndHeight = 9; //px
+    const compactGap = 3; //px
+    const nonCompactWidthAndHeight = 12; //px
+    const nonCompactGap = 4; //px
 
     const [prevSelectedRealizations, setPrevSelectedRealizations] = React.useState<readonly number[]>();
     const [allRealizationsInRange, setAllRealizationsInRange] = React.useState<number[]>(
@@ -31,13 +36,11 @@ export const RealizationNumberDisplay: React.FC<RealizationNumberDisplayProps> =
         if (props.disableInteraction) {
             return;
         }
-
         if (!props.selectedRealizations.includes(realization)) {
             // Add the realization to the selected realizations
             props.onRealizationNumberSelectionsChange([...props.selectedRealizations, realization]);
             return;
         }
-
         // Remove the realization from the selected realizations
         const newRealizationNumberSelections = props.selectedRealizations.filter(
             (selectedRealization) => selectedRealization !== realization
@@ -45,181 +48,62 @@ export const RealizationNumberDisplay: React.FC<RealizationNumberDisplayProps> =
         props.onRealizationNumberSelectionsChange(newRealizationNumberSelections);
     }
 
-    function createNonCompactRealizationNumberVisualization(): React.ReactNode {
-        const groupDivs: JSX.Element[] = [];
-        const numGroupedRealizations = 5;
+    function createRealizationNumberVisualization2(isCompact: boolean, numRealizationPerRow: number): React.ReactNode {
+        const mainDivElements: JSX.Element[] = [];
 
-        let groupElmCounter = 0;
-        let groupRealizationDivs: JSX.Element[] = [];
-        for (const realization of allRealizationsInRange) {
+        // Compact/non-compact div size and gap class definitions
+        const gapClass = isCompact ? `gap-[${compactGap}px]` : `gap-[${nonCompactGap}px]`;
+        const realizationDivSizeClass = isCompact
+            ? `w-[${compactWidthAndHeight}px] h-[${compactWidthAndHeight}px]`
+            : `w-[${nonCompactWidthAndHeight}px] h-[${nonCompactWidthAndHeight}px]`;
+
+        let rowElmCounter = 0;
+        let rowElements: JSX.Element[] = [];
+        for (const [index, realization] of allRealizationsInRange.entries()) {
             const isCurrentRealizationAvailable = props.availableRealizations.includes(realization);
             const isRealizationSelected = props.selectedRealizations.includes(realization);
             const isClickDisabled = props.disableInteraction || !isCurrentRealizationAvailable;
-
+            if (rowElmCounter === 0) {
+                rowElements = [];
+            }
             const realizationDiv = (
                 <div
                     title={`real-${realization}`}
                     key={realization}
                     className={resolveClassNames(
-                        "w-[0.75rem] rounded-full aspect-square flex justify-center items-center",
+                        `${realizationDivSizeClass} rounded-full aspect-square flex justify-center items-center`,
                         {
                             "bg-green-600": isRealizationSelected,
                             "bg-gray-400": !isRealizationSelected && isCurrentRealizationAvailable,
                             "bg-gray-300": !isRealizationSelected && !isCurrentRealizationAvailable,
-                            "border-gray-600": !isCurrentRealizationAvailable,
                             "cursor-pointer": !isClickDisabled,
                         }
                     )}
                     onClick={isClickDisabled ? undefined : () => handleRealizationElementClick(realization)}
                 />
             );
+            rowElements.push(realizationDiv);
 
-            // Add circle to group of 5, when group is full, add to groupDivs and reset groupRealizationDivs and groupElmCounter
-            groupRealizationDivs.push(realizationDiv);
-            groupElmCounter++;
-
-            if (groupElmCounter === numGroupedRealizations) {
-                groupDivs.push(<div className="flex gap-[0.2rem] ">{[...groupRealizationDivs]}</div>);
-                groupRealizationDivs = [];
-                groupElmCounter = 0;
+            // If the group is full (or last realization), add it to the main div elements and reset counter
+            const isLastRealization = index === allRealizationsInRange.length - 1;
+            if (++rowElmCounter === numRealizationPerRow || isLastRealization) {
+                const groupDiv = <div className={resolveClassNames(`flex ${gapClass}`)}>{[...rowElements]}</div>;
+                mainDivElements.push(groupDiv);
+                rowElmCounter = 0;
             }
         }
-
-        return <div className="flex flex-wrap gap-1">{groupDivs}</div>;
+        return <div className={resolveClassNames(`flex flex-col justify-start ${gapClass}`)}>{mainDivElements}</div>;
     }
 
-    function createNonCompactRealizationNumberVisualization_NEW(): React.ReactNode {
-        const mainDivElements: JSX.Element[] = [];
-
-        const numRealizationsPerGroup = 25;
-        let realizationCount = 0;
-        let groupElements: JSX.Element[] = [];
-        for (const realization of allRealizationsInRange) {
-            const isCurrentRealizationAvailable = props.availableRealizations.includes(realization);
-            const isRealizationSelected = props.selectedRealizations.includes(realization);
-            const isClickDisabled = props.disableInteraction || !isCurrentRealizationAvailable;
-
-            if (realizationCount === 0) {
-                groupElements = [];
-            }
-
-            groupElements.push(
-                <div
-                    title={`real-${realization}`}
-                    key={realization}
-                    className={resolveClassNames("w-[12px] h-[12px] rounded-sm flex justify-center items-center", {
-                        "bg-green-600": isRealizationSelected,
-                        "bg-gray-400": !isRealizationSelected && isCurrentRealizationAvailable,
-                        "bg-gray-300": !isRealizationSelected && !isCurrentRealizationAvailable,
-                        "cursor-pointer": !isClickDisabled,
-                    })}
-                    onClick={isClickDisabled ? undefined : () => handleRealizationElementClick(realization)}
-                />
-            );
-
-            if (++realizationCount === numRealizationsPerGroup) {
-                const groupElement = (
-                    <div className="flex flex-wrap justify-start w-[84px] gap-[4px] p-[4px]">{[...groupElements]}</div>
-                );
-                mainDivElements.push(groupElement);
-                realizationCount = 0;
-            }
-        }
-
-        return (
-            <div className="flex flex-row">
-                <div className="flex flex-wrap justify-start gap-1 bg-white">{mainDivElements}</div>
-                <div className="flex-grow" />
-            </div>
-        );
-    }
-
-    function createCompactRealizationNumberVisualization(): React.ReactNode {
-        const { width, height } = divSize;
-        const realizationCount = allRealizationsInRange.length;
-        const lineWidth = width / realizationCount;
-        return (
-            <div className="h-10">
-                <svg width={width} height={height} viewBox={`0 0 ${width} ${height}`}>
-                    {allRealizationsInRange.map((realization, index) => {
-                        const isCurrentRealizationAvailable = props.availableRealizations.includes(realization);
-                        const isRealizationSelected = props.selectedRealizations.includes(realization);
-
-                        return (
-                            <rect
-                                key={realization}
-                                x={index * lineWidth} // Adjust the x position to distribute lines evenly
-                                y={0}
-                                width={lineWidth} // Set the width of each rect to lineWidth
-                                height={"100%"}
-                                className={resolveClassNames("", {
-                                    "fill-green-600": isRealizationSelected,
-                                    "border-green-600": isRealizationSelected,
-                                    "fill-gray-300": !isRealizationSelected && isCurrentRealizationAvailable,
-                                    "border-gray-300": !isRealizationSelected && isCurrentRealizationAvailable,
-                                    "fill-gray-200": !isRealizationSelected && !isCurrentRealizationAvailable,
-                                    "border-gray-200": !isRealizationSelected && !isCurrentRealizationAvailable,
-                                })}
-                            >
-                                <title>{`real-${realization}`}</title>
-                            </rect>
-                        );
-                    })}
-                </svg>
-            </div>
-        );
-    }
-
-    function createCompactRealizationNumberVisualization_NEW(): React.ReactNode {
-        const mainDivElements: JSX.Element[] = [];
-
-        const numRealizationsPerGroup = 25;
-        let realizationCount = 0;
-        let groupElements: JSX.Element[] = [];
-        for (const realization of allRealizationsInRange) {
-            const isCurrentRealizationAvailable = props.availableRealizations.includes(realization);
-            const isRealizationSelected = props.selectedRealizations.includes(realization);
-            const isClickDisabled = props.disableInteraction || !isCurrentRealizationAvailable;
-
-            if (realizationCount === 0) {
-                groupElements = [];
-            }
-
-            groupElements.push(
-                <div
-                    title={`real-${realization}`}
-                    key={realization}
-                    className={resolveClassNames("w-[6px] h-[6px] flex justify-center items-center", {
-                        "bg-green-600": isRealizationSelected,
-                        "bg-gray-400": !isRealizationSelected && isCurrentRealizationAvailable,
-                        "bg-gray-300": !isRealizationSelected && !isCurrentRealizationAvailable,
-                        "cursor-pointer": !isClickDisabled,
-                    })}
-                />
-            );
-
-            if (++realizationCount === numRealizationsPerGroup) {
-                const groupElement = (
-                    <div className="flex flex-wrap justify-start w-[38px] gap-[2px] bg-white">{[...groupElements]}</div>
-                );
-                mainDivElements.push(groupElement);
-                realizationCount = 0;
-            }
-        }
-
-        return (
-            <div className="flex flex-row">
-                <div className="flex flex-wrap justify-start gap-1 bg-white">{mainDivElements}</div>
-                <div className="flex-grow" />
-            </div>
-        );
-    }
+    // Find the number of realizations that can fit in a row based on non-compact size, as factor of 5
+    const candidateNumberOfRealizationsPerRow = Math.floor(divSize.width / (nonCompactWidthAndHeight + nonCompactGap));
+    const remainder = candidateNumberOfRealizationsPerRow % 5;
+    const newNumberOfRealizationsPerRow =
+        remainder === 0 ? candidateNumberOfRealizationsPerRow : candidateNumberOfRealizationsPerRow - remainder;
 
     return (
         <div ref={divRef}>
-            {props.showAsCompact
-                ? createCompactRealizationNumberVisualization_NEW()
-                : createNonCompactRealizationNumberVisualization_NEW()}
+            {createRealizationNumberVisualization2(props.showAsCompact ?? false, newNumberOfRealizationsPerRow)}
         </div>
     );
 };
