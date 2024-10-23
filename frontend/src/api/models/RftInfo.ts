e7970b7405449d243dda885222bfe93aee0ba8bb
Ruben Thoms <ruben.thoms@ceetronsolutions.com>
2024-10-23 11:05:56 +0200

    wip

--- frontend/src/api/models/RftInfo.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do no edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export type RftInfo = {
-    well_name: string;
-    timestamps_utc_ms: Array<number>;
-};
-
