b6df4c7b7bb9be6b87271754a80349a95137f1e6
Hans Kallekleiv <16436291+HansKallekleiv@users.noreply.github.com>
2024-10-28 15:29:18 +0100

    Rft improvements

--- frontend/src/modules/Rft/queryHooks.ts
+++ frontend/src/modules/Rft/queryHooks.ts
@@ -1,17 +1,17 @@
-import { RftInfo_api, RftRealizationData_api } from "@api";
+import { RftRealizationData_api, RftTableDefinition_api } from "@api";
 import { apiService } from "@framework/ApiService";
 import { UseQueryResult, useQuery } from "@tanstack/react-query";
 
 const STALE_TIME = 60 * 1000;
 const CACHE_TIME = 60 * 1000;
 
-export function useRftWellList(
+export function useRftTableDefinition(
     caseUuid: string | undefined,
     ensembleName: string | undefined
-): UseQueryResult<RftInfo_api[]> {
+): UseQueryResult<RftTableDefinition_api> {
     return useQuery({
-        queryKey: ["getRftWellList", caseUuid, ensembleName],
-        queryFn: () => apiService.rft.getRftInfo(caseUuid ?? "", ensembleName ?? ""),
+        queryKey: ["getRftTableDefinition", caseUuid, ensembleName],
+        queryFn: () => apiService.rft.getTableDefinition(caseUuid ?? "", ensembleName ?? ""),
         staleTime: STALE_TIME,
         gcTime: CACHE_TIME,
         enabled: caseUuid && ensembleName ? true : false,
--- frontend/src/modules/Rft/settings/atoms/baseAtoms.ts
+++ frontend/src/modules/Rft/settings/atoms/baseAtoms.ts
@@ -1,5 +1,20 @@
+import { EnsembleIdent } from "@framework/EnsembleIdent";
+import { atomWithCompare } from "@framework/utils/atomUtils";
 import { RftWellAddress } from "@modules/Rft/typesAndEnums";
 
 import { atom } from "jotai";
+import { isEqual } from "lodash";
 
+function areEnsembleIdentsEqual(a: EnsembleIdent | null, b: EnsembleIdent | null) {
+    if (a === null) {
+        return b === null;
+    }
+    return a.equals(b);
+}
+
+export const userSelectedEnsembleIdentAtom = atomWithCompare<EnsembleIdent | null>(null, areEnsembleIdentsEqual);
+export const validRealizationNumbersAtom = atom<number[] | null>(null);
+export const userSelectedResponseNameAtom = atom<string | null>(null);
+export const userSelectedWellNameAtom = atom<string | null>(null);
+export const userSelectedRftTimestampsUtcMsAtom = atom<number | null>(null);
 export const rftWellAddressAtom = atom<RftWellAddress | null>(null);
