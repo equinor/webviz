74c71f01deebba2ac298f330c82abb82b39d507c
Ruben Thoms <ruben.thoms@ceetronsolutions.com>
2024-12-16 11:26:43 +0100

    Removed unused code

--- frontend/src/modules/2DViewer/settings/atoms/derivedAtoms.ts
+++ frontend/src/modules/2DViewer/settings/atoms/derivedAtoms.ts
@@ -1,4 +1,3 @@
-import { EnsembleSet } from "@framework/EnsembleSet";
 import { EnsembleSetAtom } from "@framework/GlobalAtoms";
 
 import { atom } from "jotai";
@@ -17,15 +16,4 @@ export const selectedFieldIdentifierAtom = atom((get) => {
     }
 
     return userSelectedField;
-});
-
-export const filteredEnsembleSetAtom = atom((get) => {
-    const ensembleSet = get(EnsembleSetAtom);
-    const fieldIdentifier = get(userSelectedFieldIdentifierAtom);
-
-    if (fieldIdentifier === null) {
-        return ensembleSet;
-    }
-
-    return new EnsembleSet(ensembleSet.getEnsembleArr().filter((el) => el.getFieldIdentifier() === fieldIdentifier));
 });
