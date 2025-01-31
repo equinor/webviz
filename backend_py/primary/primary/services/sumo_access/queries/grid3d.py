1942d2f8ee139f9fbd09fa3b94c1b1986610cd38
Hans Kallekleiv <16436291+HansKallekleiv@users.noreply.github.com>
2025-01-31 11:17:12 +0100

    fix

--- backend_py/primary/primary/services/sumo_access/queries/grid3d.py
+++ backend_py/primary/primary/services/sumo_access/queries/grid3d.py
@@ -110,8 +110,6 @@ async def get_grid_geometry_and_property_blob_ids_async(
     # Try with tagname first
     grid_geometry_id, grid_property_id = await try_query_with_field(GridPropertyField.TAGNAME)
 
-    if grid_geometry_id is None:
-        raise InvalidDataError("Did not find expected document types", service=Service.SUMO)
     # If no results, try with geometry.name
     if grid_property_id is None:
         grid_geometry_id, grid_property_id = await try_query_with_field(GridPropertyField.GEOMETRY_NAME)
