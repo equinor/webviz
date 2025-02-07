2deca076cf703c948449fae6071b06cf0941fffc
Anders Rantala Hunderi <anders@ceetronsolutions.com>
2025-02-07 14:16:34 +0100

    Fixed tests

--- frontend/tests/unit/WellLogViewer/queries.test.ts
+++ frontend/tests/unit/WellLogViewer/queries.test.ts
@@ -6,52 +6,52 @@ import { mergeResults } from "../../../src/modules/WellLogViewer/utils/queries";
 
 describe("mergeResults", () => {
     it("should merge results correctly when all queries are successful", () => {
-        const queryResults: UseQueryResult<number>[] = [
-            { data: 1, isLoading: false, isSuccess: true, isFetched: true },
-            { data: 2, isLoading: false, isSuccess: true, isFetched: true },
+        const queryResults = [
+            { data: 1, isPending: false, isSuccess: true, isError: false },
+            { data: 2, isPending: false, isSuccess: true, isError: false },
         ] as UseQueryResult<number>[];
 
         const result = mergeResults(queryResults);
 
         expect(result.data).toEqual([1, 2]);
-        expect(result.isLoading).toBe(false);
+        expect(result.isPending).toBe(false);
         expect(result.isSuccess).toBe(true);
-        expect(result.isFetched).toBe(true);
+        expect(result.isError).toBe(false);
     });
 
     it("should return loading state if any query is loading", () => {
-        const queryResults: UseQueryResult<number>[] = [
-            { data: 1, isLoading: true, isSuccess: false, isFetched: false },
-            { data: 2, isLoading: false, isSuccess: true, isFetched: true },
+        const queryResults = [
+            { data: 1, isPending: true, isSuccess: false, isError: false },
+            { data: 2, isPending: false, isSuccess: true, isError: false },
         ] as UseQueryResult<number>[];
 
         const result = mergeResults(queryResults);
 
-        expect(result.isLoading).toBe(true);
-        expect(result.isSuccess).toBe(false);
-        expect(result.isFetched).toBe(false);
         expect(result.data).toBe(undefined);
+        expect(result.isPending).toBe(true);
+        expect(result.isSuccess).toBe(false);
+        expect(result.isError).toBe(false);
     });
 
     it("should return error state if any query has an error", () => {
-        const queryResults: UseQueryResult<number>[] = [
-            { data: 1, isLoading: false, isSuccess: true, isFetched: true },
-            { error: new Error("Test error"), isLoading: false, isSuccess: false, isFetched: false },
+        const queryResults = [
+            { isPending: false, isSuccess: true, isError: false, data: 1 },
+            { isPending: false, isSuccess: false, isError: true, error: new Error("Test error") },
         ] as UseQueryResult<number>[];
 
         const result = mergeResults(queryResults);
 
         expect(result.error).toEqual(new Error("Test error"));
-        expect(result.isLoading).toBe(false);
+        expect(result.isError).toBe(true);
+        expect(result.isPending).toBe(false);
         expect(result.isSuccess).toBe(false);
-        expect(result.isFetched).toBe(false);
         expect(result.data).toBe(undefined);
     });
 
     it("should apply data transform if provided", () => {
-        const queryResults: UseQueryResult<number>[] = [
-            { data: 1, isLoading: false, isSuccess: true, isFetched: true },
-            { data: 2, isLoading: false, isSuccess: true, isFetched: true },
+        const queryResults = [
+            { data: 1, isPending: false, isSuccess: true, isError: false },
+            { data: 2, isPending: false, isSuccess: true, isError: false },
         ] as UseQueryResult<number>[];
 
         const dataTransform = (data: number[]) => data.map((x) => x * 2);
@@ -59,8 +59,8 @@ describe("mergeResults", () => {
         const result = mergeResults(queryResults, dataTransform);
 
         expect(result.data).toEqual([2, 4]);
-        expect(result.isLoading).toBe(false);
+        expect(result.isPending).toBe(false);
         expect(result.isSuccess).toBe(true);
-        expect(result.isFetched).toBe(true);
+        expect(result.isError).toBe(false);
     });
 });
