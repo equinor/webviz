0e58b5fb6178bee85093029d3c8f152d2e0eeeb5
Sigurd Pettersen <sigurd.pettersen@ceetronsolutions.com>
2025-02-25 22:38:30 +0100

    Use create_sumo_client() and get_sas_token_and_blob_base_uri_for_case_async() in UserGrid3dService

--- backend_py/primary/primary/services/sumo_access/sumo_blob_access.py
+++ backend_py/primary/primary/services/sumo_access/sumo_blob_access.py
@@ -1,4 +1,3 @@
-import requests
 import httpx
 
 from primary import config
@@ -6,29 +5,6 @@ from primary.services.service_exceptions import AuthorizationError, Service
 from primary.services.utils.httpx_async_client_wrapper import HTTPX_ASYNC_CLIENT_WRAPPER
 
 SUMO_BASE_URI = f"https://main-sumo-{config.SUMO_ENV}.radix.equinor.com/api/v1"
-
-
-def get_sas_token_and_blob_store_base_uri_for_case(sumo_access_token: str, case_uuid: str) -> tuple[str, str]:
-    """
-    Get a SAS token and a base URI that allows reading of all children of case_uuid
-    The returned base uri looks something like this:
-        https://xxxsumoxxx.blob.core.windows.net/{case_uuid}
-
-    To actually fetch data for a blob belonging to this case, you need to form a SAS URI:
-        {blob_store_base_uri}/{my_blob_id}?{sas_token}
-    """
-
-    req_url = f"{SUMO_BASE_URI}/objects('{case_uuid}')/authtoken"
-    req_headers = {"Authorization": f"Bearer {sumo_access_token}"}
-    res = requests.get(url=req_url, headers=req_headers, timeout=60)
-    if res.status_code != 200:
-        raise AuthorizationError(f"Failed to get SAS token for case {case_uuid}", Service.GENERAL)
-
-    body = res.json()
-    sas_token = body["auth"]
-    blob_store_base_uri = body["baseuri"].removesuffix("/")
-
-    return sas_token, blob_store_base_uri
 
 
 async def get_sas_token_and_blob_base_uri_for_case_async(sumo_access_token: str, case_uuid: str) -> tuple[str, str]:
@@ -47,8 +23,8 @@ async def get_sas_token_and_blob_base_uri_for_case_async(sumo_access_token: str,
     try:
         res = await HTTPX_ASYNC_CLIENT_WRAPPER.client.get(url=req_url, headers=req_headers, timeout=60)
         res.raise_for_status()
-    except httpx.HTTPError as ex:
-        raise AuthorizationError(f"Failed to get SAS token for case {case_uuid}", Service.GENERAL)
+    except httpx.HTTPError as exc:
+        raise AuthorizationError(f"Failed to get SAS token for case {case_uuid}", Service.GENERAL) from exc
 
     body = res.json()
     sas_token = body["auth"]
