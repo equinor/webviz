d2e02f03c9bd94f5635f7a087f8c1b776c5fe07b
Ruben Thoms <ruben.thoms@ceetronsolutions.com>
2025-03-24 09:25:38 +0100

    Variable renaming

--- frontend/src/modules/_shared/LayerFramework/layers/LayerRegistry.ts
+++ frontend/src/modules/_shared/LayerFramework/layers/LayerRegistry.ts
@@ -9,37 +9,37 @@ export class LayerRegistry {
             customDataLayerImplementation: {
                 new (customParams?: any): CustomDataLayerImplementation<any, any, any, any, any, any>;
             };
-            customDataLayerImplementationParams?: any;
+            customDataLayerImplementationConstructorParams?: any;
         }
     > = new Map();
 
     static registerLayer<
-        TDataLayer extends { new (...params: any[]): CustomDataLayerImplementation<any, any, any, any, any, any> }
+        TDataLayer extends { new (...params: any[]): CustomDataLayerImplementation<any, any, any, any, any, any> },
     >(
         type: string,
         customDataLayerImplementation: TDataLayer,
-        customDataLayerImplementationParams?: ConstructorParameters<TDataLayer>
+        customDataLayerImplementationConstructorParams?: ConstructorParameters<TDataLayer>,
     ): void {
         if (this._registeredLayers.has(type)) {
             throw new Error(`Layer ${type} already registered`);
         }
         this._registeredLayers.set(type, {
             customDataLayerImplementation,
-            customDataLayerImplementationParams,
+            customDataLayerImplementationConstructorParams,
         });
     }
 
     static makeLayer(
         type: string,
         layerManager: DataLayerManager,
-        instanceName?: string
+        instanceName?: string,
     ): DataLayer<any, any, any, any> {
         const stored = this._registeredLayers.get(type);
         if (!stored) {
             throw new Error(`Layer ${type} not found`);
         }
         const customDataLayerImplementation = new stored.customDataLayerImplementation(
-            ...(stored.customDataLayerImplementationParams ?? [])
+            ...(stored.customDataLayerImplementationConstructorParams ?? []),
         );
 
         return new DataLayer({
