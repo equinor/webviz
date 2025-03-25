5ae969972ddc19ca7fed31bbf27163cf9fdc1ac9
Hans Kallekleiv <16436291+HansKallekleiv@users.noreply.github.com>
2025-03-25 14:11:12 +0100

    wip

--- frontend/src/modules/_shared/PlotlyTraceUtils/types.ts
+++
@@ -1,9 +0,0 @@
-import { PlotData } from "plotly.js";
-
-export interface PlotDataWithLegendRank extends Partial<PlotData> {
-    // TODO: Have realizationNumber?
-    //realizationNumber?: number | null;
-
-    // Did they forget to expose this one
-    legendrank?: number;
-}
