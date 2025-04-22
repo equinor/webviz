5d6e6172890a3209fdfb7af66e229dba143e7fdf
Ruben Thoms <ruben.thoms@ceetronsolutions.com>
2025-04-22 14:58:19 +0200

    fix

---
+++ frontend/src/modules/_shared/DataProviderFramework/dataProviders/DataProviderRegistry/_registerAllSharedDataProviders.ts
@@ -0,0 +1,7 @@
+import { DataProviderRegistry } from "./_DataProviderRegistry";
+import { DataProviderType } from "../dataProviderTypes";
+import { DrilledWellborePicksProvider } from "../implementations/DrilledWellborePicksProvider";
+import { DrilledWellTrajectoriesProvider } from "../implementations/DrilledWellTrajectoriesProvider";
+
+DataProviderRegistry.registerDataProvider(DataProviderType.DRILLED_WELLBORE_PICKS, DrilledWellborePicksProvider);
+DataProviderRegistry.registerDataProvider(DataProviderType.DRILLED_WELL_TRAJECTORIES, DrilledWellTrajectoriesProvider);
