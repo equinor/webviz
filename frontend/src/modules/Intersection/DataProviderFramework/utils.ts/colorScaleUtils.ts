2421d325b053d23392e85e48baddf27eb44e4bc6
jorgenherje <jorgen.herje@ceetronsolutions.com>
2025-05-02 14:21:47 +0200

    Fix typing of utility functions

--- frontend/src/modules/Intersection/DataProviderFramework/utils.ts/colorScaleUtils.ts
+++ frontend/src/modules/Intersection/DataProviderFramework/utils.ts/colorScaleUtils.ts
@@ -1,11 +1,17 @@
-export function gridColorScaleValues(valueRange: [number, number]): { min: number; max: number; mid: number } {
+/**
+ * Create color scale values for grid data from value range
+ */
+export function createGridColorScaleValues(valueRange: [number, number]): { min: number; max: number; mid: number } {
     const min = valueRange[0];
     const max = valueRange[1];
     const mid = min + (max - min) / 2;
     return { min, max, mid };
 }
 
-export function seismicColorScaleValues(valueRange: [number, number]): { min: number; max: number; mid: number } {
+/**
+ * Create color scale values for seismic data from value range
+ */
+export function createSeismicColorScaleValues(valueRange: [number, number]): { min: number; max: number; mid: number } {
     const min = Math.min(0.0, valueRange[0]);
     const max = Math.max(0.0, valueRange[1]);
     const mid = 0;
