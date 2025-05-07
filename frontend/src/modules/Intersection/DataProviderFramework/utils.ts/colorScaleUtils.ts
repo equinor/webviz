b58963c1e0378f81c9cda3b1de7af1e24eda6bb1
jorgenherje <jorgen.herje@ceetronsolutions.com>
2025-05-07 11:27:55 +0200

    Adjust seismic colorScale to be symmetric around 0

--- frontend/src/modules/Intersection/DataProviderFramework/utils.ts/colorScaleUtils.ts
+++ frontend/src/modules/Intersection/DataProviderFramework/utils.ts/colorScaleUtils.ts
@@ -10,10 +10,18 @@ export function createGridColorScaleValues(valueRange: [number, number]): { min:
 
 /**
  * Create color scale values for seismic data from value range
+ *
+ * Seismic color scale should be symmetric around 0, so we calculate the min and max values
  */
 export function createSeismicColorScaleValues(valueRange: [number, number]): { min: number; max: number; mid: number } {
     const min = Math.min(0.0, valueRange[0]);
     const max = Math.max(0.0, valueRange[1]);
     const mid = 0;
-    return { min, max, mid };
+
+    // Seismic data should be symmetric around 0
+    const absMax = Math.max(Math.abs(min), Math.abs(max));
+    const symmetricMax = absMax;
+    const symmetricMin = -absMax;
+
+    return { min: symmetricMin, max: symmetricMax, mid };
 }
