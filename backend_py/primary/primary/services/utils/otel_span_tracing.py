bfe0e5260507abc03f52300fe65a6b115d349925
Sigurd Pettersen <sigurd.pettersen@ceetronsolutions.com>
2025-05-08 11:05:57 +0200

    Typing and linting

--- backend_py/primary/primary/services/utils/otel_span_tracing.py
+++ backend_py/primary/primary/services/utils/otel_span_tracing.py
@@ -1,16 +1,17 @@
-import inspect
 import contextlib
+import inspect
 from functools import wraps
-from opentelemetry import trace
-from opentelemetry.trace import Status, StatusCode
-from opentelemetry.util.types import Attributes
+from typing import AsyncGenerator, Generator, Callable, Any
 
+from opentelemetry import trace
+from opentelemetry.trace import Span, Status, StatusCode
+from opentelemetry.util.types import Attributes
 
 _tracer = trace.get_tracer("PrimaryBackendSpanInstrumentation")
 
 
 @contextlib.asynccontextmanager
-async def start_otel_span_async(span_name, span_attributes: Attributes = None):
+async def start_otel_span_async(span_name: str, span_attributes: Attributes = None) -> AsyncGenerator[Span]:
     """
     Context manager to create an async OpenTelemetry span.
     Usage:
@@ -22,7 +23,7 @@ async def start_otel_span_async(span_name, span_attributes: Attributes = None):
 
 
 @contextlib.contextmanager
-def start_otel_span(span_name: str, span_attributes: Attributes = None):
+def start_otel_span(span_name: str, span_attributes: Attributes = None) -> Generator[Span]:
     """
     Context manager to create an sync OpenTelemetry span.
 
@@ -35,7 +36,7 @@ def start_otel_span(span_name: str, span_attributes: Attributes = None):
         yield span
 
 
-def otel_span_decorator(custom_span_name=None):
+def otel_span_decorator(custom_span_name: str | None = None) -> Callable:
     """
     Decorator to create an OpenTelemetry span for a function.
     Unless a custom span name is specified, the span name will be the function name.
@@ -47,40 +48,40 @@ def otel_span_decorator(custom_span_name=None):
         pass
     """
 
-    def decorator(func):
+    def decorator(func: Callable) -> Callable:
         is_coroutine = inspect.iscoroutinefunction(func)
 
         @wraps(func)
-        async def async_wrapper(*args, **kwargs):
+        async def _wrapper_async(*args: Any, **kwargs: Any) -> Callable:
             span_name = custom_span_name or _get_full_function_name(func, args) + "()"
             async with start_otel_span_async(span_name) as span:
                 try:
                     return await func(*args, **kwargs)
-                except Exception as e:
-                    span.record_exception(e)
-                    span.set_status(Status(StatusCode.ERROR, str(e)))
+                except Exception as exc:
+                    span.record_exception(exc)
+                    span.set_status(Status(StatusCode.ERROR, str(exc)))
                     raise
 
         @wraps(func)
-        def sync_wrapper(*args, **kwargs):
+        def _wrapper_sync(*args: Any, **kwargs: Any) -> Callable:
             span_name = custom_span_name or _get_full_function_name(func, args) + "()"
             with _tracer.start_as_current_span(span_name) as span:
                 try:
                     return func(*args, **kwargs)
-                except Exception as e:
-                    span.record_exception(e)
-                    span.set_status(Status(StatusCode.ERROR, str(e)))
+                except Exception as exc:
+                    span.record_exception(exc)
+                    span.set_status(Status(StatusCode.ERROR, str(exc)))
                     raise
 
         if is_coroutine:
-            return async_wrapper
-        else:
-            return sync_wrapper
+            return _wrapper_async
+
+        return _wrapper_sync
 
     return decorator
 
 
-def _get_full_function_name(func, args) -> str:
+def _get_full_function_name(func: Callable, args: Any) -> str:
     """
     Get 'ClassName.method_name' if possible, else just function name.
     """
