1248558a3ccb827cb9557b28757a58853ab0a647
Anders Rantala Hunderi <anders@ceetronsolutions.com>
2025-06-10 11:24:01 +0200

    fixed strat column error

--- backend_py/primary/primary/routers/surface/router.py
+++ backend_py/primary/primary/routers/surface/router.py
@@ -71,11 +71,14 @@ async def get_realization_surfaces_metadata(
 
     async with asyncio.TaskGroup() as tg:
         access = SurfaceAccess.from_iteration_name(authenticated_user.get_sumo_access_token(), case_uuid, ensemble_name)
+        case_inspector = CaseInspector(authenticated_user.get_sumo_access_token(), case_uuid)
+
         surf_meta_task = tg.create_task(access.get_realization_surfaces_metadata_async())
         surf_meta_task.add_done_callback(lambda _: perf_metrics.record_lap_no_reset("get-meta"))
 
+        strat_column_ident = await case_inspector.get_stratigraphic_column_identifier_async()
         strat_units_task = tg.create_task(
-            _get_stratigraphic_units_for_strat_column_async(authenticated_user, case_uuid)
+            _get_stratigraphic_units_for_strat_column_async(authenticated_user, strat_column_ident)
         )
         strat_units_task.add_done_callback(lambda _: perf_metrics.record_lap_no_reset("get-strat"))
 
@@ -105,11 +108,14 @@ async def get_observed_surfaces_metadata(
 
     async with asyncio.TaskGroup() as tg:
         access = SurfaceAccess.from_case_uuid_no_iteration(authenticated_user.get_sumo_access_token(), case_uuid)
+        case_inspector = CaseInspector(authenticated_user.get_sumo_access_token(), case_uuid)
+
         surf_meta_task = tg.create_task(access.get_observed_surfaces_metadata_async())
         surf_meta_task.add_done_callback(lambda _: perf_metrics.record_lap_no_reset("get-meta"))
 
+        strat_column_ident = await case_inspector.get_stratigraphic_column_identifier_async()
         strat_units_task = tg.create_task(
-            _get_stratigraphic_units_for_strat_column_async(authenticated_user, case_uuid)
+            _get_stratigraphic_units_for_strat_column_async(authenticated_user, strat_column_ident)
         )
         strat_units_task.add_done_callback(lambda _: perf_metrics.record_lap_no_reset("get-strat"))
 
