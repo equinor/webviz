bdf342142b834ce7b895a0d351422120b8767ee3
Ruben Thoms <ruben.thoms@ceetronsolutions.com>
2025-07-28 16:15:57 +0200

    wip

--- frontend/src/framework/internal/components/ActiveSessionRecoveryDialog/activeSessionRecoveryDialog.tsx
+++ frontend/src/framework/internal/components/ActiveSessionRecoveryDialog/activeSessionRecoveryDialog.tsx
@@ -1,8 +1,12 @@
 import { GuiState, useGuiState } from "@framework/GuiMessageBroker";
+import type { PrivateWorkbenchSession } from "@framework/internal/WorkbenchSession/PrivateWorkbenchSession";
+import { loadAllWorkbenchSessionsFromLocalStorage } from "@framework/internal/WorkbenchSession/WorkbenchSessionLoader";
 import type { Workbench } from "@framework/Workbench";
 import { Button } from "@lib/components/Button";
 import { Dialog } from "@lib/components/Dialog";
 import React from "react";
+import { DashboardPreview } from "../DashboardPreview/dashboardPreview";
+import { timeAgo } from "@lib/utils/dates";
 
 export type ActiveSessionRecoveryDialogProps = {
     workbench: Workbench;
@@ -13,7 +17,28 @@ export function ActiveSessionRecoveryDialog(props: ActiveSessionRecoveryDialogPr
 
     const activeSession = props.workbench.getWorkbenchSession();
 
-    if (!isOpen) {
+    const [session, setSession] = React.useState<PrivateWorkbenchSession | null>(null);
+
+    async function loadSession() {
+        const loadedSessions = await loadAllWorkbenchSessionsFromLocalStorage(
+            props.workbench.getAtomStoreMaster(),
+            props.workbench.getQueryClient(),
+        );
+
+        const storedSession = loadedSessions.find((s) => s.getId() === activeSession.getId());
+        setSession(storedSession || null);
+    }
+
+    React.useEffect(
+        function loadSessionOnOpen() {
+            if (isOpen) {
+                loadSession();
+            }
+        },
+        [isOpen],
+    );
+
+    if (!isOpen || !session) {
         return null;
     }
 
@@ -45,6 +70,23 @@ export function ActiveSessionRecoveryDialog(props: ActiveSessionRecoveryDialogPr
         >
             We found an unsaved version of your current session in your local storage. You can either discard it or open
             it to recover your work.
+            <div className="flex gap-4 mt-4">
+                <DashboardPreview height={150} width={150} layout={session?.getActiveDashboard()?.getLayout() ?? []} />
+                <div className="flex flex-col gap-2">
+                    <div className="flex flex-col gap-1">
+                        <strong className="text-xs text-gray-500">Title</strong>
+                        {session?.getMetadata().title}
+                    </div>
+                    <div className="flex flex-col gap-1">
+                        <strong className="text-xs text-gray-500">Last modified</strong>
+                        {timeAgo(Date.now() - session.getMetadata().lastModifiedMs)}
+                    </div>
+                    <div className="flex flex-col gap-1">
+                        <strong className="text-xs text-gray-500">Last persisted</strong>
+                        {timeAgo(Date.now() - activeSession.getMetadata().lastModifiedMs)}
+                    </div>
+                </div>
+            </div>
         </Dialog>
     );
 }
