161363a598dc7360f9e547afb04663d2fd997bad
Anders Rantala Hunderi <anders@ceetronsolutions.com>
2025-08-12 10:13:40 +0200

    Fixed sizing inconsistency in user avatar states

--- frontend/src/framework/internal/components/UserAvatar/userAvatar.tsx
+++ frontend/src/framework/internal/components/UserAvatar/userAvatar.tsx
@@ -6,10 +6,15 @@ import { useQuery } from "@tanstack/react-query";
 
 import type { GraphUserPhoto_api } from "@api";
 import { getUserPhotoOptions } from "@api";
+import type { SizeName } from "@framework/utils/getNamedSizeClass";
+import { getSizeClass } from "@framework/utils/getNamedSizeClass";
 import { CircularProgress } from "@lib/components/CircularProgress";
+import { resolveClassNames } from "@lib/utils/resolveClassNames";
 
 export type UserAvatarProps = {
     userEmail: string;
+    size?: SizeName;
+    className?: React.HTMLAttributes<HTMLDivElement>["className"];
 };
 
 function useUserInfoQuery(userEmail: string): UseQueryResult<GraphUserPhoto_api> {
@@ -25,24 +30,27 @@ function useUserInfoQuery(userEmail: string): UseQueryResult<GraphUserPhoto_api>
 
 export const UserAvatar: React.FC<UserAvatarProps> = (props) => {
     const userInfo = useUserInfoQuery(props.userEmail);
+    const sizeOrDefault = props.size ?? "medium-small";
+    const sizeClass = getSizeClass(sizeOrDefault);
 
     if (userInfo.isFetching) {
-        return <CircularProgress size="medium-small" className="mr-1" />;
+        return <CircularProgress size={sizeOrDefault} className={resolveClassNames("p-0.5", props.className)} />;
     }
 
     if (userInfo.data?.avatar_b64str) {
         return (
             <img
-                src={`data:image/png;base64,${userInfo.data.avatar_b64str}`}
+                src={`data:image/png;base64,${userInfo.data?.avatar_b64str}`}
                 alt="Avatar"
-                className="w-5 h-5 rounded-full mr-1"
                 title={props.userEmail}
+                className={resolveClassNames(sizeClass, "rounded-full p-0.5", props.className)}
             />
         );
     }
     return (
-        <span title={props.userEmail}>
-            <AccountCircle className="w-5 h-5 mr-1" />
+        <span title={props.userEmail} className={resolveClassNames(sizeClass, props.className)}>
+            {/* size-auto ensures the icon follows the wrapper size-class */}
+            <AccountCircle classes={{ root: "size-auto! " }} />
         </span>
     );
 };
