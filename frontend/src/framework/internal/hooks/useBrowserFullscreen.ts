d247642258821bc9958d23729f7b2daf5bd12d44
Anders Rantala Hunderi <anders@ceetronsolutions.com>
2025-08-15 15:31:57 +0200

    Added workaround for fullscreen issue

--- frontend/src/framework/internal/hooks/useBrowserFullscreen.ts
+++ frontend/src/framework/internal/hooks/useBrowserFullscreen.ts
@@ -19,13 +19,27 @@ export function useBrowserFullscreen(): [boolean, () => void] {
         else enterFullscreen();
     }, [enterFullscreen, exitFullscreen]);
 
-    React.useEffect(function setupFullscreenListener() {
+    React.useEffect(function setupFullscreenListeners() {
+        // ! The browsers (chrome and firefox at-least) are for some reason not actually using
+        // ! the Fullscreen API when the user presses their fullscreen key, causing 'fullscreenElement'
+        // ! to not be set. As a workaround, we manually call the API ourselves
+        function handleFullscreenKey(evt: KeyboardEvent) {
+            if (evt.code !== "F11") return;
+
+            evt.preventDefault();
+            toggleFullscreen();
+        }
+
         function handleFullscreenChange() {
             setIsFullscreen(document.fullscreenElement != null);
         }
 
+        document.addEventListener("keydown", handleFullscreenKey);
         document.addEventListener("fullscreenchange", handleFullscreenChange);
-        return () => document.removeEventListener("fullscreenchange", handleFullscreenChange);
+        return function tearDownFullscreenListeners() {
+            document.removeEventListener("keydown", handleFullscreenKey);
+            document.removeEventListener("fullscreenchange", handleFullscreenChange);
+        };
     });
 
     return [isFullscreen, toggleFullscreen];
