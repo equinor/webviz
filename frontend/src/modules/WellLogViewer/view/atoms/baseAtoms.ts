df0f536b0e9b39c149247de2392ad530560aa14e
Anders Rantala Hunderi <anders@ceetronsolutions.com>
2025-09-01 15:26:17 +0200

    Removed old workaround which is no longer needed

--- frontend/src/modules/WellLogViewer/view/atoms/baseAtoms.ts
+++ frontend/src/modules/WellLogViewer/view/atoms/baseAtoms.ts
@@ -4,7 +4,3 @@ import type { WellboreHeader_api } from "@api";
 
 export const wellboreHeaderAtom = atom<WellboreHeader_api | null>(null);
 export const selectedFieldIdentAtom = atom<string | null>(null);
-
-// Local switch to avoid unneccessary queries while applying interface effects.
-// See issue #846 (https://github.com/equinor/webviz/issues/846)
-export const lockQueriesAtom = atom<boolean>(false);
