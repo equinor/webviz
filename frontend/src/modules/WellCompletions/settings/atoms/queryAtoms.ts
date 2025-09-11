2c8bbeca4b2f2bb68b6fc27283e6c22538faf15a
jorgenherje <jorgen.herje@ceetronsolutions.com>
2025-09-11 14:07:10 +0200

    Add encoding of realizations array for endpoint
    Change arg to be `int | str | null` instead of `int | list[str] | null`

--- frontend/src/modules/WellCompletions/settings/atoms/queryAtoms.ts
+++ frontend/src/modules/WellCompletions/settings/atoms/queryAtoms.ts
@@ -1,8 +1,8 @@
 import { atomWithQuery } from "jotai-tanstack-query";
 
 import { getWellCompletionsDataOptions } from "@api";
+import { encodeAsUintListStr } from "@lib/utils/queryStringUtils";
 import { RealizationSelection } from "@modules/WellCompletions/typesAndEnums";
-
 
 import { userSelectedRealizationSelectionAtom } from "./baseAtoms";
 import { selectedEnsembleIdentAtom, selectedRealizationNumberAtom, validRealizationNumbersAtom } from "./derivedAtoms";
@@ -17,11 +17,15 @@ export const wellCompletionsQueryAtom = atomWithQuery((get) => {
     const ensembleName = selectedEnsembleIdent?.getEnsembleName();
 
     // Initialize with multiple realizations request
-    let realizations: number | number[] | null = validRealizationNumbers;
-    let hasValidRealizations = validRealizationNumbers.length !== 0;
+    let realizationsOrRealizationsEncodedAsUintListStr: number | string | null = null;
+    let hasValidRealizations = false;
     if (userSelectedRealizationSelection === RealizationSelection.SINGLE) {
-        realizations = selectedRealizationNumber;
+        realizationsOrRealizationsEncodedAsUintListStr = selectedRealizationNumber;
         hasValidRealizations = selectedRealizationNumber !== null;
+    }
+    if (userSelectedRealizationSelection === RealizationSelection.AGGREGATED) {
+        realizationsOrRealizationsEncodedAsUintListStr = encodeAsUintListStr(validRealizationNumbers);
+        hasValidRealizations = validRealizationNumbers.length !== 0;
     }
 
     // Disable query if realization number is null for single realization request
@@ -30,7 +34,7 @@ export const wellCompletionsQueryAtom = atomWithQuery((get) => {
             query: {
                 case_uuid: caseUuid ?? "",
                 ensemble_name: ensembleName ?? "",
-                realization: realizations,
+                realization_or_realizations_encoded_as_uint_list_str: realizationsOrRealizationsEncodedAsUintListStr,
             },
         }),
         enabled: Boolean(caseUuid && ensembleName && hasValidRealizations),
