92fcf87994afbdaffe1779c0fbf4140b70f10ba4
jorgenherje <jorgen.herje@ceetronsolutions.com>
2025-09-11 14:07:10 +0200

    Add encoding of realizations array for endpoint
    Change arg to be `int | str | null` instead of `int | list[str] | null`

--- frontend/src/api/autogen/types.gen.ts
+++ frontend/src/api/autogen/types.gen.ts
@@ -2828,9 +2828,9 @@ export type GetWellCompletionsDataData_api = {
          */
         ensemble_name: string;
         /**
-         * Optional realizations to include. Provide single realization or list of realizations. If not specified, all realizations will be returned.
+         * Optional realizations to include. Provide single realization or list of realizations encoded as string. If not specified, all realizations will be returned.
          */
-        realization?: number | Array<number> | null;
+        realization_or_realizations_encoded_as_uint_list_str?: number | string | null;
         t?: number;
     };
     url: "/well_completions/well_completions_data/";
