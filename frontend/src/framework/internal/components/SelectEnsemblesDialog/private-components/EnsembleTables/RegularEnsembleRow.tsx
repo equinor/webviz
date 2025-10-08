1dfb999cd53ecc407732e04e12f76dc6afca8cbd
jorgenherje <jorgen.herje@ceetronsolutions.com>
2025-10-08 13:11:04 +0200

    Format code front- and back-end

--- frontend/src/framework/internal/components/SelectEnsemblesDialog/private-components/EnsembleTables/RegularEnsembleRow.tsx
+++ frontend/src/framework/internal/components/SelectEnsemblesDialog/private-components/EnsembleTables/RegularEnsembleRow.tsx
@@ -1,10 +1,11 @@
 import type React from "react";
 
+import { DragIndicator, Remove } from "@mui/icons-material";
+
 import { ColorSelect } from "@lib/components/ColorSelect";
 import { IconButton } from "@lib/components/IconButton";
 import { Input } from "@lib/components/Input";
 import { SortableList } from "@lib/components/SortableList";
-import { DragIndicator, Remove } from "@mui/icons-material";
 
 import type { InternalRegularEnsembleSetting } from "../../types";
 
