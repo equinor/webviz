a5184c8924fe282a142dfb499d32693f25caf74f
Ruben Thoms <ruben.thoms@ceetronsolutions.com>
2025-10-13 23:08:04 +0200

    wip

--- frontend/src/framework/internal/components/NewLayout/controllers/LayoutController.ts
+++ frontend/src/framework/internal/components/NewLayout/controllers/LayoutController.ts
@@ -1,17 +1,56 @@
 import { GuiEvent, type GuiEventPayloads, type GuiMessageBroker } from "@framework/GuiMessageBroker";
+import type { Size2D } from "@lib/utils/geometry";
 import { UnsubscribeFunctionsManagerDelegate } from "@lib/utils/UnsubscribeFunctionsManagerDelegate";
+import type { Vec2 } from "@lib/utils/vec2";
 
-enum Mode {
+enum ModeKind {
     IDLE = "IDLE",
+    POINTER_DOWN = "POINTER_DOWN",
     DRAGGING = "DRAGGING",
     RESIZING = "RESIZING",
 }
+
+export enum DragSourceKind {
+    EXISTING = "EXISTING",
+    NEW = "NEW",
+}
+
+export type DragSource = {
+    id: string;
+    elementPos: Vec2;
+    elementSize: Size2D;
+    pointerDownClientPos: Vec2;
+} & ({ kind: DragSourceKind.EXISTING } | { kind: DragSourceKind.NEW; moduleName: string });
+
+export type ResizeSource = {};
+
+type Mode =
+    | {
+          kind: ModeKind.IDLE;
+      }
+    | {
+          kind: ModeKind.POINTER_DOWN;
+          source: DragSource;
+          pointerOffset: Vec2;
+          lastClientPos: Vec2;
+      }
+    | {
+          kind: ModeKind.DRAGGING;
+          source: DragSource;
+          pointerOffset: Vec2;
+          lastClientPos: Vec2;
+      }
+    | {
+          kind: ModeKind.RESIZING;
+          source: ResizeSource;
+          lastClientPos: Vec2;
+      };
 
 export class LayoutController {
     private _guiMessageBroker: GuiMessageBroker;
     private _unsubscribeFunctionsManagerDelegate = new UnsubscribeFunctionsManagerDelegate();
 
-    private _mode: Mode = Mode.IDLE;
+    private _mode: Mode = { kind: ModeKind.IDLE };
 
     constructor(guiMessageBroker: GuiMessageBroker) {
         this._guiMessageBroker = guiMessageBroker;
