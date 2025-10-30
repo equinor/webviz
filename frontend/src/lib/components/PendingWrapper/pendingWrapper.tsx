2c637ed71525e4b9130dcfe7c81ab10026eeba26
jorgenherje <jorgen.herje@ceetronsolutions.com>
2025-10-30 15:25:16 +0100

    THIS IS A CHERRY PICK DO NOT INCLUDE in MERGE TO MAIN!!!
    "Add StatusWrapper and modify PendingWrapper"
    - New StatusWrapper component
    - Use StatusWrapper in PendingWrapper

--- frontend/src/lib/components/PendingWrapper/pendingWrapper.tsx
+++ frontend/src/lib/components/PendingWrapper/pendingWrapper.tsx
@@ -3,33 +3,33 @@ import type React from "react";
 import { CircularProgress } from "@lib/components/CircularProgress";
 import { resolveClassNames } from "@lib/utils/resolveClassNames";
 
-// Base state wrapper props
+import { StatusWrapper } from "../StatusWrapper";
+
 export type PendingWrapperProps = {
     isPending: boolean;
-    className?: string;
-    errorMessage?: string;
     children: React.ReactNode;
+    errorMessage?: string;
+    warningMessage?: string;
+    infoMessage?: string;
+    className?: string;
 };
 
 export const PendingWrapper: React.FC<PendingWrapperProps> = (props) => {
     return (
-        <div
-            className={resolveClassNames("relative rounded-sm", props.className, {
+        <StatusWrapper
+            errorMessage={props.isPending ? undefined : props.errorMessage}
+            warningMessage={props.isPending ? undefined : props.warningMessage}
+            infoMessage={props.isPending ? undefined : props.infoMessage}
+            className={resolveClassNames("relative", props.className, {
                 "outline outline-blue-100 outline-offset-2": props.isPending,
-                "outline outline-red-100 outline-offset-2": Boolean(!props.isPending && props.errorMessage),
             })}
         >
             {props.isPending && (
-                <div className="absolute left-0 right-0 w-full h-full bg-white/80 flex items-center justify-center z-10 backdrop-blur-xs">
+                <div className="absolute inset-0 bg-white/80 flex items-center justify-center z-20 backdrop-blur-xs">
                     <CircularProgress size="medium-small" />
                 </div>
             )}
-            {!props.isPending && props.errorMessage && (
-                <div className="absolute left-0 right-0 w-full h-full bg-white/80 flex items-center justify-center z-10 p-4 text-center backdrop-blur-xs">
-                    {props.errorMessage}
-                </div>
-            )}
             {props.children}
-        </div>
+        </StatusWrapper>
     );
 };
---
+++ frontend/src/lib/components/StatusWrapper/index.ts
@@ -0,0 +1,1 @@
+export { StatusWrapper } from "./statusWrapper";
