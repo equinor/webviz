d5339e41c00fbd9581b523e32246d347f1a62bf6
jorgenherje <jorgen.herje@ceetronsolutions.com>
2025-11-14 15:55:28 +0100

    Add persistence

--- frontend/src/modules/WellCompletions/registerModule.ts
+++ frontend/src/modules/WellCompletions/registerModule.ts
@@ -3,12 +3,13 @@ import { ModuleDataTagId } from "@framework/ModuleDataTags";
 import { ModuleRegistry } from "@framework/ModuleRegistry";
 
 import type { Interfaces } from "./interfaces";
+import { SERIALIZED_STATE_SCHEMA, type SerializedState } from "./persistence";
 import { preview } from "./preview";
 
 export const MODULE_NAME = "WellCompletions";
 const description = "Vizualizes Eclipse well completions data per well. The data is grouped by well and zone.";
 
-ModuleRegistry.registerModule<Interfaces>({
+ModuleRegistry.registerModule<Interfaces, SerializedState>({
     moduleName: MODULE_NAME,
     defaultTitle: "Well Completions",
     category: ModuleCategory.MAIN,
@@ -16,4 +17,5 @@ ModuleRegistry.registerModule<Interfaces>({
     dataTagIds: [ModuleDataTagId.WELL_COMPLETIONS],
     preview,
     description,
+    serializedStateSchema: SERIALIZED_STATE_SCHEMA,
 });
