b7a42e70f8551f694e12a80975b5448e109629a5
Jørgen Herje <82032112+jorgenherje@users.noreply.github.com>
2025-11-20 14:18:13 +0100

    [Persistence]: Implement module persistence for SimulationTimeSeriesPerSensitivity (#1288)

--- frontend/src/modules/SimulationTimeSeriesSensitivity/settings/atoms/baseAtoms.ts
+++ frontend/src/modules/SimulationTimeSeriesSensitivity/settings/atoms/baseAtoms.ts
@@ -1,17 +1,7 @@
 import { atom } from "jotai";
-import { isEqual } from "lodash";
 
 import { Frequency_api } from "@api";
 import type { RegularEnsembleIdent } from "@framework/RegularEnsembleIdent";
-import { atomWithCompare } from "@framework/utils/atomUtils";
-
-
-function areRegularEnsembleIdentsEqual(a: RegularEnsembleIdent | null, b: RegularEnsembleIdent | null) {
-    if (a === null) {
-        return b === null;
-    }
-    return a.equals(b);
-}
 
 export const syncedRegularEnsembleIdentsAtom = atom<RegularEnsembleIdent[] | null>(null);
 export const syncedVectorNameAtom = atom<string | null>(null);
@@ -20,18 +10,3 @@ export const resamplingFrequencyAtom = atom<Frequency_api | null>(Frequency_api.
 export const showStatisticsAtom = atom<boolean>(true);
 export const showRealizationsAtom = atom<boolean>(false);
 export const showHistoricalAtom = atom<boolean>(true);
-
-export const userSelectedRegularEnsembleIdentAtom = atomWithCompare<RegularEnsembleIdent | null>(
-    null,
-    areRegularEnsembleIdentsEqual,
-);
-export const userSelectedVectorNameAndTagAtom = atomWithCompare<{ name: string | null; tag: string | null }>(
-    {
-        name: null,
-        tag: null,
-    },
-    isEqual,
-);
-
-// Note: Default value of null, to detect uninitialized state, and select all sensitivities on first render
-export const userSelectedSensitivityNamesAtom = atomWithCompare<string[] | null>(null, isEqual);
