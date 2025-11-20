d941efe8a0f64020b98f7dcae84fe83d54d0a4bd
Ruben Thoms <ruben.thoms@ceetronsolutions.com>
2025-11-20 15:29:13 +0100

    Added function names to `useMemo` hooks

--- frontend/src/modules/Vfp/view/hooks/useVfpPlotBuilder.ts
+++ frontend/src/modules/Vfp/view/hooks/useVfpPlotBuilder.ts
@@ -1,13 +1,12 @@
 import React from "react";
 
-import type { Layout, PlotData } from "plotly.js";
-
 import type { ViewContext } from "@framework/ModuleContext";
 import { useContinuousColorScale, type WorkbenchSettings } from "@framework/WorkbenchSettings";
 import { ColorScaleGradientType } from "@lib/utils/ColorScale";
 import type { Size2D } from "@lib/utils/geometry";
 import type { Interfaces } from "@modules/Vfp/interfaces";
 import { VfpType } from "@modules/Vfp/types";
+import type { Layout, PlotData } from "plotly.js";
 
 import { VfpPlotBuilder } from "../utils/vfpPlotBuilder";
 
@@ -27,52 +26,61 @@ export function usePlotBuilder(
     const selectedColorBy = viewContext.useSettingsToViewInterfaceValue("selectedColorBy");
 
     // Memoize plot builder to avoid unnecessary re-creation
-    const vfpTypeAndPlotBuilder = React.useMemo<{ type: VfpType; plotBuilder: VfpPlotBuilder } | null>(() => {
-        if (!tableDataAccessor) {
-            return null;
-        }
+    const vfpTypeAndPlotBuilder = React.useMemo<{ type: VfpType; plotBuilder: VfpPlotBuilder } | null>(
+        function makeVfpTypeAndPlotBuilder() {
+            if (!tableDataAccessor) {
+                return null;
+            }
 
-        return {
-            type: tableDataAccessor.getVfpType(),
-            plotBuilder: new VfpPlotBuilder(tableDataAccessor, colorScale),
-        };
-    }, [tableDataAccessor, colorScale]);
+            return {
+                type: tableDataAccessor.getVfpType(),
+                plotBuilder: new VfpPlotBuilder(tableDataAccessor, colorScale),
+            };
+        },
+        [tableDataAccessor, colorScale],
+    );
 
-    const plotData = React.useMemo(() => {
-        if (!vfpTypeAndPlotBuilder) {
-            return null;
-        }
-        if (vfpTypeAndPlotBuilder.type === VfpType.VFPPROD) {
-            return vfpTypeAndPlotBuilder.plotBuilder.makeVfpProdTraces(
-                selectedThpIndices,
-                selectedWfrIndices,
-                selectedGfrIndices,
-                selectedAlqIndices,
-                selectedPressureOption,
-                selectedColorBy,
-            );
-        }
-        if (vfpTypeAndPlotBuilder.type === VfpType.VFPINJ) {
-            return vfpTypeAndPlotBuilder.plotBuilder.makeVfpInjTraces(selectedThpIndices, selectedPressureOption);
-        }
+    const plotData = React.useMemo(
+        function makePlotData() {
+            if (!vfpTypeAndPlotBuilder) {
+                return null;
+            }
+            if (vfpTypeAndPlotBuilder.type === VfpType.VFPPROD) {
+                return vfpTypeAndPlotBuilder.plotBuilder.makeVfpProdTraces(
+                    selectedThpIndices,
+                    selectedWfrIndices,
+                    selectedGfrIndices,
+                    selectedAlqIndices,
+                    selectedPressureOption,
+                    selectedColorBy,
+                );
+            }
+            if (vfpTypeAndPlotBuilder.type === VfpType.VFPINJ) {
+                return vfpTypeAndPlotBuilder.plotBuilder.makeVfpInjTraces(selectedThpIndices, selectedPressureOption);
+            }
 
-        throw new Error(`Unsupported VFP type: ${vfpTypeAndPlotBuilder.type}`);
-    }, [
-        selectedAlqIndices,
-        selectedColorBy,
-        selectedGfrIndices,
-        selectedPressureOption,
-        selectedThpIndices,
-        selectedWfrIndices,
-        vfpTypeAndPlotBuilder,
-    ]);
+            throw new Error(`Unsupported VFP type: ${vfpTypeAndPlotBuilder.type}`);
+        },
+        [
+            selectedAlqIndices,
+            selectedColorBy,
+            selectedGfrIndices,
+            selectedPressureOption,
+            selectedThpIndices,
+            selectedWfrIndices,
+            vfpTypeAndPlotBuilder,
+        ],
+    );
 
-    const plotLayout = React.useMemo(() => {
-        if (!vfpTypeAndPlotBuilder) {
-            return null;
-        }
-        return vfpTypeAndPlotBuilder.plotBuilder.makeLayout(plotSize, selectedPressureOption);
-    }, [vfpTypeAndPlotBuilder, plotSize, selectedPressureOption]);
+    const plotLayout = React.useMemo(
+        function makePlotLayout() {
+            if (!vfpTypeAndPlotBuilder) {
+                return null;
+            }
+            return vfpTypeAndPlotBuilder.plotBuilder.makeLayout(plotSize, selectedPressureOption);
+        },
+        [vfpTypeAndPlotBuilder, plotSize, selectedPressureOption],
+    );
 
     if (!plotData || !plotLayout) {
         return null;
