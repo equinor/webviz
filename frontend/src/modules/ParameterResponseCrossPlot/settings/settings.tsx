145b076ce1af0ecce0513337561ce354c3cabde4
Jørgen Herje <82032112+jorgenherje@users.noreply.github.com>
2025-11-21 15:29:43 +0100

    [Persistence]: Implement persistence for ParameterResponseCrossPlot-module (#1340)
    Co-authored-by: Hans Kallekleiv <16436291+HansKallekleiv@users.noreply.github.com>

--- frontend/src/modules/ParameterResponseCrossPlot/settings/settings.tsx
+++ frontend/src/modules/ParameterResponseCrossPlot/settings/settings.tsx
@@ -1,12 +1,8 @@
 import React from "react";
 
-import { useAtom } from "jotai";
+import { useAtom, useSetAtom } from "jotai";
 
-import { ParameterIdent } from "@framework/EnsembleParameters";
-import { useApplyInitialSettingsToState } from "@framework/InitialSettings";
 import type { ModuleSettingsProps } from "@framework/Module";
-import { RegularEnsemble } from "@framework/RegularEnsemble";
-import { RegularEnsembleIdent } from "@framework/RegularEnsembleIdent";
 import { SyncSettingKey, SyncSettingsHelper } from "@framework/SyncSettings";
 import { KeyKind } from "@framework/types/dataChannnel";
 import { Checkbox } from "@lib/components/Checkbox";
@@ -14,29 +10,22 @@ import { CollapsibleGroup } from "@lib/components/CollapsibleGroup";
 import { Dropdown } from "@lib/components/Dropdown";
 import { Label } from "@lib/components/Label";
 import { Select } from "@lib/components/Select";
-import { getContinuousAndNonConstantParameterIdentsInEnsembles } from "@modules/_shared/parameterUnions";
 
 import type { Interfaces } from "../interfaces";
 import { PlotType } from "../typesAndEnums";
 
-import { parameterIdentStringAtom, plotTypeAtom, showTrendlineAtom } from "./atoms/baseAtoms";
-
+import { plotTypeAtom, receivedChannelAtom, showTrendlineAtom } from "./atoms/baseAtoms";
+import { availableParameterIdentsAtom } from "./atoms/derivedAtoms";
+import { parameterIdentStringAtom } from "./atoms/persistedAtoms";
 const plotTypes = [{ value: PlotType.ParameterResponseCrossPlot, label: "Parameter correlation" }];
 
 //-----------------------------------------------------------------------------------------------------------
-export function Settings({
-    initialSettings,
-    settingsContext,
-    workbenchSession,
-    workbenchServices,
-}: ModuleSettingsProps<Interfaces>) {
+export function Settings({ settingsContext, workbenchServices }: ModuleSettingsProps<Interfaces>) {
     const [plotType, setPlotType] = useAtom(plotTypeAtom);
     const [parameterIdentString, setParameterIdentString] = useAtom(parameterIdentStringAtom);
     const [showTrendline, setShowTrendline] = useAtom(showTrendlineAtom);
-
-    useApplyInitialSettingsToState(initialSettings, "plotType", "string", setPlotType);
-    useApplyInitialSettingsToState(initialSettings, "parameterIdentString", "string", setParameterIdentString);
-
+    const setReceivedChannel = useSetAtom(receivedChannelAtom);
+    const [availableParameterIdents] = useAtom(availableParameterIdentsAtom);
     const syncedSettingKeys = settingsContext.useSyncedSettingKeys();
     const syncHelper = new SyncSettingsHelper(syncedSettingKeys, workbenchServices);
     const globalSyncedParameter = syncHelper.useValue(SyncSettingKey.PARAMETER, "global.syncValue.parameter");
@@ -46,27 +35,12 @@ export function Settings({
         receiverIdString: "channelResponse",
         expectedKindsOfKeys: [KeyKind.REALIZATION],
     });
-
-    let ensembleIdentStringsFromChannels: string[] = [];
-    if (!receiverResponse.channel || !receiverResponse.channel.contents) {
-        ensembleIdentStringsFromChannels = [];
-    } else {
-        ensembleIdentStringsFromChannels = receiverResponse.channel.contents.map((content) => {
-            return content.metaData.ensembleIdentString;
-        });
-    }
-
-    const ensembleSet = workbenchSession.getEnsembleSet();
-
-    // The channel could have non-regular ensembles (?)
-    const regularEnsembleIdentsFromChannels: RegularEnsembleIdent[] = ensembleIdentStringsFromChannels.flatMap((id) => {
-        const ensemble = ensembleSet.findEnsembleByIdentString(id);
-        return ensemble instanceof RegularEnsemble ? [RegularEnsembleIdent.fromString(id)] : [];
-    });
-
-    const parameterIdents = getContinuousAndNonConstantParameterIdentsInEnsembles(
-        ensembleSet,
-        regularEnsembleIdentsFromChannels,
+    React.useEffect(
+        () => {
+            setReceivedChannel(receiverResponse);
+        }, // We only want to listen to revision number changes, but we need the whole channel response to set it
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        [receiverResponse.revisionNumber, setReceivedChannel],
     );
 
     // Check if the parameterIdentString/globalSyncedParameter is valid
@@ -74,22 +48,13 @@ export function Settings({
 
     React.useEffect(
         function updateParameterIdentString() {
-            const newParameterIdentString = fixupParameterIdent(parameterIdents, parameterIdentString);
-            if (newParameterIdentString !== parameterIdentString) {
-                setParameterIdentString(newParameterIdentString);
-            }
-            if (globalSyncedParameter !== null && globalSyncedParameter !== parameterIdentString) {
-                if (fixupParameterIdent(parameterIdents, globalSyncedParameter) === globalSyncedParameter) {
-                    setParameterIdentString(globalSyncedParameter);
-                }
+            if (globalSyncedParameter !== null && globalSyncedParameter !== parameterIdentString.value) {
+                setParameterIdentString(globalSyncedParameter);
             }
         },
-        [parameterIdents, globalSyncedParameter, parameterIdentString, setParameterIdentString],
+        [globalSyncedParameter, parameterIdentString, setParameterIdentString],
     );
 
-    if (ensembleIdentStringsFromChannels.length === 0) {
-        return;
-    }
     function handlePlotTypeChanged(value: string) {
         setPlotType(value as PlotType);
     }
@@ -101,7 +66,7 @@ export function Settings({
             syncHelper.publishValue(SyncSettingKey.PARAMETER, "global.syncValue.parameter", selectedValue);
         }
     }
-    const parameterOptions = parameterIdents.map((parameterIdent) => ({
+    const parameterOptions = availableParameterIdents.map((parameterIdent) => ({
         value: parameterIdent.toString(),
         label: `${parameterIdent.name} (${parameterIdent.groupName})`,
     }));
@@ -118,7 +83,7 @@ export function Settings({
             </CollapsibleGroup>
             <CollapsibleGroup title="Parameter selection" expanded>
                 <Select
-                    value={parameterIdentString ? [parameterIdentString] : [""]}
+                    value={parameterIdentString.value ? [parameterIdentString.value] : [""]}
                     onChange={handleParameterChanged}
                     options={parameterOptions}
                     multiple={false}
@@ -128,23 +93,4 @@ export function Settings({
             </CollapsibleGroup>
         </div>
     );
-}
-
-function fixupParameterIdent(parameterIdents: ParameterIdent[], parameterIdentString: string | null) {
-    if (parameterIdents.length > 0) {
-        const currentParamIdent = parameterIdentString ? ParameterIdent.fromString(parameterIdentString) : null;
-        const isCurrentParamValid = currentParamIdent
-            ? parameterIdents.some((availableParam) =>
-                  currentParamIdent.equals(
-                      ParameterIdent.fromNameAndGroup(availableParam.name, availableParam.groupName),
-                  ),
-              )
-            : false;
-
-        if (!parameterIdentString || !isCurrentParamValid) {
-            return parameterIdents[0].toString();
-        }
-        return parameterIdentString;
-    }
-    return null;
 }
