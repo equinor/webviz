d906ec71936832737ddb7e543115dc9a93d64b63
Ruben Thoms <ruben.thoms@ceetronsolutions.com>
2025-12-09 17:50:54 +0100

    wip

--- frontend/src/modules/_shared/DataProviderFramework/settings/implementations/SliderRangeSetting.tsx
+++ frontend/src/modules/_shared/DataProviderFramework/settings/implementations/SliderRangeSetting.tsx
@@ -272,7 +272,6 @@ export class SliderRangeSetting
                             ]}
                             valueLabelDisplay="auto"
                             step={step}
-                            allowLockingToMinMax
                         />
                     </div>
                     <ToggleButton size="small" active={localValue[1] === "max"} onToggle={handleLockMaxToggle}>
