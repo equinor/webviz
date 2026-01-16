92382b33485a40e6452402907e7fa8db00673c38
jorgenherje <jorgen.herje@ceetronsolutions.com>
2026-01-16 13:41:40 +0100

    Simplify EnsembleColorTile prop to prevent internal business logic
    Only provide isRealizationFilterEffective flag, and handle use of ensembleRealizationFilterFunction outside of the simple framework component

--- frontend/src/framework/components/EnsembleColorTile/ensembleColorTile.tsx
+++ frontend/src/framework/components/EnsembleColorTile/ensembleColorTile.tsx
@@ -2,47 +2,28 @@ import { FilterAlt } from "@mui/icons-material";
 
 import type { DeltaEnsemble } from "@framework/DeltaEnsemble";
 import type { RegularEnsemble } from "@framework/RegularEnsemble";
-import { isEnsembleRealizationFilterEffective } from "@framework/utils/realizationFilterUtils";
-import type { EnsembleRealizationFilterFunction } from "@framework/WorkbenchSession";
 import { ColorTileWithBadge } from "@lib/components/ColorTileWithBadge";
 import { Tooltip } from "@lib/components/Tooltip";
 
-type EnsembleColorTileBaseProps = {
+export type EnsembleColorTileProps = {
     ensemble: RegularEnsemble | DeltaEnsemble;
+    isRealizationFilterEffective?: boolean;
     wrapperClassName?: string;
     badgeClassName?: string;
 };
-type EnsembleColorTileWithFilterFuncProps = EnsembleColorTileBaseProps & {
-    ensembleRealizationFilterFunction?: EnsembleRealizationFilterFunction;
-};
-type EnsembleColorTileWithFilterFlagProps = EnsembleColorTileBaseProps & {
-    isRealizationFilterEffective: boolean;
-};
-
-export type EnsembleColorTileProps = EnsembleColorTileWithFilterFuncProps | EnsembleColorTileWithFilterFlagProps;
 
 export function EnsembleColorTile(props: EnsembleColorTileProps): React.ReactNode {
     const { ensemble, badgeClassName } = props;
 
-    let isRealizationFilterEffective = false;
-    if ("isRealizationFilterEffective" in props) {
-        isRealizationFilterEffective = props.isRealizationFilterEffective;
-    } else {
-        isRealizationFilterEffective = isEnsembleRealizationFilterEffective(
-            ensemble,
-            props.ensembleRealizationFilterFunction,
-        );
-    }
-
     return (
         <Tooltip
-            title={isRealizationFilterEffective ? "Some realizations are being filtered out" : undefined}
+            title={props.isRealizationFilterEffective ? "Some realizations are being filtered out" : undefined}
             enterDelay="medium"
         >
             <span className={`flex items-center ${props.wrapperClassName}`}>
                 <ColorTileWithBadge
                     color={ensemble.getColor()}
-                    showBadge={isRealizationFilterEffective}
+                    showBadge={props.isRealizationFilterEffective ?? false}
                     badgeIcon={FilterAlt}
                     badgeClassName={badgeClassName}
                 />
