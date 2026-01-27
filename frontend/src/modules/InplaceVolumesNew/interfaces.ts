8526a4969d25759ce4e07c2946e405286e1cf138
Hans Kallekleiv <16436291+HansKallekleiv@users.noreply.github.com>
2026-01-27 15:28:28 +0100

    New inplace volumes plotting module (#1391)
    Co-authored-by: jorgenherje <jorgen.herje@ceetronsolutions.com>

---
+++ frontend/src/modules/InplaceVolumesNew/interfaces.ts
@@ -0,0 +1,49 @@
+import type { InterfaceInitialization } from "@framework/UniDirectionalModuleComponentsInterface";
+
+import { plotOptionsAtom, selectedPlotTypeAtom, showTableAtom } from "./settings/atoms/baseAtoms";
+import { areSelectedTablesComparableAtom, areTableDefinitionSelectionsValidAtom } from "./settings/atoms/derivedAtoms";
+import {
+    selectedColorByAtom,
+    selectedEnsembleIdentsAtom,
+    selectedIndicesWithValuesAtom,
+    selectedResultNameAtom,
+    selectedSelectorColumnAtom,
+    selectedSubplotByAtom,
+    selectedTableNamesAtom,
+} from "./settings/atoms/persistableFixableAtoms";
+import type { InplaceVolumesFilterSelections, InplaceVolumesPlotOptions, PlotType } from "./typesAndEnums";
+
+export type SettingsToViewInterface = {
+    filter: InplaceVolumesFilterSelections;
+    resultName: string | null;
+    selectorColumn: string | null;
+    subplotBy: string;
+    colorBy: string;
+    plotType: PlotType;
+    areTableDefinitionSelectionsValid: boolean;
+    plotOptions: InplaceVolumesPlotOptions;
+    showTable: boolean;
+};
+
+export type Interfaces = {
+    settingsToView: SettingsToViewInterface;
+};
+
+export const settingsToViewInterfaceInitialization: InterfaceInitialization<SettingsToViewInterface> = {
+    filter: (get) => {
+        return {
+            ensembleIdents: get(selectedEnsembleIdentsAtom).value,
+            tableNames: get(selectedTableNamesAtom).value,
+            indicesWithValues: get(selectedIndicesWithValuesAtom).value,
+            areSelectedTablesComparable: get(areSelectedTablesComparableAtom),
+        };
+    },
+    resultName: (get) => get(selectedResultNameAtom).value,
+    selectorColumn: (get) => get(selectedSelectorColumnAtom).value,
+    subplotBy: (get) => get(selectedSubplotByAtom).value,
+    colorBy: (get) => get(selectedColorByAtom).value,
+    plotType: (get) => get(selectedPlotTypeAtom),
+    areTableDefinitionSelectionsValid: (get) => get(areTableDefinitionSelectionsValidAtom),
+    plotOptions: (get) => get(plotOptionsAtom),
+    showTable: (get) => get(showTableAtom),
+};
---
+++ frontend/src/modules/InplaceVolumesNew/view/atoms/baseAtoms.ts
@@ -0,0 +1,18 @@
+import { atom } from "jotai";
+
+import { TableOriginKey } from "@modules/_shared/InplaceVolumes/types";
+import type { InplaceVolumesFilterSelections } from "@modules/InplaceVolumesNew/typesAndEnums";
+import { PlotType } from "@modules/InplaceVolumesNew/typesAndEnums";
+
+export const filterAtom = atom<InplaceVolumesFilterSelections>({
+    ensembleIdents: [],
+    tableNames: [],
+    indicesWithValues: [],
+    areSelectedTablesComparable: false,
+});
+export const resultNameAtom = atom<string | null>(null);
+export const selectorColumnAtom = atom<string | null>(null);
+export const subplotByAtom = atom<string>(TableOriginKey.ENSEMBLE);
+export const plotTypeAtom = atom<PlotType>(PlotType.HISTOGRAM);
+export const colorByAtom = atom<string>(TableOriginKey.TABLE_NAME);
+export const areTableDefinitionSelectionsValidAtom = atom(false);
