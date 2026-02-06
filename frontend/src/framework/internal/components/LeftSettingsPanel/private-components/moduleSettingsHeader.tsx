3cc2495d00c2e5c8d2f7de8f2463b72697c2ca2f
jorgenherje <jorgen.herje@ceetronsolutions.com>
2026-02-06 09:25:10 +0100

    Adjust shadow and hover shape to rounded for SettingsTab

--- frontend/src/framework/internal/components/LeftSettingsPanel/private-components/moduleSettingsHeader.tsx
+++ frontend/src/framework/internal/components/LeftSettingsPanel/private-components/moduleSettingsHeader.tsx
@@ -59,8 +59,8 @@ const SettingTab: React.FC<SettingTabProps> = (props) => {
                 className={resolveClassNames(
                     "relative flex items-center justify-center h-full px-2 transition-colors",
                     {
-                        "bg-white rounded-t-xl": props.isActive,
-                        "hover:bg-slate-200 cursor-pointer": !props.isActive,
+                        "bg-white rounded-t-xl inset-shadow": props.isActive,
+                        "hover:bg-slate-200 hover:rounded-t-xl cursor-pointer": !props.isActive,
                     },
                 )}
             >
@@ -106,7 +106,7 @@ export const ModuleSettingsHeader: React.FC<ModuleSettingsHeaderProps> = (props)
         }
 
         return (
-            <div className="flex h-full items-center" onMouseLeave={() => setHoveredTabIndex(null)}>
+            <div className="flex h-full items-center pl-px" onMouseLeave={() => setHoveredTabIndex(null)}>
                 {settingKeys.map((key, index) => {
                     const isActive = key === props.activeSetting;
                     const isNextActive = settingKeys.at(index + 1) === props.activeSetting;
@@ -180,7 +180,7 @@ export const ModuleSettingsHeader: React.FC<ModuleSettingsHeaderProps> = (props)
 
     return (
         <div className="flex flex-col">
-            <div className="flex items-center bg-slate-100 h-10 shadow-sm gap-2 pr-2">{makeHeaderContent()}</div>
+            <div className="flex items-center bg-slate-100 h-10 --shadow-sm gap-2 pr-2">{makeHeaderContent()}</div>
             {!props.isCollapsed && isWarningVisible && warningText && (
                 <WarningBanner text={warningText} onDismiss={dismissWarning} />
             )}
