bc92c44e0e04b56795129660eb1ba390dcf82889
Anders Rantala Hunderi <anders@ceetronsolutions.com>
2026-02-26 16:46:59 +0100

    Made MenuButton properly implement forward-ref

--- frontend/src/lib/components/MenuButton/menuButton.tsx
+++ frontend/src/lib/components/MenuButton/menuButton.tsx
@@ -1,26 +1,29 @@
-import type React from "react";
+import React from "react";
 
 import { MenuButton as MuiMenuButton } from "@mui/base/MenuButton";
 
 import { resolveClassNames } from "@lib/utils/resolveClassNames";
 
 export type MenuButtonProps = {
-    ref?: React.Ref<HTMLButtonElement>;
     label?: string;
     disabled?: boolean;
     children?: React.ReactNode;
 };
 
-export function MenuButton(props: MenuButtonProps): React.ReactNode {
+function MenuButtonComponent(props: MenuButtonProps, ref: React.ForwardedRef<HTMLButtonElement>): React.ReactNode {
     return (
         <MuiMenuButton
             {...props}
+            ref={ref}
             className={resolveClassNames(
                 "p-1 text-sm rounded-sm flex gap-1 items-center text-gray-600",
                 "hover:bg-blue-200 hover:text-gray-900",
                 "focus:outline focus:outline-blue-600",
                 "disabled:pointer-events-none disabled:text-gray-400",
+                "[&.Mui-expanded]:bg-blue-300",
             )}
         />
     );
 }
+
+export const MenuButton = React.forwardRef(MenuButtonComponent);
