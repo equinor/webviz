3946413beeab3690c1c9cfd851d902251c1a7aee
Hans Kallekleiv <16436291+HansKallekleiv@users.noreply.github.com>
2026-03-03 15:48:00 +0100

    Remove current track from wellbore header info

--- frontend/src/api/autogen/types.gen.ts
+++ frontend/src/api/autogen/types.gen.ts
@@ -2870,10 +2870,6 @@ export type WellboreHeader_api = {
      */
     wellboreStatus: string;
     /**
-     * Currenttrack
-     */
-    currentTrack: number;
-    /**
      * Tvdmax
      */
     tvdMax: number;
