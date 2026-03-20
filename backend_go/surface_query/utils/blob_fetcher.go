10c67662f4a60fb7ab9a6d9998e97f5ac5a4db37
Sigurd Pettersen <sigurd.pettersen@ceetronsolutions.com>
2026-03-20 13:49:08 +0100

    Removed unused function

--- backend_go/surface_query/utils/blob_fetcher.go
+++ backend_go/surface_query/utils/blob_fetcher.go
@@ -2,7 +2,6 @@ package utils
 
 import (
 	"fmt"
-	"surface_query/xtgeo"
 )
 
 type BlobFetcher struct {
@@ -26,21 +25,6 @@ func (bf BlobFetcher) FetchAsBytes(objectUuid string) ([]byte, error) {
 	}
 
 	return byteArr, nil
-}
-
-// Download blob and deserialize to surface
-func (bf BlobFetcher) FetchAsSurface(objectUuid string) (*xtgeo.Surface, error) {
-	byteArr, err := bf.FetchAsBytes(objectUuid)
-	if err != nil {
-		return nil, err
-	}
-
-	surface, err := xtgeo.DeserializeBlobToSurface(byteArr)
-	if err != nil {
-		return nil, err
-	}
-
-	return surface, nil
 }
 
 func fetchBlobBytes(sasToken string, baseUrl string, objectUuid string) ([]byte, int, error) {
