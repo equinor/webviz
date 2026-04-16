01d4b492509daf69a02d0e524cf2f4721003a971
Anders Rantala Hunderi <anders@ceetronsolutions.com>
2026-04-16 13:01:32 +0200

    Added Pressed state property to button

--- frontend/src/lib/newComponents/Button/button.tsx
+++ frontend/src/lib/newComponents/Button/button.tsx
@@ -12,6 +12,7 @@ export type ButtonProps = {
     disabled?: boolean;
     round?: boolean;
     iconOnly?: boolean;
+    pressed?: boolean;
 } & ButtonPropsBase;
 
 const DEFAULT_PROPS = {
@@ -25,24 +26,33 @@ const VARIANT_TONE_CLASSES: Record<
     Record<NonNullable<ButtonProps["tone"] | "disabled">, string>
 > = {
     contained: {
-        accent: "bg-accent-strong text-accent-strong-on-emphasis hover:bg-accent-strong-hover active:bg-accent-strong-active border-transparent",
+        accent: "bg-accent-strong text-accent-strong-on-emphasis hover:bg-accent-strong-hover active:bg-accent-strong-active data-pressed:bg-accent-strong-active border-transparent",
+
         neutral:
-            "bg-neutral-strong text-neutral-strong-on-emphasis hover:bg-neutral-strong-hover active:bg-neutral-strong-active  border-transparent",
-        danger: "bg-danger-strong text-danger-strong-on-emphasis hover:bg-danger-strong-hover active:bg-danger-strong-active border-transparent",
+            "bg-neutral-strong text-neutral-strong-on-emphasis hover:bg-neutral-strong-hover active:bg-neutral-strong-active  border-transparent data-pressed:bg-neutral-strong-active",
+
+        danger: "bg-danger-strong text-danger-strong-on-emphasis hover:bg-danger-strong-hover active:bg-danger-strong-active border-transparent data-pressed:bg-danger-strong-active",
+
         disabled: "bg-disabled text-disabled border-disabled cursor-not-allowed opacity-50",
     },
     outlined: {
-        accent: "outline -outline-offset-1 outline-accent text-accent-subtle hover:bg-accent-hover active:bg-accent-active bg-transparent",
+        accent: "outline -outline-offset-1 outline-accent text-accent-subtle hover:bg-accent-hover active:bg-accent-active bg-transparent data-pressed:bg-accent-active",
         neutral:
-            "outline -outline-offset-1 outline-neutral text-neutral-subtle hover:bg-neutral-hover active:bg-neutral-active bg-transparent",
-        danger: "outline -outline-offset-1 outline-danger text-danger-subtle hover:bg-danger-hover active:bg-danger-active bg-transparent",
+            "outline -outline-offset-1 outline-neutral text-neutral-subtle hover:bg-neutral-hover active:bg-neutral-active bg-transparent data-pressed:bg-neutral-active",
+
+        danger: "outline -outline-offset-1 outline-danger text-danger-subtle hover:bg-danger-hover active:bg-danger-active bg-transparent data-pressed:bg-danger-active",
+
         disabled:
             "outline -outline-offset-1 outline-disabled text-disabled cursor-not-allowed opacity-50 bg-transparent",
     },
     text: {
-        accent: "text-accent-subtle hover:bg-accent-hover active:bg-accent-active bg-transparent",
-        neutral: "text-neutral-subtle hover:bg-neutral-hover active:bg-neutral-active bg-transparent",
-        danger: "text-danger-subtle hover:bg-danger-hover active:bg-danger-active bg-transparent",
+        accent: "text-accent-subtle hover:bg-accent-hover active:bg-accent-active data-pressed:bg-accent-active bg-transparent",
+
+        neutral:
+            "text-neutral-subtle hover:bg-neutral-hover active:bg-neutral-active data-pressed:bg-neutral-active bg-transparent",
+
+        danger: "text-danger-subtle hover:bg-danger-hover active:bg-danger-active  data-pressed:bg-danger-active bg-transparent",
+
         disabled: "text-disabled cursor-not-allowed opacity-50",
     },
 };
@@ -55,17 +65,19 @@ const SIZE_CLASSES: Record<NonNullable<ButtonProps["size"]>, string> = {
 
 export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(function Button(props, ref) {
     const defaultedProps = { ...DEFAULT_PROPS, ...props };
+    const { pressed, iconOnly, ...rest } = defaultedProps;
 
     return (
         <ButtonBase
-            {...defaultedProps}
+            {...rest}
             ref={ref}
+            data-pressed={pressed ? "" : undefined}
             className={resolveClassNames(
                 "button",
                 {
                     "rounded-full": props.round,
                     rounded: !props.round,
-                    "button__icon aspect-square justify-center": props.iconOnly,
+                    "button__icon aspect-square justify-center": iconOnly,
                 },
                 SIZE_CLASSES[defaultedProps.size],
                 VARIANT_TONE_CLASSES[defaultedProps.variant][
@@ -73,7 +85,7 @@ export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(function 
                 ],
             )}
         >
-            {props.iconOnly ? props.children : <span className="button__label">{props.children}</span>}
+            {iconOnly ? props.children : <span className="button__label">{props.children}</span>}
         </ButtonBase>
     );
 });
