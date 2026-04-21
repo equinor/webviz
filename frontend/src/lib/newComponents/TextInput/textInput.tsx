445aa06cc36f3548a67f79ab086e899b9b2cff33
Anders Rantala Hunderi <anders@ceetronsolutions.com>
2026-04-21 14:03:33 +0200

    Made text input better apply state-based styling

--- frontend/src/lib/newComponents/TextInput/textInput.tsx
+++ frontend/src/lib/newComponents/TextInput/textInput.tsx
@@ -1,35 +1,64 @@
 import React from "react";
 
-import { Input, type InputProps as InputBaseProps } from "@base-ui/react";
+import type { HTMLProps, InputProps as InputBaseProps, InputState } from "@base-ui/react";
+import { Input } from "@base-ui/react";
+import { Error } from "@mui/icons-material";
+import { omit } from "lodash";
 
 import { resolveClassNames } from "@lib/utils/resolveClassNames";
 
-export type TextInputProps = Omit<InputBaseProps, "className"> & {
+export type TextInputProps = Omit<InputBaseProps, "className" | "render"> & {
     startAdornment?: React.ReactNode;
     endAdornment?: React.ReactNode;
 };
 
 export const TextInput = React.forwardRef<HTMLInputElement, TextInputProps>(function TextInput(props, ref) {
+    const baseProps = omit(props, ["startAdornment", "endAdornment"]);
+
+    function makeStartAdornment(state: InputState) {
+        // state.valid is explicitly null when no validity is applied
+        if (state.valid === false) {
+            return <Error fontSize="inherit" className="fill-text-danger-subtle!" />;
+        }
+        return props.startAdornment;
+    }
+
+    function extractDataProps(p: HTMLProps) {
+        const dataAttributes: Record<string, any> = {};
+
+        for (const key in p) {
+            if (key.startsWith("data-")) {
+                dataAttributes[key] = p[key as keyof typeof p];
+            }
+        }
+
+        return dataAttributes;
+    }
+
     return (
-        <div
-            className={resolveClassNames(
-                "gap-vertical-xs bg-canvas py-vertical-3xs px-vertical-xs text-body-lg data-invalid:outline-danger data-invalid:bg-danger-surface flex items-center",
-                {
-                    "form-element outline-neutral text-neutral outline -outline-offset-2": !props.disabled,
-                    "text-disabled bg-disabled": props.disabled,
-                },
+        <Input
+            {...baseProps}
+            ref={ref}
+            render={(p, state) => (
+                <div
+                    {...extractDataProps(p)}
+                    className={resolveClassNames(
+                        "py-vertical-3xs px-vertical-xs text-body-lg",
+                        "gap-vertical-xs flex items-center -outline-offset-2",
+                        "bg-canvas",
+                        "outline-neutral outline",
+                        "text-neutral not-data-disabled:form-element",
+                        // Disabled
+                        "data-disabled:bg-disabled data-disabled:text-disabled data-disabled:outline-hidden",
+                        // Invalid
+                        "data-invalid:outline-danger data-invalid:bg-danger-canvas",
+                    )}
+                >
+                    {makeStartAdornment(state)}
+                    <input className="min-w-0 grow p-0 outline-0 data-disabled:cursor-not-allowed" {...p} />
+                    {props.endAdornment}
+                </div>
             )}
-        >
-            {props.startAdornment}
-
-            <Input
-                {...props}
-                ref={ref}
-                className={resolveClassNames("grow p-0 outline-0 data-disabled:cursor-not-allowed")}
-                disabled={props.disabled}
-            />
-
-            {props.endAdornment}
-        </div>
+        />
     );
 });
