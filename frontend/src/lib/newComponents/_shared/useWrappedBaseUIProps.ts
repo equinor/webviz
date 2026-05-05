c2200f0ff593421f1060e01ce5406a2efdc2c024
Anders Rantala Hunderi <anders@ceetronsolutions.com>
2026-05-05 10:47:59 +0200

    Added style to base-ui wrapper hook

--- frontend/src/lib/newComponents/_shared/useWrappedBaseUIProps.ts
+++ frontend/src/lib/newComponents/_shared/useWrappedBaseUIProps.ts
@@ -5,7 +5,7 @@ import type { BaseUIComponentProps } from "node_modules/@base-ui/react/esm/utils
 /** Utility type for base-ui wrapper classes. Removes the class-name and render function from the prop, as these tend to give implementers too much freedom. We also reintroduce a layoutClassName prop to make it clear that classnames should only be used for layout values (like margins, width, position) */
 export type WrappedBaseUIProps<BaseUIProps extends BaseUIComponentProps<any, any>> = Omit<
     BaseUIProps,
-    "className" | "render"
+    "className" | "render" | "style"
 > & {
     /** Class names applied to the element. Should only be used for adjusting layout (margins, visibility, positioning) */
     layoutClassName?: string;
