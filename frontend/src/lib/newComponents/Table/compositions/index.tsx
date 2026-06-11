cff108def541aefedcc3c382962b467c63c54e21
Anders Rantala Hunderi <anders@ceetronsolutions.com>
2026-06-11 16:00:21 +0200

    Added table composition for virtualized rows

--- frontend/src/lib/newComponents/Table/compositions/index.tsx
+++ frontend/src/lib/newComponents/Table/compositions/index.tsx
@@ -1,7 +1,11 @@
 import { PendingRow, PendingRows } from "./pendingRows";
+import { VirtualizedRows } from "./virtualizedRows";
+
+export type { VirtualizedRowsProps } from "./virtualizedRows";
 export type { PendingRowsProps } from "./pendingRows";
 
 export const TableCompositions = {
     PendingRows,
     PendingRow,
+    VirtualizedRows,
 } as const;
