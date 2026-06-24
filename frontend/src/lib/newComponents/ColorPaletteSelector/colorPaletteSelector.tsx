163adde89e09cf0e7e7ba75d1b9b89de2f5e1993
Ruben Thoms <ruben.thoms@ceetronsolutions.com>
2026-06-24 10:56:15 +0200

    refactor: callbacks and support for default combobox props

--- frontend/src/lib/newComponents/ColorPaletteSelector/colorPaletteSelector.tsx
+++ frontend/src/lib/newComponents/ColorPaletteSelector/colorPaletteSelector.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { ColorGradient } from "@lib/newComponents/ColorGradient";
 import { ColorTile } from "@lib/newComponents/ColorTile";
-import { Combobox } from "@lib/newComponents/Combobox";
+import { Combobox, type ComboboxProps } from "@lib/newComponents/Combobox";
 import type { ComboboxItem } from "@lib/newComponents/Combobox/types";
 import type { ColorPalette } from "@lib/utils/ColorPalette";
 
@@ -19,15 +19,18 @@ function makeColorPalettePreview(
 ): React.ReactNode {
     switch (type) {
         case ColorPaletteSelectorType.Continuous:
-            return <ColorGradient colorPalette={colorPalette} layoutClassName="w-24" size="small" />;
+            return <ColorGradient colorPalette={colorPalette} layoutClassName="w-full" size="small" />;
         case ColorPaletteSelectorType.Categorical:
-            return <ColorTile.Group colorPalette={colorPalette} layoutClassName="w-24" size="small" />;
+            return <ColorTile.Group colorPalette={colorPalette} layoutClassName="w-full" size="small" />;
         case ColorPaletteSelectorType.Discrete:
-            return <ColorGradient colorPalette={colorPalette} layoutClassName="w-24" steps={steps} size="small" />;
+            return <ColorGradient colorPalette={colorPalette} layoutClassName="w-full" steps={steps} size="small" />;
     }
 }
 
-export type ColorPaletteSelectorProps = {
+export type ColorPaletteSelectorProps = Omit<
+    ComboboxProps<ColorPalette>,
+    "items" | "value" | "renderItemAdornment" | "onValueChange" | "onValueCommit" | "defaultValue"
+> & {
     /** The list of available color palettes to choose from. */
     colorPalettes: ColorPalette[];
     /** The ID of the currently selected color palette. */
@@ -38,38 +41,46 @@ export type ColorPaletteSelectorProps = {
     type: ColorPaletteSelectorType;
     /** Number of discrete steps shown in the preview. Only used when `type` is `Discrete`. */
     steps?: number;
+    /** Whether the selector is disabled. */
+    disabled?: boolean;
 };
 
 export const ColorPaletteSelector = React.forwardRef<HTMLInputElement, ColorPaletteSelectorProps>(
     function ColorPaletteSelector(props, ref) {
+        const { colorPalettes, selectedColorPaletteId, onValueChange, type, steps, ...comboboxProps } = props;
+
         const selectedColorPalette =
-            props.colorPalettes.find((el) => el.getId() === props.selectedColorPaletteId) ?? props.colorPalettes[0];
+            colorPalettes.find((el) => el.getId() === selectedColorPaletteId) ?? colorPalettes[0];
 
         const comboboxItems = React.useMemo(
             () =>
-                props.colorPalettes.map<ComboboxItem<ColorPalette>>((palette) => ({
+                colorPalettes.map<ComboboxItem<ColorPalette>>((palette) => ({
                     value: palette,
                     label: palette.getName(),
                 })),
-            [props.colorPalettes],
+            [colorPalettes],
         );
 
-        function handleValueChange(colorPalette: ColorPalette | null) {
-            if (!colorPalette) {
-                return;
-            }
-            props.onValueChange?.(colorPalette);
-        }
+        const handleValueChange = React.useCallback(
+            function handleValueChange(colorPalette: ColorPalette | null) {
+                if (!colorPalette) {
+                    return;
+                }
+                onValueChange?.(colorPalette);
+            },
+            [onValueChange],
+        );
 
         return (
             <Combobox
+                {...comboboxProps}
                 ref={ref}
                 items={comboboxItems}
                 value={selectedColorPalette}
                 onValueChange={handleValueChange}
                 filter={null}
                 renderItemAdornment={(palette) => (
-                    <span className="">{makeColorPalettePreview(palette, props.type, props.steps)}</span>
+                    <span className="flex w-24 min-w-0">{makeColorPalettePreview(palette, type, steps)}</span>
                 )}
             />
         );
