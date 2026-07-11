51710f82f88fef4a54bd018ad59eb54b58310a1a
Sigurd Pettersen <sigurd.pettersen@ceetronsolutions.com>
2026-07-11 20:13:03 +0200

    Use queue name from config/environment

--- backend_py/pyworker_default/src/pyworker_default/worker_config.py
+++ backend_py/pyworker_default/src/pyworker_default/worker_config.py
@@ -16,7 +16,7 @@ def load_worker_config_from_env() -> WorkerConfig:
 
     return WorkerConfig(
         sb_fq_namespace=os.environ["WEBVIZ_SERVICE_BUS_FQ_NAMESPACE"],
-        sb_queue_name=os.getenv("WEBVIZ_SERVICE_BUS_QUEUE_NAME", "test-queue"),
+        sb_queue_name=os.environ["WEBVIZ_SERVICE_BUS_QUEUE_NAME"],
         sb_payload_fernet_key=os.environ["WEBVIZ_SERVICE_BUS_PAYLOAD_FERNET_KEY"],
         redis_cache_url=f"redis://:{redis_password}@redis-cache:6379",
         sumo_env=os.getenv("WEBVIZ_SUMO_ENV", "prod"),
