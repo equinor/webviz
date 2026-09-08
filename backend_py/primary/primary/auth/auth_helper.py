bbe7fab01002a2195a99158494c83691e4520386
Sigurd Pettersen <sigurd.pettersen@ceetronsolutions.com>
2026-09-08 12:51:23 +0200

    Removed up-front inclusion of all scopes

--- backend_py/primary/primary/auth/auth_helper.py
+++ backend_py/primary/primary/auth/auth_helper.py
@@ -67,10 +67,6 @@ class AuthHelper:
         request.session.clear()
 
         all_scopes_list = config.GRAPH_SCOPES.copy()
-        all_scopes_list.extend(config.RESOURCE_SCOPES_DICT["sumo"])
-        # all_scopes_list.extend(config.RESOURCE_SCOPES_DICT["smda"])
-        # all_scopes_list.extend(config.RESOURCE_SCOPES_DICT["ssdl"])
-        all_scopes_list.extend(config.RESOURCE_SCOPES_DICT["pdm"])
 
         if "CODESPACE_NAME" in os.environ:
             # Developer is using GitHub codespace, so we use the GitHub codespace port forward URL
@@ -203,12 +199,14 @@ def _acquire_access_token_for_resource_scopes(
     if not scopes_list:
         return None
 
+    # Earlier we used acquire_token_silent() here, but it doesn't give any feedback on why a call failed,
+    # so we switched to acquire_token_silent_with_error() instead and log errors.
     token_dict = cca.acquire_token_silent_with_error(scopes=scopes_list, account=account)
     if "error" in token_dict:
-        LOGGER.error(f"Error acquiring token silently ({resource_name=}, {scopes_list=}), error: {token_dict['error']}, error_description: {token_dict.get('error_description')}")
+        LOGGER.error(
+            f"Error acquiring token silently ({resource_name=}, {scopes_list=}), error: {token_dict['error']}, error_description: {token_dict.get('error_description')}"
+        )
         return None
-
-    #token_dict = cca.acquire_token_silent(scopes=scopes_list, account=account)
 
     access_token = token_dict.get("access_token") if token_dict else None
     if not access_token:
